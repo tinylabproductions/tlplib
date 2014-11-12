5868f374dd9eaad2378169bdc65a6dcdca0a0ea4
Arturas Slajus <arturas@tinylabproductions.com>
2014-11-12 13:24:19 +0200

    Bounds#randomPoint

---
+++ TLPLib/Extensions/BoundsExts.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class BoundsExts {
+    public static Vector3 randomPoint(this Bounds bounds) {
+      var min = bounds.min;
+      var max = bounds.max;
+      var x = Random.Range(min.x, max.x);
+      var y = Random.Range(min.y, max.y);
+      var z = Random.Range(min.z, max.z);
+      return new Vector3(x, y, z);
+    }
+  }
+}
