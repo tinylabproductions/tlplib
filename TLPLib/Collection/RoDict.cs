0bbeffe9eff4e9f3b57debe86a2d32be468c71b7
Arturas Slajus <arturas@tinylabproductions.com>
2014-11-13 17:30:52 +0200

    Read only dicts.

---
+++ TLPLib/Collection/RoDict.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Functional;
+
+namespace com.tinylabproductions.TLPLib.Collection {
+  public static class RoDict {
+    public static RoDict<K, V> a<K, V>(IDictionary<K, V> dict) { return new RoDict<K, V>(dict);}
+  }
+
+  /* Read only dictionary */
+  public struct RoDict<K, V> {
+    readonly IDictionary<K, V> underlying;
+
+    public RoDict(IDictionary<K, V> underlying) { this.underlying = underlying; }
+
+    public int count { get { return underlying.Count; } }
+    public bool isEmpty { get { return count == 0; } }
+    public bool nonEmpty { get { return ! isEmpty; } }
+    public IEnumerable<K> keys { get { return underlying.Keys; } }
+    public IEnumerable<V> values { get { return underlying.Values; } }
+    public Option<V> get(K key) { return underlying.get(key); }
+  }
+}
