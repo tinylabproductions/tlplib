bda7f22f374620a0c129fc4e50fcb0f7d09131d4
Arturas Slajus <arturas@tinylabproductions.com>
2014-11-13 17:30:42 +0200

    Moved option methods back to struct.

--- TLPLib/Functional/Option.cs
+++ TLPLib/Functional/Option.cs
@@ -10,74 +10,17 @@ namespace com.tinylabproductions.TLPLib.Functional {
  * http://stackoverflow.com/questions/1188354/can-i-specify-a-supertype-relation-in-c-sharp-generic-constraints
  * 
  * Beware that this causes boxing for value types.
- * 
- * Also Mono compiler seems to be a lot happier with generic extension 
- * methods, than instance ones. 
  **/
 public static class Option {
   public static IEnumerable<A> asEnum<A, B>(this Option<B> opt)
-  where B : A {
+  where B : class, A  {
     return opt.isDefined 
       ? (IEnumerable<A>) opt.get.Yield() 
       : Enumerable.Empty<A>();
   }
 
-  public static IEnumerable<A> asEnum<A>(this Option<A> opt) {
-    return opt.isDefined ? opt.get.Yield() : Enumerable.Empty<A>();
-  }
-
-  public static Option<A> createOrTap<A>(
-    this Option<A> opt, Fn<A> ifEmpty, Act<A> ifNonEmpty
-  ) {
-    if (opt.isEmpty) return new Option<A>(ifEmpty());
-
-    ifNonEmpty(opt.get);
-    return opt;
-  }
-
-  public static Option<A> orElse<A>(this Option<A> opt, Fn<Option<A>> other) {
-    return opt.isDefined ? opt : other();
-  }
-
-  public static Option<A> orElse<A>(this Option<A> opt, Option<A> other) {
-    return opt.isDefined ? opt : other;
-  }
-
-  public static A orNull<A>(this Option<A> opt) where A : class {
+  public static A orNull<A>(Option<A> opt) where A : class {
     return opt.fold(() => null, _ => _);
-  }
-
-  // Alias for #fold with elements switched up.
-  public static B cata<A, B>(this Option<A> opt, Fn<A, B> ifNonEmpty, Fn<B> ifEmpty) {
-    return opt.fold(ifEmpty, ifNonEmpty);
-  }
-
-  // Alias for #fold with elements switched up.
-  public static B cata<A, B>(this Option<A> opt, Fn<A, B> ifNonEmpty, B ifEmpty) {
-    return opt.fold(ifEmpty, ifNonEmpty);
-  }
-
-  public static void voidFold<A>(this Option<A> opt, Action ifEmpty, Act<A> ifNonEmpty) {
-    if (opt.isSome) ifNonEmpty(opt.get);
-    else ifEmpty();
-  }
-
-  public static Option<B> map<A, B>(this Option<A> opt, Fn<A, B> func) {
-    return opt.isDefined ? F.some(func(opt.get)) : F.none<B>();
-  }
-
-  public static Option<B> flatMap<A, B>(
-    this Option<A> opt, Fn<A, Option<B>> func
-  ) {
-    return opt.isDefined ? func(opt.get) : F.none<B>();
-  }
-
-  public static Option<Tpl<A, B>> zip<A, B>(
-    this Option<A> opt1, Option<B> opt2
-  ) {
-    return opt1.isDefined && opt2.isDefined
-      ? F.some(F.t(opt1.get, opt2.get))
-      : F.none<Tpl<A, B>>();
   }
 }
 
@@ -104,11 +47,6 @@ public struct Option<A> {
     return this;
   }
 
-  public void voidFold(Act ifEmpty, Act<A> ifNonEmpty) {
-    if (isSome) ifNonEmpty(value);
-    else ifEmpty();
-  }
-
   public Option<A> filter(Fn<A, bool> predicate) {
     return (isSome ? (predicate(value) ? this : F.none<A>()) : this);
   }
@@ -122,7 +60,7 @@ public struct Option<A> {
   }
 
   public bool exists(A a, IEqualityComparer<A> comparer) {
-    return isSome && comparer.Equals(value, a); ;
+    return isSome && comparer.Equals(value, a);
   }
 
   public bool isDefined { get { return isSome; } }
@@ -158,6 +96,43 @@ public struct Option<A> {
 
   public override string ToString() {
     return isSome ? "Some(" + value + ")" : "None";
+  }
+
+  public IEnumerable<A> asEnum { get { return isSome ? value.Yield() : Enumerable.Empty<A>(); } }
+
+  public Option<A> createOrTap(Fn<A> ifEmpty, Act<A> ifNonEmpty) {
+    if (isEmpty) return new Option<A>(ifEmpty());
+
+    ifNonEmpty(value);
+    return this;
+  }
+
+  public Option<A> orElse(Fn<Option<A>> other) { return isSome ? this : other(); }
+  public Option<A> orElse(Option<A> other) { return isSome ? this : other; }
+
+  // Alias for #fold with elements switched up.
+  public B cata<B>(Fn<A, B> ifNonEmpty, Fn<B> ifEmpty) { return fold(ifEmpty, ifNonEmpty); }
+
+  // Alias for #fold with elements switched up.
+  public B cata<B>(Fn<A, B> ifNonEmpty, B ifEmpty) { return fold(ifEmpty, ifNonEmpty); }
+
+  public void voidFold(Act ifEmpty, Act<A> ifNonEmpty) {
+    if (isSome) ifNonEmpty(value);
+    else ifEmpty();
+  }
+
+  public Option<B> map<B>(Fn<A, B> func) {
+    return isSome ? F.some(func(value)) : F.none<B>();
+  }
+
+  public Option<B> flatMap<B>(Fn<A, Option<B>> func) {
+    return isSome ? func(value) : F.none<B>();
+  }
+
+  public Option<Tpl<A, B>> zip<B>(Option<B> opt2) {
+    return isSome && opt2.isDefined
+      ? F.some(F.t(value, opt2.get))
+      : F.none<Tpl<A, B>>();
   }
 
   public B fold<B>(Fn<B> ifEmpty, Fn<A, B> ifNonEmpty) {
