192a51ba044f6a4537cbaa6aca91ba184daaa8e3
Arturas Slajus <arturas@tinylabproductions.com>
2014-11-13 17:30:19 +0200

    Rewrote matchers to be heap allocation free.

--- TLPLib/Functional/Matching/Match.cs
+++ TLPLib/Functional/Matching/Match.cs
@@ -1,98 +1,80 @@
 ﻿using System;
+using com.tinylabproductions.TLPLib.Extensions;
 
 namespace com.tinylabproductions.TLPLib.Functional.Matching {
-  public interface IVoidMatcher<Base> where Base : class {
-    IVoidMatcher<Base> when<T>(Act<T> onMatch) where T : Base;
-    IVoidMatcher<Base> when<T>(Fn<T, bool> predicate, Act<T> onMatch) where T : Base;
-    void orElse(Act<Base> act);
-  }
-
-  public interface IMatcher<in Base, Return> where Base : class {
-    IMatcher<Base,Return> when<T>(Fn<T, Return> onMatch) where T : class, Base;
-    /* Unrestrained version of when, needed to work around C# type system 
-     * sometimes. */
-    IMatcher<Base, Return> whenU<T>(Fn<T, Return> onMatch) where T : class;
-
-    Return get();
-    Return getOrElse(Fn<Return> elseFunc);
-    Return getOrElse(Return elseVal);
-  }
-
-  public class MatchError : Exception {
-    public MatchError(string message) : base(message) { }
-  }
-
-  public struct Matcher<Base, Return> : IVoidMatcher<Base>, IMatcher<Base, Return>
-  where Base : class {
-    private readonly Base subject;
+  public struct Matcher<Base, Return> where Base : class {
+    readonly Base subject;
+    readonly Option<Return> result;
 
     public Matcher(Base subject) {
       this.subject = subject;
+      result = F.none<Return>();
     }
 
-    public IVoidMatcher<Base> when<T>(Act<T> onMatch) where T : Base 
+    Matcher(Return result) {
+      subject = default(Base);
+      this.result = result.some();
+    }
+
+    static Fn<A, Return> actToFn<A>(Act<A> act) { return a => { act(a); return default(Return); }; }
+
+    #region Constrained matchers
+
+    public Matcher<Base, Return> doWhen<A>(Act<A> onMatch) where A : class, Base 
+    { return when(actToFn(onMatch)); }
+
+    public Matcher<Base, Return> when<A>(Fn<A, Return> onMatch) where A : class, Base 
     { return when(_ => true, onMatch); }
 
-    public IVoidMatcher<Base> when<T>(Fn<T, bool> predicate, Act<T> onMatch) where T : Base {
-      if (subject is T) {
-        var casted = (T) subject;
-        if (predicate(casted)) onMatch(casted);
-        return new SuccessfulMatcher<Base, Unit>(F.unit);
-      }
-      else return this;
-    }
+    public Matcher<Base, Return> doWhen<A>(Fn<A, bool> guard, Act<A> onMatch) where A : class, Base 
+    { return when(guard, actToFn(onMatch)); }
 
-    public void orElse(Act<Base> act) { act(subject); }
+    public Matcher<Base, Return> when<A>(Fn<A, bool> guard, Fn<A, Return> onMatch) where A : class, Base 
+    { return whenU(guard, onMatch); }
 
-    public IMatcher<Base, Return> when<T>(Fn<T, Return> onMatch)
-    where T : class, Base { return whenU(onMatch); }
+    #endregion
 
-    public IMatcher<Base, Return> whenU<T>(Fn<T, Return> onMatch) where T : class {
-      var casted = subject as T;
-      if (casted != null) {
-        return new SuccessfulMatcher<Base, Return>(onMatch.Invoke(casted));
+    #region Unconstrained matchers
+
+    public Matcher<Base, Return> doWhenU<A>(Act<A> onMatch) where A : class 
+    { return whenU(actToFn(onMatch)); }
+
+    public Matcher<Base, Return> whenU<A>(Fn<A, Return> onMatch) where A : class 
+    { return whenU(_ => true, onMatch); }
+
+    public Matcher<Base, Return> doWhenU<A>(Fn<A, bool> guard, Act<A> onMatch) where A : class 
+    { return whenU(guard, actToFn(onMatch)); }
+
+    public Matcher<Base, Return> whenU<A>(Fn<A, bool> guard, Fn<A, Return> onMatch) where A : class {
+      if (result.isDefined) return this;
+
+      if (subject is A) {
+        var casted = (A)(object)subject;
+        if (guard(casted)) return new Matcher<Base, Return>(onMatch(casted));
       }
 
       return this;
     }
 
-    public Return get() {
-      throw new MatchError(string.Format(
-        "Subject {0} of type {1} couldn't be matched!", subject, typeof(Base)
-      ));
-    }
+    #endregion
 
-    public Return getOrElse(Fn<Return> elseFunc) { return elseFunc.Invoke(); }
-    public Return getOrElse(Return elseVal) { return elseVal; }
+    #region Getters
+
+    public void orElse(Act<Base> act) { if (result.isEmpty) act(subject); }
+    public Return get() {
+      var s = subject;
+      return result.getOrThrow(() => new MatchError(string.Format(
+        "Subject {0} of type {1} couldn't be matched!", s, typeof(Base)
+      )));
+    }
+    public Return getOrElse(Fn<Return> elseFunc) { return result.getOrElse(elseFunc); }
+    public Return getOrElse(Return elseVal) { return result.getOrElse(elseVal); }
+
+    #endregion
   }
 
-  public struct SuccessfulMatcher<Base, Return> 
-  : IVoidMatcher<Base>, IMatcher<Base, Return>
-  where Base : class {
-    private readonly Return result;
-
-    public SuccessfulMatcher(Return result) {
-      this.result = result;
-    }
-
-    public IVoidMatcher<Base> when<T>(Act<T> onMatch) 
-    where T : Base { return this; }
-
-    public IVoidMatcher<Base> when<T>(Fn<T, bool> predicate, Act<T> onMatch) 
-    where T : Base { return this; }
-
-    public void orElse(Act<Base> act) {}
-
-    public IMatcher<Base, Return> when<T>(Fn<T, Return> onMatch)
-    where T : class, Base { return this; }
-
-    public IMatcher<Base, Return> whenU<T>(Fn<T, Return> onMatch) 
-    where T : class { return this; }
-
-    public Return get() { return result; }
-
-    public Return getOrElse(Fn<Return> elseFunc) { return get(); }
-    public Return getOrElse(Return elseVal) { return get(); }
+  public class MatchError : Exception {
+    public MatchError(string message) : base(message) { }
   }
 
   public struct MatcherBuilder<T> where T : class {
@@ -102,7 +84,7 @@ namespace com.tinylabproductions.TLPLib.Functional.Matching {
       this.subject = subject;
     }
 
-    public IMatcher<T, Return> returning<Return>() {
+    public Matcher<T, Return> returning<Return>() {
       return new Matcher<T, Return>(subject);
     }
   }
@@ -111,7 +93,7 @@ namespace com.tinylabproductions.TLPLib.Functional.Matching {
     public static MatcherBuilder<T> match<T>(this T subject)
     where T : class { return new MatcherBuilder<T>(subject); }
 
-    public static IVoidMatcher<T> matchVoid<T>(this T subject)
+    public static Matcher<T, Unit> matchVoid<T>(this T subject)
     where T : class { return new Matcher<T, Unit>(subject); }
   }
 }
