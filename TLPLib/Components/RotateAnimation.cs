b17fa7f751faf04cbb298dd8f9f054342ee172cc
Arturas Slajus <arturas@tinylabproductions.com>
2014-11-14 17:50:31 +0200

    RotateAnimation component

---
+++ TLPLib/Components/RotateAnimation.cs
@@ -0,0 +1,12 @@
+﻿using com.tinylabproductions.TLPLib.Annotations;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Components {
+  public class RotateAnimation : MonoBehaviour {
+    public Vector3 rotation;
+
+    [UsedImplicitly] void Update() {
+      transform.Rotate(rotation * Time.deltaTime);
+    }
+  }
+}
