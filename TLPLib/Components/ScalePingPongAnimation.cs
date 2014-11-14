44f3d4b92e209d75ffc7672ee49c26b35e9e965d
Arturas Slajus <arturas@tinylabproductions.com>
2014-11-14 17:50:47 +0200

    ScalePingPongAnimation component

---
+++ TLPLib/Components/ScalePingPongAnimation.cs
@@ -0,0 +1,15 @@
+﻿using com.tinylabproductions.TLPLib.Annotations;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Components {
+  public class ScalePingPongAnimation : MonoBehaviour {
+    public Vector3 startScale, endScale;
+    public float durationS;
+
+    [UsedImplicitly]
+    void Update() {
+      var d = Mathf.PingPong(Time.time, durationS) / durationS;
+      transform.localScale = startScale + (endScale - startScale) * d;
+    }
+  }
+}
