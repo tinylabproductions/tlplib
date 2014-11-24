3d04de9a84210679fb52e8a3a5c6f965d5a77b57
Evaldas Ciakas <evaldas757@gmail.com>
2014-11-24 16:19:19 +0200

    DestroyAfter behaviour.

---
+++ TLPLib/Components/DestroyAfter.cs
@@ -0,0 +1,12 @@
+﻿using com.tinylabproductions.TLPLib.Annotations;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Components {
+  class DestroyAfter : MonoBehaviour {
+    public float delay = 1f;
+
+    [UsedImplicitly] void Start() {
+      Destroy(gameObject, delay);
+    }
+  }
+}
