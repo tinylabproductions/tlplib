e3ca96ba64d70d36ad2a16845fb9c1566a149ea0
Arturas Slajus <x11@arturaz.net>
2015-01-03 18:34:09 +0200

    Huh?

---
+++ TLPLib/Threads/ThreadPooled.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using com.tinylabproductions.TLPLib.Concurrent;
+using com.tinylabproductions.TLPLib.Functional;
+
+namespace com.tinylabproductions.TLPLib.Threads {
+  public static class ThreadPooled {
+    /* Loop `count` times, return when done. */
+    public static void loop(int count, Act<int> eachIndex) {
+      var finished = 0;
+      for (var idx = 0; idx < count; idx++) {
+        var localIdx = idx;
+        ThreadPool.QueueUserWorkItem(_ => {
+          eachIndex(localIdx);
+          Interlocked.Increment(ref finished);
+        });
+      }
+      // ReSharper disable once LoopVariableIsNeverChangedInsideLoop
+      while (finished < count) Thread.Sleep(0);
+    }
+
+    /* Loop `count` times, complete future when done. */
+    public static Future<Unit> loopF(int count, Act<int> eachIndex) {
+      return Future.a<Unit>(promise => {
+        var finished = 0;
+        for (var idx = 0; idx < count; idx++) {
+          var localIdx = idx;
+          ThreadPool.QueueUserWorkItem(_ => {
+            eachIndex(localIdx);
+            if (Interlocked.Increment(ref finished) == count) 
+              promise.completeSuccess(F.unit);
+          });
+        }
+      });
+    }
+  }
+}
