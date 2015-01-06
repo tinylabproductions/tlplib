305fe41e6c3966ca7f29d85303dbd911f4c47afa
Arturas Slajus <arturas@tinylabproductions.com>
2015-01-06 09:37:45 +0200

    ThreadPooled#loop & friends now create 1 chunk for each core.

--- TLPLib/Threads/ThreadPooled.cs
+++ TLPLib/Threads/ThreadPooled.cs
@@ -2,56 +2,71 @@
 using System.Threading;
 using com.tinylabproductions.TLPLib.Concurrent;
 using com.tinylabproductions.TLPLib.Functional;
-using com.tinylabproductions.TLPLib.Logger;
+using Smooth.Pools;
+using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Threads {
   public static class ThreadPooled {
-    /* Loop `count` times, return when done. */
-    public static void loop(int count, Act<int> eachIndex) {
+    static readonly Pool<ManualResetEvent> eventPool = new Pool<ManualResetEvent>(
+      () => new ManualResetEvent(false),
+      e => e.Reset()
+    );
+
+    static void loop(
+      int count, int chunks, Act onFinish, Act<Exception> onError, Act<int> eachIndex
+    ) {
+      if (chunks <= 0) chunks = Environment.ProcessorCount;
+      if (chunks > count) chunks = count;
+
+      var chunkSize = Mathf.CeilToInt((float) count / chunks);
       var finished = 0;
-      var error = F.none<Exception>();
-      for (var idx = 0; idx < count; idx++) {
-        var localIdx = idx;
+      for (var chunkStartIdx = 0; chunkStartIdx < count; chunkStartIdx += chunkSize) {
+        var startIdx = chunkStartIdx;
+        var chunkEndIdx = Mathf.Min(chunkStartIdx + chunkSize, count); // Exclusive
         ThreadPool.QueueUserWorkItem(_ => {
-          try {
-            eachIndex(localIdx);
-            Interlocked.Increment(ref finished);
+          for (var idx = startIdx; idx < chunkEndIdx; idx++) {
+            try { eachIndex(idx); }
+            catch (Exception e) { onError(e); }
           }
-          catch (Exception e) { error = F.some(e); }
+          if (Interlocked.Add(ref finished, chunkEndIdx - startIdx) == count) onFinish();
         });
       }
-      // ReSharper disable once LoopVariableIsNeverChangedInsideLoop
-      while (finished < count && error.isEmpty) Thread.Sleep(0);
+    }
+
+    /* Loop `count` times, doing each iteration in separate work item, return when done.
+     * 
+     * If `chunks` <= 0, chunks = processor count.
+     * If `chunks` >= count, clamps to count.
+     */
+    public static void loop(int count, Act<int> eachIndex, int chunks=0) {
+      var evt = eventPool.Borrow();
+      var error = F.none<Exception>();
+      loop(
+        count, chunks, () => evt.Set(), err => { error = F.some(err); evt.Set(); }, 
+        eachIndex
+      );
+      evt.WaitOne();
+      eventPool.Release(evt);
       error.each(F.doThrow);
     }
 
     /* As #loop, but uses Future API. */
-    public static Future<Unit> loopSyncF(int count, Act<int> eachIndex) {
-      loop(count, eachIndex);
+    public static Future<Unit> loopSyncF(int count, Act<int> eachIndex, int chunks = 0) {
+      loop(count, eachIndex, chunks);
       return Future.successfulUnit;
     }
 
-    /* Loop `count` times, completes the future asynchronously in main thread when done. 
+    /* As #loop, but completes the future asynchronously in main thread when done. 
      * 
      * Adds 1 frame time because calling from other threads back to unity requires going 
      * through Update(). */
-    public static Future<Unit> loopF(int count, Act<int> eachIndex) {
-      return Future.a<Unit>(promise => {
-        var finished = 0;
-        for (var idx = 0; idx < count; idx++) {
-          var localIdx = idx;
-          ThreadPool.QueueUserWorkItem(_ => {
-            try {
-              eachIndex(localIdx);
-              if (Interlocked.Increment(ref finished) == count)
-                ASync.OnMainThread(() => promise.tryCompleteSuccess(F.unit));
-            }
-            catch (Exception e) {
-              ASync.OnMainThread(() => promise.tryCompleteError(e));
-            }
-          });
-        }
-      });
+    public static Future<Unit> loopF(int count, Act<int> eachIndex, int chunks = 0) {
+      return Future.a<Unit>(promise => loop(
+        count, chunks,
+        () => ASync.OnMainThread(() => promise.tryCompleteSuccess(F.unit)), 
+        err => ASync.OnMainThread(() => promise.tryCompleteError(err)), 
+        eachIndex
+      ));
     }
   }
 }
