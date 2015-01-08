50b67a03a59c2796b50781e5d910f8bd8a82fe69
Arturas Slajus <arturas@tinylabproductions.com>
2015-01-08 15:59:49 +0200

    Profile.block

---
+++ TLPLib/Profiling/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Profiling {
+  public static class Profile {
+    public static A block<A>(string name, Fn<A> f) {
+      Profiler.BeginSample(name);
+      var a = f();
+      Profiler.EndSample();
+      return a;
+    }
+
+    public static void block(string name, Act f) {
+      Profiler.BeginSample(name);
+      f();
+      Profiler.EndSample();
+    }
+  }
+}
