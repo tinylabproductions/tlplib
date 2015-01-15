675b72e7982094fb6ca980da348393d6c9f8608a
Evaldas Ciakas <evaldas757@gmail.com>
2015-01-15 18:01:02 +0200

    More tween behaviours.

---
+++ TLPLib/Tween/Behaviours/ColorTween.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace com.tinylabproductions.TLPLib.Tween.Behaviours {
+  class ColorTween : BaseTween {
+    public Color from = Color.white;
+    public Color to = Color.white;
+    [Serializable] public class ColorEvent : UnityEvent<Color> { }
+    public ColorEvent onChange;
+
+    public override GoTweenConfig config(GoTweenConfig cfg) {
+      return cfg.floatCallbackProp(f => onChange.Invoke(Color.Lerp(from, to, f)));
+    }
+  }
+}
