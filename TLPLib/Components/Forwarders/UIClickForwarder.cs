1463698724e467006234bd646255621fa68c7486
Arturas Slajus <arturas@tinylabproductions.com>
2015-01-19 17:46:02 +0200

    UIBehaviour/GameObject#uiClick

--- TLPLib/Components/Forwarders/UIClickForwarder.cs
+++ TLPLib/Components/Forwarders/UIClickForwarder.cs
@@ -1,13 +1,12 @@
-﻿using com.tinylabproductions.TLPLib.Annotations;
-using com.tinylabproductions.TLPLib.Functional;
+﻿using com.tinylabproductions.TLPLib.Functional;
 using com.tinylabproductions.TLPLib.Reactive;
-using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace com.tinylabproductions.TLPLib.Components {
   public class UIClickForwarder : UIBehaviour, IPointerClickHandler {
     private readonly Subject<Unit> _onClick = new Subject<Unit>();
     public IObservable<Unit> onClick { get { return _onClick; } }
+
     public void OnPointerClick(PointerEventData eventData) {
       if (eventData.button == PointerEventData.InputButton.Left && IsActive())
         _onClick.push(F.unit);
---
+++ TLPLib/Extensions/UIExts.cs
@@ -0,0 +1,15 @@
+﻿using com.tinylabproductions.TLPLib.Components;
+using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class UIExts {
+    public static IObservable<Unit> uiClick(this UIBehaviour elem) 
+    { return elem.gameObject.uiClick(); }
+
+    public static IObservable<Unit> uiClick(this GameObject go) 
+    { return go.EnsureComponent<UIClickForwarder>().onClick; }
+  }
+}
