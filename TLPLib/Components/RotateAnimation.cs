91439882556e291c3a5446f56e4a230b14a99964
Evaldas Ciakas <evaldas757@gmail.com>
2015-03-16 10:18:30 +0200

    RotateAnimation.reset()

--- TLPLib/Components/RotateAnimation.cs
+++ TLPLib/Components/RotateAnimation.cs
@@ -1,12 +1,20 @@
-﻿using com.tinylabproductions.TLPLib.Annotations;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Components {
   public class RotateAnimation : MonoBehaviour {
     public Vector3 rotation;
+    Quaternion initial;
 
-    [UsedImplicitly] void Update() {
+    internal void Start() {
+      initial = transform.localRotation;
+    }
+
+    internal void Update() {
       transform.Rotate(rotation * Time.deltaTime);
+    }
+
+    public void reset() {
+      transform.localRotation = initial;
     }
   }
 }
