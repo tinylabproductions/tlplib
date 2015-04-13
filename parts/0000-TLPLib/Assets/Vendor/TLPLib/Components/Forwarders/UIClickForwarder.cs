1935327a7d2192b990b7c4214122d077e075533c
Evaldas Ciakas <evaldas757@gmail.com>
2015-04-13 16:00:16 +0300

    UIClickForwarder

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Components/Forwarders/UIClickForwarder.cs
@@ -0,0 +1,15 @@
+﻿using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace com.tinylabproductions.TLPLib.Components {
+  public class UIClickForwarder : UIBehaviour, IPointerClickHandler {
+    private readonly Subject<Unit> _onClick = new Subject<Unit>();
+    public IObservable<Unit> onClick { get { return _onClick; } }
+    public void OnPointerClick(PointerEventData eventData) {
+      if (eventData.button == PointerEventData.InputButton.Left && IsActive())
+        _onClick.push(F.unit);
+    }
+  }
+}
