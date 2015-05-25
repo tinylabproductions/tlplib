d8fc02b4ed65940afea34c6104b534c49c9e6185
Evaldas Ciakas <evaldas757@gmail.com>
2015-05-25 11:56:45 +0300

    runOnUIBlocking

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Android/AndroidActivity.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Android/AndroidActivity.cs
@@ -1,5 +1,7 @@
 ﻿#if UNITY_ANDROID
 using System;
+using System.Threading;
+using com.tinylabproductions.TLPLib.Functional;
 using com.tinylabproductions.TLPLib.Logger;
 using UnityEngine;
 
@@ -55,6 +57,48 @@ namespace com.tinylabproductions.TLPLib.Android {
     }
 
     public static void runOnUI(Act act) { current.Call("runOnUiThread", act); }
+
+    public static A runOnUIBlocking<A>(Func<A> f) {
+      return new UIThreadOperation<A>(f).execute();
+    }
+
+    public static void runOnUIBlocking(Act act) {
+      new UIThreadOperation<Unit>(() => { act(); return new Unit(); }).execute();
+    }
+
+  }
+
+  public class UIThreadOperation<A> {
+    readonly AutoResetEvent evt = new AutoResetEvent(false);
+    readonly int timeoutMs;
+    readonly Func<A> func;
+
+    Exception completedException;
+    A result;
+
+    public UIThreadOperation(Func<A> func, int timeoutMs = 1000) {
+      this.func = func;
+      this.timeoutMs = timeoutMs;
+    }
+
+    public A execute() {
+      AndroidActivity.runOnUI(onUI);
+      evt.WaitOne(timeoutMs);
+      if (completedException != null) throw completedException;
+      return result;
+    }
+
+    void onUI() {
+      try {
+        result = func();
+      }
+      catch (Exception e) {
+        completedException = e;
+      }
+      finally {
+        evt.Set();
+      }
+    }
   }
 }
 #endif
