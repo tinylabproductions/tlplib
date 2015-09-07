8fc0eea6d3bb2f3a59873d4254ba98be74202d96
Evaldas Ciakas <evaldas757@gmail.com>
2015-09-07 17:49:33 +0300

    Retry.cs

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Retry/Retry.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using com.tinylabproductions.TLPLib.Concurrent;
+
+namespace com.tinylabproductions.TLPLib.Retry {
+  public class Retry<A> {
+    readonly int retryCount;
+    readonly float retryDelay;
+    readonly Act tryAction;
+    readonly Act<A> failAction;
+    int retries;
+
+    public Retry(int retryCount, float retryDelay, Act tryAction, Act<A> failAction) {
+      this.retryCount = retryCount;
+      this.retryDelay = retryDelay;
+      this.tryAction = tryAction;
+      this.failAction = failAction;
+    }
+
+    public void start() {
+      tryAction();
+    }
+
+    public void failure(Fn<A> result) {
+      if (retries < retryCount) {
+        retries++;
+        ASync.WithDelay(retryDelay, tryAction);
+      }
+      else {
+        failAction(result());
+      }
+    }
+  }
+}
