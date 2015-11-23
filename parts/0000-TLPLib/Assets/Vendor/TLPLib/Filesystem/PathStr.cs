ce93d88c066ef999db21b6c4ed61923fa91c108c
Arturas Slajus <arturas@tinylabproductions.com>
2015-11-23 15:24:58 +0200

    PathStr

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Filesystem/PathStr.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace com.tinylabproductions.TLPLib.Filesystem {
+  public struct PathStr {
+    public readonly string path;
+
+    public PathStr(string path) { this.path = path; }
+
+    public static PathStr operator /(PathStr s1, string s2) {
+      return new PathStr(Path.Combine(s1.path, s2));
+    }
+
+    public static implicit operator string(PathStr s) { return s.path; }
+
+    public PathStr dirname => new PathStr(Path.GetDirectoryName(path));
+
+    public override string ToString() { return path; }
+  }
+}
