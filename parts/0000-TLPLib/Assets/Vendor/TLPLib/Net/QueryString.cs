07ea844d4ae13f8102ce3606fd4144a2cc926674
Arturas Slajus <arturas@tinylabproductions.com>
2015-12-11 11:07:57 +0200

    Net.QueryString

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Net/QueryString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.tinylabproductions.TLPLib.Net {
+  public class QueryString {
+    public static string build(IEnumerable<Tpl<string, string>> qsParams) {
+      var sb = new StringBuilder();
+      var isFirst = true;
+      foreach (var qsParam in qsParams) {
+        if (isFirst) isFirst = false;
+        else sb.Append("&");
+        sb.Append(Uri.EscapeDataString(qsParam._1));
+        sb.Append("=");
+        sb.Append(Uri.EscapeDataString(qsParam._2));
+      }
+      return sb.ToString();
+    }
+  }
+}
