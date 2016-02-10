75bdd94ca6da7dc4f18cbcb87cd5f81b936a94cb
Arturas Slajus <arturas@tinylabproductions.com>
2016-02-10 18:45:42 +0200

    ScreenUtils.Size is replaced by Size

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Utilities/ScreenUtils.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Utilities/ScreenUtils.cs
@@ -1,57 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using com.tinylabproductions.TLPLib.Reactive;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Utilities {
   public static class ScreenUtils {
-    public struct Size : IEquatable<Size> {
-      public readonly int width, height;
-
-      public Size(int width, int height) {
-        this.width = width;
-        this.height = height;
-      }
-
-      #region Equality
-
-      public bool Equals(Size other) {
-        return width == other.width && height == other.height;
-      }
-
-      public override bool Equals(object obj) {
-        if (ReferenceEquals(null, obj)) return false;
-        return obj is Size && Equals((Size) obj);
-      }
-
-      public override int GetHashCode() {
-        unchecked { return (width * 397) ^ height; }
-      }
-
-      public static bool operator ==(Size left, Size right) { return left.Equals(right); }
-      public static bool operator !=(Size left, Size right) { return !left.Equals(right); }
-
-      sealed class WidthHeightEqualityComparer : IEqualityComparer<Size> {
-        public bool Equals(Size x, Size y) {
-          return x.width == y.width && x.height == y.height;
-        }
-
-        public int GetHashCode(Size obj) {
-          unchecked { return (obj.width * 397) ^ obj.height; }
-        }
-      }
-
-      static readonly IEqualityComparer<Size> WidthHeightComparerInstance = new WidthHeightEqualityComparer();
-
-      public static IEqualityComparer<Size> widthHeightComparer {
-        get { return WidthHeightComparerInstance; }
-      }
-
-      #endregion
-
-      public override string ToString() { return string.Format("ScreenSize[width: {0}, height: {1}]", width, height); }
-    }
-
     private static float sw { get { return Screen.width; } }
     private static float sh { get { return Screen.height; } }
 
