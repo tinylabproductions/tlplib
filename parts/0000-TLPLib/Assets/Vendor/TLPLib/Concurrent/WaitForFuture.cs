90c93a0991250d64b47a3f0607a3bd25baf00f70
Arturas Slajus <arturas@tinylabproductions.com>
2016-03-18 18:50:27 +0200

    WaitForFuture

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Concurrent/WaitForFuture.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Concurrent {
+  public static class WaitForFuture {
+    public static WaitForFuture<A> a<A>(Future<A> f) { return new WaitForFuture<A>(f); }
+    public static WaitForFuture<A> coroutineWait<A>(this Future<A> f) { return new WaitForFuture<A>(f); }
+  }
+
+  public class WaitForFuture<A> : CustomYieldInstruction {
+    public readonly Future<A> future;
+
+    public WaitForFuture(Future<A> future) { this.future = future; }
+
+    public override bool keepWaiting => future.value.isEmpty;
+  }
+}
