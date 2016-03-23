1047c4e467556112a418bc47edc15eddb7d7511d
Arturas Slajus <arturas@tinylabproductions.com>
2016-03-23 10:46:19 +0200

    Extensions & Logger for testing

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/TestExts.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace com.tinylabproductions.TLPLib.Test {
+  public static class TestExts {
+    public static void shouldBeTrue(this bool b, string message = null) {
+      Assert.True(b, message);
+    }
+
+    public static void shouldBeFalse(this bool b, string message = null) {
+      Assert.False(b, message);
+    }
+
+    public static void shouldEqual<A>(this A a, A expected, string message=null) {
+      Assert.AreEqual(expected, a, message);
+    }
+  }
+}
---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/TestLogger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using com.tinylabproductions.TLPLib.Logger;
+
+namespace com.tinylabproductions.TLPLib.Test {
+  public class TestLogger : LogBase {
+    public readonly List<string> 
+      debugMsgs = new List<string>(), 
+      infoMsgs = new List<string>(),
+      warnMsgs = new List<string>(),
+      errorMsgs = new List<string>();
+
+    public TestLogger() {
+      level = Log.Level.DEBUG; 
+    }
+
+    public void clear() {
+      debugMsgs.Clear();
+      infoMsgs.Clear();
+      warnMsgs.Clear();
+      errorMsgs.Clear();
+    }
+
+    protected override void logDebug(string s) { debugMsgs.Add(s); }
+    protected override void logInfo(string s) { infoMsgs.Add(s); }
+    protected override void logWarn(string s) { warnMsgs.Add(s); }
+    protected override void logError(string s) { errorMsgs.Add(s); }
+  }
+}
