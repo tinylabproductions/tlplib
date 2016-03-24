a3f7470b26875f25954c1c12cd871dc394bf4163
Astro75 <evaldas757@gmail.com>
2016-03-24 15:27:20 +0200

    SceneExts

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/SceneExts.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Vendor.TLPLib.Extensions {
+  public static class SceneExts {
+    // includes inactive objects
+    // may not work on scene awake
+    // http://forum.unity3d.com/threads/bug-getrootgameobjects-is-not-working-in-awake.379317/
+    public static IEnumerable<T> findObjectsOfTypeAll<T>(this Scene scene) where T : UnityEngine.Object {
+      return scene.GetRootGameObjects().SelectMany(o => o.GetComponentsInChildren<T>(true));
+    }
+  }
+}
