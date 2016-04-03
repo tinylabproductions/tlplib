f7a8e364139ebe0f1d7f899b151a2bc3d77e2c59
Arturas Slajus <x11@arturaz.net>
2016-04-03 22:53:34 +0300

    OneOf<A, B, C> - like Either but for 3 options.

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/OneOf.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using com.tinylabproductions.TLPLib.Extensions;
+
+namespace com.tinylabproductions.TLPLib.Functional {
+  static class OneOf {
+    public enum Choice { A, B, C }
+  }
+
+  public struct OneOf<A, B, C> {
+    readonly A _aValue;
+    readonly B _bValue;
+    readonly C _cValue;
+    readonly OneOf.Choice whichOne;
+
+    public OneOf(A a) {
+      _aValue = a;
+      _bValue = default(B);
+      _cValue = default(C);
+      whichOne = OneOf.Choice.A;
+    }
+
+    public OneOf(B b) {
+      _aValue = default(A);
+      _bValue = b;
+      _cValue = default(C);
+      whichOne = OneOf.Choice.B;
+    }
+
+    public OneOf(C c) {
+      _aValue = default(A);
+      _bValue = default(B);
+      _cValue = c;
+      whichOne = OneOf.Choice.C;
+    }
+
+    public bool isA => whichOne == OneOf.Choice.A;
+    public Option<A> aValue => isA.opt(_aValue);
+    internal A __unsafeGetA => _aValue;
+
+    public bool isB => whichOne == OneOf.Choice.B;
+    public Option<B> bValue => isB.opt(_bValue);
+    internal B __unsafeGetB => _bValue;
+
+    public bool isC => whichOne == OneOf.Choice.C;
+    public Option<C> cValue => isC.opt(_cValue);
+    internal C __unsafeGetC => _cValue;
+    
+    public void voidFold(Act<A> onA, Act<B> onB, Act<C> onC) {
+      switch (whichOne) {
+        case OneOf.Choice.A:
+          onA(_aValue);
+          break;
+        case OneOf.Choice.B:
+          onB(_bValue);
+          break;
+        case OneOf.Choice.C:
+          onC(_cValue);
+          break;
+      }
+      throw new IllegalStateException("Unreachable code");
+    }
+
+    public R fold<R>(Fn<A, R> onA, Fn<B, R> onB, Fn<C, R> onC) {
+      switch (whichOne) {
+        case OneOf.Choice.A: return onA(_aValue);
+        case OneOf.Choice.B: return onB(_bValue);
+        case OneOf.Choice.C: return onC(_cValue);
+      }
+      throw new IllegalStateException("Unreachable code");
+    }
+  }
+}
