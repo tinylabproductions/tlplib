038b71f2b2969ba55d3e250ac19d371d42e3b290
Arturas Slajus <arturas@tinylabproductions.com>
2016-04-05 11:40:48 +0300

    EditorUtils.getSceneObjects

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Utils/EditorUtils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace com.tinylabproductions.TLPLib.Editor.Utils {
+  public static class EditorUtils {
+    public static IEnumerable<GameObject> getSceneObjects() {
+      return Resources.FindObjectsOfTypeAll<GameObject>()
+          .Where(go => string.IsNullOrEmpty(AssetDatabase.GetAssetPath(go))
+                 && go.hideFlags == HideFlags.None);
+    }
+  }
+}
