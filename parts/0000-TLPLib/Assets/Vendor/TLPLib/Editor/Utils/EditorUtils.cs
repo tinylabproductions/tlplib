25e2a9528bc4dee9686f735eaa1bc2057a7bc410
Arturas Slajus <arturas@tinylabproductions.com>
2016-04-05 13:03:07 +0300

    EditorUtils.getFileID

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Utils/EditorUtils.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Utils/EditorUtils.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEditor;
 
 namespace com.tinylabproductions.TLPLib.Editor.Utils {
@@ -9,6 +10,20 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
       return Resources.FindObjectsOfTypeAll<GameObject>()
           .Where(go => string.IsNullOrEmpty(AssetDatabase.GetAssetPath(go))
                  && go.hideFlags == HideFlags.None);
+    }
+
+    // http://forum.unity3d.com/threads/how-to-get-the-local-identifier-in-file-for-scene-objects.265686/
+    public static int getFileID(Object unityObject) {
+      var inspectorModeInfo =
+        typeof(SerializedObject).GetProperty("inspectorMode", BindingFlags.NonPublic | BindingFlags.Instance);
+
+      var serializedObject = new SerializedObject(unityObject);
+      inspectorModeInfo.SetValue(serializedObject, InspectorMode.Debug, null);
+
+      var localIdProp =
+          serializedObject.FindProperty("m_LocalIdentfierInFile");   //note the misspelling!
+
+      return localIdProp.intValue;
     }
   }
 }
