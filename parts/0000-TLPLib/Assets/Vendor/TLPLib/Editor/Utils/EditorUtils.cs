fd50845a49ae8fde9cdb0868fa9bfc30a30100c4
Arturas Slajus <arturas@tinylabproductions.com>
2016-04-05 13:57:57 +0300

    EditorUtils.getFileID: updated implementation for unity 5.

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Utils/EditorUtils.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Utils/EditorUtils.cs
@@ -12,18 +12,31 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
                  && go.hideFlags == HideFlags.None);
     }
 
-    // http://forum.unity3d.com/threads/how-to-get-the-local-identifier-in-file-for-scene-objects.265686/
-    public static int getFileID(Object unityObject) {
-      var inspectorModeInfo =
-        typeof(SerializedObject).GetProperty("inspectorMode", BindingFlags.NonPublic | BindingFlags.Instance);
+    static PropertyInfo cachedInspectorModeInfo;
+
+    // https://gist.github.com/DmitriyYukhanov/feb182871a14e355ac38
+    public static long getFileID(Object unityObject) {
+      var id = -1L;
+      if (unityObject == null) return id;
+
+      if (cachedInspectorModeInfo == null)
+        cachedInspectorModeInfo = typeof(SerializedObject).GetProperty(
+          "inspectorMode", BindingFlags.NonPublic | BindingFlags.Instance
+        );
 
       var serializedObject = new SerializedObject(unityObject);
-      inspectorModeInfo.SetValue(serializedObject, InspectorMode.Debug, null);
+      cachedInspectorModeInfo.SetValue(serializedObject, InspectorMode.Debug, null);
+      var serializedProperty = serializedObject.FindProperty("m_LocalIdentfierInFile");
+			id = serializedProperty.longValue;
+      if (id <= 0) {
+        var prefabType = PrefabUtility.GetPrefabType(unityObject);
+        id = prefabType != PrefabType.None 
+          ? getFileID(PrefabUtility.GetPrefabObject(unityObject))
+          // this will work for the new objects in scene which weren't saved yet
+          : unityObject.GetInstanceID();
+      }
 
-      var localIdProp =
-          serializedObject.FindProperty("m_LocalIdentfierInFile");   //note the misspelling!
-
-      return localIdProp.intValue;
+      return id;
     }
   }
 }
