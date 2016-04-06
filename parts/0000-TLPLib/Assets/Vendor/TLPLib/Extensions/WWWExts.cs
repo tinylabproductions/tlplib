3075e5fc222b13fd5e475c4f97e31e8cba527a31
Arturas Slajus <arturas@tinylabproductions.com>
2016-04-06 18:59:08 +0300

    WWWExts

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/WWWExts.cs
@@ -0,0 +1,13 @@
+﻿using com.tinylabproductions.TLPLib.Functional;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class WWWExts {
+    public static Either<WWWError, WWW> toEither(this WWW www) {
+      return 
+        string.IsNullOrEmpty(www.error)
+        ? F.right<WWWError, WWW>(www)
+        : F.left<WWWError, WWW>(new WWWError(www));
+    }
+  }
+}
