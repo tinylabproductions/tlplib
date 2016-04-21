35c357289003f2977b21c9b59d1a43986dcb872d
Arturas Slajus <arturas@tinylabproductions.com>
2016-04-21 14:04:27 +0300

    UnityEngine.Object#path()

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/UnityObjectExts.cs
@@ -0,0 +1,15 @@
+﻿using com.tinylabproductions.TLPLib.Filesystem;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Assets.Vendor.TLPLib.Extensions {
+  public static class UnityObjectExts {
+#if UNITY_EDITOR
+    public static PathStr path(this Object obj) {
+      return new PathStr(AssetDatabase.GetAssetPath(obj));
+    }
+#endif
+  }
+}
