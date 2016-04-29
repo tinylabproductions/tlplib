90a6f9af27cfc42f4950d31ce3ef8b21b10d492b
Tadas Valutis <tadas@valutis.lt>
2016-04-29 13:57:59 +0300

    RuntimePlatform2.all

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Platform/RuntimePlatform2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Platform {
+  public static class RuntimePlatform2 {
+    public static readonly ReadOnlyCollection<RuntimePlatform> all = new ReadOnlyCollection<RuntimePlatform>(
+      new[] {
+        RuntimePlatform.WindowsEditor, RuntimePlatform.WindowsPlayer,
+        RuntimePlatform.OSXEditor, RuntimePlatform.OSXPlayer,
+        RuntimePlatform.LinuxPlayer,
+        RuntimePlatform.Android, RuntimePlatform.IPhonePlayer, RuntimePlatform.OSXWebPlayer,
+        RuntimePlatform.OSXDashboardPlayer, RuntimePlatform.WindowsWebPlayer, RuntimePlatform.PS3,
+        RuntimePlatform.XBOX360,
+        RuntimePlatform.Android,
+        RuntimePlatform.LinuxPlayer,
+        RuntimePlatform.WebGLPlayer,
+        RuntimePlatform.WSAPlayerX86,
+        RuntimePlatform.WSAPlayerX64,
+        RuntimePlatform.WSAPlayerARM,
+        RuntimePlatform.WP8Player,
+        RuntimePlatform.BlackBerryPlayer,
+        RuntimePlatform.TizenPlayer,
+        RuntimePlatform.PSP2,
+        RuntimePlatform.PS4,
+        RuntimePlatform.PSM,
+        RuntimePlatform.XboxOne,
+        RuntimePlatform.SamsungTVPlayer,
+        RuntimePlatform.WiiU,
+        RuntimePlatform.tvOS,
+      }
+    );
+  }
+}
