9699707ac1fbf97c763ccc76eca4471b7f94dad0
Tadas Valutis <tadas@valutis.lt>
2016-05-04 11:23:27 +0300

    IDictionary#getOrUpdate

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Extensions/IDictionaryExtsTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using com.tinylabproductions.TLPLib.Test;
+using NUnit.Framework;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  [TestFixture]
+  public class IDictionaryExtsTestGetOrUpdate {
+    [Test]
+    public void Test() {
+      var dictionary = new Dictionary<int,string>();
+      dictionary.getOrUpdate(1, () => "one").shouldEqual("one");
+      dictionary.getOrUpdate(1, () => "two").shouldEqual("one");
+    }
+  }
+}
