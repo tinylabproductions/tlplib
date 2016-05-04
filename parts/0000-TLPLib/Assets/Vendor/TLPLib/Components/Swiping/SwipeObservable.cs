4fda8b99c3b94380b0ad0185e5ea1b29ff6582be
Tadas Valutis <tadas@valutis.lt>
2016-05-04 11:21:44 +0300

    SwipeObservable

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Components/Swiping/SwipeObservable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace com.tinylabproductions.TLPLib.Components.Swiping {
+  public struct SwipeData {
+    public readonly Vector2 direction;
+  }
+
+  public interface ISwipeEventSource {
+  }
+
+  public class SwipeObservable : Observable<SwipeData> {
+    float distanceThreshold;
+    ISwipeEventSource eventSource;
+    
+
+    public SwipeObservable(float distanceThreshold, ISwipeEventSource eventSource) {
+      this.distanceThreshold = distanceThreshold;
+      this.eventSource = eventSource;
+    }
+  }
+
+  
+}
---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Components/Swiping/UpdateEventSource.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.tinylabproductions.TLPLib.Components.Swiping {
+  class UpdateEventSource {
+  }
+}
