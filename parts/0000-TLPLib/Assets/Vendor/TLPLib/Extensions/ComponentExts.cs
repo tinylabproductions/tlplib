2bd9caeabdaa0b807747456382b3d24c872ad722
Astro75 <evaldas757@gmail.com>
2016-05-06 17:02:42 +0300

    ComponentExts.clone improved.

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/ComponentExts.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/ComponentExts.cs
@@ -3,14 +3,14 @@
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class ComponentExts {
     public static A clone<A>(
-      this A self, Vector3? position=null, Quaternion? rotation=null, 
+      this A self, Vector3? position=null, Quaternion? rotation=null,
       Transform parent=null
     ) where A : Component {
-      var cloned = Object.Instantiate(self.gameObject);
+      var cloned = Object.Instantiate(self);
       if (position != null) cloned.transform.position = (Vector3) position;
       if (rotation != null) cloned.transform.rotation = (Quaternion) rotation;
-      if (parent != null) cloned.transform.parent = parent;
-      return cloned.GetComponent<A>();
+      if (parent != null) cloned.transform.SetParent(parent, false);
+      return cloned;
     }
   }
 }
