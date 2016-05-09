40e2f0ef646efcd7fdbfb1e73fcf1fdb4747c998
Arturas Slajus <arturas@tinylabproductions.com>
2016-05-09 18:05:43 +0300

    UnityEngine.Object#recordEditorChanges

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Utilities/EditorUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace com.tinylabproductions.TLPLib.Utilities {
+  public static class EditorUtils {
+    [Conditional("UNITY_EDITOR")]
+    public static void recordEditorChanges(this Object o, string name) {
+      Undo.RecordObject(o, name);
+      EditorUtility.SetDirty(o);
+    }
+  }
+}
