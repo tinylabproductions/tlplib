6419b27a3bcc2b7fac2bcd53bc6fe3d238dfd992
Arturas Slajus <arturas@tinylabproductions.com>
2016-05-09 18:05:50 +0300

    FieldValidations class

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Components/Validation/FieldValidations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AdvancedInspector;
+using com.tinylabproductions.TLPLib.Extensions;
+
+namespace com.tinylabproductions.TLPLib.Components.Validation {
+  public static class FieldValidations {
+    public static HelpItem strHelp(Fn<string> get, int maxLength, bool isRequired) {
+      var value = get() ?? "";
+      if (isRequired && value.Trim().isEmpty())
+        return new HelpItem(HelpType.Error, "This field is required.");
+      else if (value.Length >= maxLength)
+        return new HelpItem(HelpType.Info, $"Max length = {maxLength}");
+      else
+        return new HelpItem(HelpType.None, "");
+    }
+  }
+}
