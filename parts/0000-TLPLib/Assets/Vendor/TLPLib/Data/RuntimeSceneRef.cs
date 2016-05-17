b03fd7b343a6d5b5e10fe384d65acd52e118c5f5
Astro75 <evaldas757@gmail.com>
2016-05-17 11:40:16 +0300

    RuntimeSceneRef

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Data/RuntimeSceneRef.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AdvancedInspector;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace com.tinylabproductions.TLPLib.Data {
+  [Serializable]
+  [AdvancedInspector(true)]
+  public class RuntimeSceneRef {
+    [SerializeField] public Object scene;
+
+    [SerializeField, HideInInspector] string _sceneName;
+
+    public string sceneName => Application.isEditor ? (scene ? scene.name : "") : _sceneName;
+
+#if UNITY_EDITOR
+    public void prepareForRuntime() {
+      _sceneName = scene.name;
+    }
+#endif
+  }
+}
