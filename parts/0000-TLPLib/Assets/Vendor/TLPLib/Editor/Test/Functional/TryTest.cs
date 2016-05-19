afb1988d00036e6c844798d298227ea426c783c6
Arturas Slajus <arturas@tinylabproductions.com>
2016-05-19 09:34:16 +0300

    Try#map should catch exceptions in the mapping function.

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Functional/TryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Test;
+using NUnit.Framework;
+
+namespace com.tinylabproductions.TLPLib.Editor.Test.Functional {
+  class TestException : Exception {}
+
+  public class TryTestMap {
+    static readonly TestException ex = new TestException();
+
+    [Test]
+    public void ErrorToGood() {
+      new Try<int>(ex).map(a => a * 2).shouldBeError(ex.GetType());
+    }
+
+    [Test]
+    public void ErrorToError() {
+      new Try<int>(ex).map<int>(a => { throw new Exception(); }).shouldBeError(ex.GetType());
+    }
+
+    [Test]
+    public void GoodToGood() {
+      new Try<int>(1).map(a => a * 2).shouldBeSuccess(2);
+    }
+
+    [Test]
+    public void GoodToError() {
+      new Try<int>(1).map<int>(a => { throw ex; }).shouldBeError(ex.GetType());
+    }
+  }
+}
--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/Try.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/Try.cs
@@ -44,13 +44,14 @@ namespace com.tinylabproductions.TLPLib.Functional {
       if (isSuccess) onValue(_value); else onException(_exception);
     }
 
-    public Try<B> map<B>(Fn<A, B> onValue) 
-    { return flatMap(a => F.scs(onValue(a))); }
+    public Try<B> map<B>(Fn<A, B> onValue) {
+      return flatMap(a => F.doTry(() => onValue(a)));
+    }
 
-    public Try<B> flatMap<B>(Fn<A, Try<B>> onValue) 
+    public Try<B> flatMap<B>(Fn<A, Try<B>> onValue)
     { return isSuccess ? onValue(_value) : F.err<B>(_exception); }
 
-    public A getOrThrow 
+    public A getOrThrow
       { get { return isSuccess ? _value : F.throws<A>(_exception); } }
 
     public override string ToString() {
