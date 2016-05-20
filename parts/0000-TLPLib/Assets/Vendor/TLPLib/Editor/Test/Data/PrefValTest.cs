50686cb1aa919e9a55649b764f9c2867f9681ad2
Arturas Slajus <arturas@tinylabproductions.com>
2016-05-20 18:04:53 +0300

    PrefVal: test whether default value is persisted upon PrefVal creation

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Data/PrefValTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using com.tinylabproductions.TLPLib.Test;
+using NUnit.Framework;
+using Random = UnityEngine.Random;
+
+namespace com.tinylabproductions.TLPLib.Data {
+  class PrefValTestDefaultValueStorage {
+    [Test]
+    public void ItShouldStoreDefaultValueUponCreation() {
+      var key = $"test-{DateTime.Now}";
+      var p1 = PrefVal.player.integer(key, Random.Range(0, 100));
+      var p2 = PrefVal.player.integer(key, p1.value + 1);
+      p2.value.shouldEqual(p1.value);
+    }
+  }
+}
