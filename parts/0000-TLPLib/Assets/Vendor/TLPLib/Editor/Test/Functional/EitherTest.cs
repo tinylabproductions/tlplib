1e39dd6ceb2ef55b1c86a823e0b71f53ba31cf90
Arturas Slajus <arturas@tinylabproductions.com>
2016-05-27 18:34:09 +0300

    IEnumerable<Either<ImmutableList<A>, B>>#sequence

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Functional/EitherTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Immutable;
+using com.tinylabproductions.TLPLib.Test;
+using NUnit.Framework;
+
+namespace com.tinylabproductions.TLPLib.Functional {
+  public class EitherTest {
+    [Test]
+    public void WhenHasOneError() {
+      var l = ImmutableList.Create("error");
+      new[] {
+        Either<ImmutableList<string>, int>.Left(l),
+        Either<ImmutableList<string>, int>.Right(4)
+      }.sequence().shouldBeLeftEnum(l);
+    }
+
+    [Test]
+    public void WhenHasMultipleErrors() {
+      new[] {
+        Either<ImmutableList<string>, int>.Left(ImmutableList.Create("error")),
+        Either<ImmutableList<string>, int>.Left(ImmutableList.Create("error2"))
+      }.sequence().shouldBeLeftEnum(ImmutableList.Create("error", "error2"));
+    }
+
+    [Test]
+    public void WhenHasNoErrors() {
+      new[] {
+        Either<ImmutableList<string>, int>.Right(3),
+        Either<ImmutableList<string>, int>.Right(4)
+      }.sequence().shouldBeRightEnum(ImmutableList.Create(3, 4));
+    }
+  }
+}
---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/Either_Validation2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace com.tinylabproductions.TLPLib.Functional {
+  // Non-generated methods for validation.
+  public static class Either_Validation2 {
+    public static Either<ImmutableList<A>, ImmutableList<B>> sequence<A, B>(
+      this IEnumerable<Either<ImmutableList<A>, B>> eithers
+    ) {
+      var errors = ImmutableList<A>.Empty;
+      var result = ImmutableList<B>.Empty;
+      foreach (var either in eithers) {
+        foreach (var errs in either.leftValue) errors = errors.AddRange(errs);
+        if (errors.IsEmpty) {
+          // No point in accumulating result if we have at least one error.
+          foreach (var b in either.rightValue) result = result.Add(b);
+        }
+      }
+      return errors.IsEmpty
+        ? Either<ImmutableList<A>, ImmutableList<B>>.Right(result)
+        : Either<ImmutableList<A>, ImmutableList<B>>.Left(errors);
+    }
+  }
+}
