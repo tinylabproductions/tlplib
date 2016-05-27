12e06a55653fe095848b733e548ef31440ec0eba
Arturas Slajus <arturas@tinylabproductions.com>
2016-05-27 18:32:51 +0300

    Lambda

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Lambda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace com.tinylabproductions.TLPLib {
+  public static class Lambda {
+    class Comparer<A> : IComparer<A> {
+      readonly Fn<A, A, int> comparer;
+
+      public Comparer(Fn<A, A, int> comparer) { this.comparer = comparer; }
+
+      public int Compare(A x, A y) { return comparer(x, y); }
+    }
+
+    public static IComparer<A> comparer<A>(Fn<A, A, int> comparer) {
+      return new Comparer<A>(comparer);
+    }
+  }
+}
