ddec4c3040c178e7df5c29e7a312ecf6c86592a5
Arturas Slajus <arturas@tinylabproductions.com>
2016-05-30 20:11:14 +0300

    A#validationSuccess, A#stringValidationSuccess

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/Either_Validation2.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/Either_Validation2.cs
@@ -4,6 +4,13 @@ using System.Collections.Immutable;
 namespace com.tinylabproductions.TLPLib.Functional {
   // Non-generated methods for validation.
   public static class Either_Validation2 {
+    public static Either<ImmutableList<A>, B> validationSuccess<A, B>(this B b) {
+      return Either<ImmutableList<A>, B>.Right(b);
+    }
+
+    public static Either<ImmutableList<string>, A> stringValidationSuccess<A>(this A b)
+      { return b.validationSuccess<string, A>(); }
+
     public static Either<ImmutableList<A>, ImmutableList<B>> sequence<A, B>(
       this IEnumerable<Either<ImmutableList<A>, B>> eithers
     ) {
