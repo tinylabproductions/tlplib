3e5aea08b42bacc9cf39a806464b525dad33eb35
Arturas Slajus <arturas@tinylabproductions.com>
2016-06-07 18:55:47 +0300

    ASyncOperation#toFuture

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/ASyncOperationExts.cs
@@ -0,0 +1,9 @@
+﻿using com.tinylabproductions.TLPLib.Concurrent;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class ASyncOperationExts {
+    public static Future<AsyncOperation> toFuture(this AsyncOperation op)
+      { return Future.fromBusyLoop(() => op.isDone.opt(op)); }
+  }
+}
