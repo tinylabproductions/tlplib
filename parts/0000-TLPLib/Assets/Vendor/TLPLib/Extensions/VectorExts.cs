6495a7950481aafe3d32971f37a35f8d502657c1
Arturas Slajus <arturas@tinylabproductions.com>
2016-06-17 19:11:17 +0300

    Vector2 & 3 # withXXX methods

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/VectorExts.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/VectorExts.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class VectorExts {
+    public static Vector2 withX(this Vector2 v, float x) => new Vector2(x, v.y);
+    public static Vector2 withY(this Vector2 v, float y) => new Vector2(v.x, y);
+
+    public static Vector3 withX(this Vector3 v, float x) => new Vector3(x, v.y, v.z);
+    public static Vector2 withY(this Vector3 v, float y) => new Vector3(v.x, y, v.z);
+    public static Vector3 withZ(this Vector3 v, float z) => new Vector3(v.x, v.y, z);
+
     public static Vector2 with2(
       this Vector2 v,
 #if UNITY_IOS
