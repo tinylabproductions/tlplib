e92b10aa59caebbd8f9bd38bb59c4a5571e6c0a6
Karolis <karolisjc@gmail.com>
2016-06-30 17:26:55 +0300

    Cleanup for code preprocessor.

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Editor/Utils/CodePreprocessorTest.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Editor/Utils/CodePreprocessorTest.cs
@@ -1,5 +1,8 @@
-﻿using com.tinylabproductions.TLPLib.Test;
+﻿using System.Collections.Immutable;
+using com.tinylabproductions.TLPLib.Filesystem;
+using com.tinylabproductions.TLPLib.Test;
 using NUnit.Framework;
+using System.IO;
 
 namespace com.tinylabproductions.TLPLib.Editor.Utils {
   public abstract class CodePreprocessorTestBase {
@@ -49,49 +52,56 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
     }
   }
   
-  public class CodePreprocessorTestGetFilePaths : CodePreprocessorTestBase
-  {
+  public class CodePreprocessorTestGetFilePaths : CodePreprocessorTestBase {
+    readonly PathStr p_rootPath, p_emptyDir, p_noCsFilesDir, p_noneCsFile;
+    readonly PathStr p_cs1, p_cs2, p_cs3, p_cs4;
+
+    public CodePreprocessorTestGetFilePaths() {
+      p_rootPath = new PathStr(Path.GetTempPath()) / "CodeProcessorTest";
+      var dirPath1 = new PathStr(Directory.CreateDirectory(p_rootPath / "TestDir1").FullName);
+      var dirPath2 = new PathStr(Directory.CreateDirectory(dirPath1 / "TestDir2").FullName);
+      p_emptyDir = new PathStr(Directory.CreateDirectory(dirPath1 / "TestDirEmpty").FullName);
+      p_noCsFilesDir = new PathStr(Directory.CreateDirectory(dirPath1 / "TestDirNoCs").FullName);
+      p_cs1 = createFile(p_rootPath / "testCs1.cs");
+      p_cs2 = createFile(dirPath2 / "testCs2.cs");
+      p_cs3 = createFile(dirPath1 / "testCs3.cs");
+      p_cs4 = createFile(dirPath1 / "testCs4.cs");
+      p_noneCsFile = createFile(new PathStr(p_noCsFilesDir / "testTxt1.txt"));
+    }
+
+    static PathStr createFile(PathStr path) {
+      File.Create(path).Close();
+      return path;
+    }
 
     [Test]
     public void WhenManySubdirectories() {
-      var dir = "Assets/Vendor/TLPLib/Editor/Test/Editor/Utils/CodePreprocessorTemp/";
-      var actual = CodePreprocessor.getFilePaths(dir, "*.cs");
-      if (actual.isEmpty) Assert.Fail();
-      else actual.get.Length.shouldEqual(4);
+      var actual = CodePreprocessor.getFilePaths(p_rootPath, "*.cs").rightValue.get.ToImmutableHashSet();
+      actual.shouldEqual(ImmutableHashSet.Create(p_cs1, p_cs2, p_cs3, p_cs4));
     }
 
     [Test]
-    public void WhenEmptyDir()
-    {
-      var dir = "Assets/Vendor/TLPLib/Editor/Test/Editor/Utils/CodePreprocessorTemp/Test1/TestEmpty";
-      var actual = CodePreprocessor.getFilePaths(dir, "*.cs");
-      actual.shouldBeNone();
+    public void WhenEmptyDir(){
+      var actual = CodePreprocessor.getFilePaths(p_emptyDir, "*.cs");
+      actual.leftValue.isDefined.shouldBeTrue();
     }
 
     [Test]
-    public void WhenDirWithNoCsFiles()
-    {
-      var dir = "Assets/Vendor/TLPLib/Editor/Test/Editor/Utils/CodePreprocessorTemp/Test1/TestDirWithNoCs";
-      var actual = CodePreprocessor.getFilePaths(dir, "*.cs");
-      actual.shouldBeNone();
+    public void WhenDirWithNoCsFiles(){
+      var actual = CodePreprocessor.getFilePaths(p_noCsFilesDir, "*.cs");
+      actual.leftValue.isDefined.shouldBeTrue();
     }
 
     [Test]
-    public void WhenCsFile()
-    {
-      var dir = "Assets/Vendor/TLPLib/Editor/Test/Editor/Utils/CodePreprocessorTemp/Test1/NewBehaviourScript32434.cs";
-      var actual = CodePreprocessor.getFilePaths(dir, "*.cs");
-      if (actual.isEmpty) Assert.Fail();
-      else actual.get.Length.shouldEqual(1);
+    public void WhenCsFile(){
+      var actual = CodePreprocessor.getFilePaths(p_cs1, "*.cs").rightValue.get.ToImmutableHashSet(); 
+      actual.shouldEqual(ImmutableHashSet.Create(p_cs1));
     }
 
     [Test]
-    public void WhenNoneCsFile()
-    {
-      var dir = "Assets/Vendor/TLPLib/Editor/Test/Editor/Utils/CodePreprocessorTemp/Test1/TestDirWithNoCs/TestJs2.js";
-      var actual = CodePreprocessor.getFilePaths(dir, "*.cs");
-      actual.shouldBeNone();
+    public void WhenNoneCsFile(){
+      var actual = CodePreprocessor.getFilePaths(p_noneCsFile, "*.cs");
+      actual.leftValue.isDefined.shouldBeTrue();
     }
   }
-
 }
--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Utils/CodePreprocessor.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Utils/CodePreprocessor.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using UnityEngine;
 using System.IO;
+using System.Linq;
 using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Filesystem;
 using com.tinylabproductions.TLPLib.Functional;
 using JetBrains.Annotations;
 using UnityEditor;
@@ -10,34 +12,36 @@ using UnityEditor;
 namespace com.tinylabproductions.TLPLib.Editor.Utils {
   public class CodePreprocessor : MonoBehaviour {
     public const string PRAG_STR = "#pragma warning disable\n";
-    static Option<string> selectedPath => AssetDatabase.GetAssetPath(Selection.activeObject).nonEmptyOpt();
+    static Option<PathStr> selectedPath => 
+      AssetDatabase.GetAssetPath(Selection.activeObject).nonEmptyOpt().map(PathStr.a);
 
-    [UsedImplicitly, MenuItem("Assets/Code processor/Disable Warnings/Enable")]
+    [UsedImplicitly, MenuItem("Assets/Code Processor/Disable Warnings/Enable")]
     static void addPragmas() => enablePragmas(true);
 
-    [UsedImplicitly, MenuItem("Assets/Code processor/Disable Warnings/Disable")]
+    [UsedImplicitly, MenuItem("Assets/Code Processor/Disable Warnings/Disable")]
     static void removePragmas() => enablePragmas(false);
-
-    static void enablePragmas(bool add) {
-      var rootPath = selectedPath;
-      if (!showDialog(add, rootPath.get))
-        return;
-      if (rootPath.isEmpty) {
-        EditorUtility.DisplayDialog("Error", "Not a valid path.", "OK");
-        return;
-      }
-      var paths = getFilePaths(rootPath.get, "*.cs");
-      if (paths.isEmpty)
-      {
-        EditorUtility.DisplayDialog("Error", "No '*.cs' files selected.", "OK");
-        return;
-      }
-      foreach (var path in paths.get) {
-        processFile(path, add);
-      }
-      EditorUtility.DisplayDialog("Success", "File processing done.", "OK");
-    }
     
+    static void enablePragmas(bool add) {
+      selectedPath.voidFold(
+        () => EditorUtility.DisplayDialog("Error", "Not a valid path.", "OK"),
+        rootPath => {
+          if (askForConfirmation(add, rootPath)) {
+            getFilePaths(rootPath, "*.cs").voidFold(
+              err => EditorUtility.DisplayDialog("Error", err, "OK"),
+              paths => {
+                processFiles(paths, add);
+                EditorUtility.DisplayDialog("Success", "File processing done.", "OK");
+              }
+            );
+          }
+        }
+      );
+    }
+
+    static void processFiles(IEnumerable<PathStr> paths, bool add) {
+      foreach (var path in paths) processFile(path, add);
+    }
+
     public static void processFile(string path, bool add) {
       var text = File.ReadAllText(path);
       var editedText = add ? checkAndWritePragmaInFront(text) : removePragmaFromFront(text);
@@ -54,17 +58,19 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
 
     public static bool hasPragmaInFront(string text) => text.StartsWith(PRAG_STR);
 
-    public static Option<ImmutableArray<string>> getFilePaths(string rootPath, string fileExt) {
-      if (Directory.Exists(rootPath)) {
-        var paths = Directory.GetFiles(rootPath, fileExt, SearchOption.AllDirectories).ToImmutableArray();
-        return paths.Length > 0 ? new Option<ImmutableArray<string>>(paths) : new Option<ImmutableArray<string>>();
-      }
-      if (!string.Equals($"*{Path.GetExtension(rootPath)}",fileExt)) return new Option<ImmutableArray<string>>();
-      var pth = ImmutableArray.Create(rootPath);
-      return new Option<ImmutableArray<string>>(pth);
+    public static Either<string, ImmutableArray<PathStr>> getFilePaths(PathStr rootPath, string fileExt) {
+      if (!Directory.Exists(rootPath))
+        return (string.Equals($"*{rootPath.extension}", fileExt)).either(
+          "Not a '*.cs' file.",
+          () => ImmutableArray.Create(new PathStr(rootPath.path))
+        );
+      var paths = 
+        Directory.GetFiles(rootPath, fileExt, SearchOption.AllDirectories)
+        .Select(PathStr.a).ToImmutableArray();
+      return (paths.Length > 0).either("No '*.cs' files in directory.", () => paths);
     }
 
-    static bool showDialog(bool add, string path) {
+    static bool askForConfirmation(bool add, string path) {
       var str = add ? "add" : "remove";
       var accepted = EditorUtility.DisplayDialog(
         "Warning", $"Do you want to {str} disable in following path?\n{path}", "Yes", "No"
