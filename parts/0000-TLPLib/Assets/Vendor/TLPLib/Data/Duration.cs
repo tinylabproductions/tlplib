f98ef9b55c928d9bb8e2b5471719edb83327305f
Arturas Slajus <arturas@tinylabproductions.com>
2016-07-05 19:51:21 +0300

    Extension methods for Duration.

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Data/Duration.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Data/Duration.cs
@@ -2,12 +2,33 @@
   public struct Duration {
     public readonly int millis;
 
-    public static Duration fromSeconds(float seconds) => Duration.fromSeconds((int) (seconds * 1000));
+    public static Duration fromSeconds(int seconds) => new Duration(seconds * 1000);
+    public static Duration fromSeconds(float seconds) => new Duration((int) (seconds * 1000));
 
     public Duration(int millis) { this.millis = millis; }
 
     public float seconds => millis / 1000f;
 
     public override string ToString() => $"{nameof(Duration)}({millis}ms)";
+  }
+
+  public static class DurationExts {
+    public static Duration milli(this int v) => v.millis();
+    public static Duration millis(this int v) => new Duration(v);
+
+    public static Duration second(this int v) => v.seconds();
+    public static Duration second(this float v) => v.seconds();
+    public static Duration seconds(this int v) => Duration.fromSeconds(v);
+    public static Duration seconds(this float v) => Duration.fromSeconds(v);
+
+    public static Duration minute(this int v) => v.minutes();
+    public static Duration minute(this float v) => v.minutes();
+    public static Duration minutes(this int v) => Duration.fromSeconds(v * 60);
+    public static Duration minutes(this float v) => Duration.fromSeconds(v * 60);
+
+    public static Duration hour(this int v) => v.hours();
+    public static Duration hour(this float v) => v.hours();
+    public static Duration hours(this int v) => Duration.fromSeconds(v * 3600);
+    public static Duration hours(this float v) => Duration.fromSeconds(v * 3600);
   }
 }
