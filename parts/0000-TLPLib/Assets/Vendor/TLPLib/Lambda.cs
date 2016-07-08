fefb163745ee76240f2e3b94c59cafdfc1bc58a8
Arturas Slajus <arturas@tinylabproductions.com>
2016-07-08 16:23:11 +0300

    CompareResult & Lambda.comparer

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Lambda.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Lambda.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 
 namespace com.tinylabproductions.TLPLib {
+  public enum CompareResult : sbyte { LT = -1, EQ = 0, GT = 1 }
+
+  public static class CompareResultExts {
+    public static CompareResult asCmpRes(this int result) =>
+        result < 0 ? CompareResult.LT 
+      : result == 0 ? CompareResult.EQ 
+      : CompareResult.GT;
+  }
+
   public static class Lambda {
     class Comparer<A> : IComparer<A> {
-      readonly Fn<A, A, int> comparer;
+      readonly Fn<A, A, CompareResult> comparer;
 
-      public Comparer(Fn<A, A, int> comparer) { this.comparer = comparer; }
+      public Comparer(Fn<A, A, CompareResult> comparer) { this.comparer = comparer; }
 
-      public int Compare(A x, A y) { return comparer(x, y); }
+      public int Compare(A x, A y) => (int) comparer(x, y);
     }
 
-    public static IComparer<A> comparer<A>(Fn<A, A, int> comparer) {
-      return new Comparer<A>(comparer);
-    }
+    public static IComparer<A> comparer<A>(Fn<A, A, CompareResult> comparer) => 
+      new Comparer<A>(comparer);
   }
 }
