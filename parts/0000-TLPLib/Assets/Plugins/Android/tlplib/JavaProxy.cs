e8153d0e42b3eefb8d5efcca5bd419b6ae7bd61c
Karolis <karolisjc@gmail.com>
2016-07-14 16:25:27 +0300

    added JavaProxy.cs

---
+++ parts/0000-TLPLib/Assets/Plugins/Android/tlplib/JavaProxy.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_ANDROID
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Android {
+  public class JavaProxy : AndroidJavaProxy {
+    public JavaProxy(string javaInterface) : base(javaInterface) {}
+    public JavaProxy(AndroidJavaClass javaInterface) : base(javaInterface) {}
+
+    /* May be called from Java side. */
+    public string toString() { return ToString(); }
+    public int hashCode() { return GetHashCode(); }
+    public bool equals(object o) { return this == o; }
+  }
+}
+#endif
