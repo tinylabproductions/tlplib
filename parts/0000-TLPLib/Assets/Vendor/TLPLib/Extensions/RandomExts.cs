29fafc4d23770e4e90daaad04795274c347f4e03
Tadas Valutis <tadas@valutis.lt>
2016-07-15 15:04:23 +0300

    Extended System.Random to have nextFloat(upperBound [inclusive], lowerBound [inclusive])

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/RandomExts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assets.Vendor.TLPLib.Extensions {
+  public static class RandomExts {
+
+    public static float nextFloat(this Random random, float upperBound) { return nextFloat(random, 0, upperBound); }
+
+    public static float nextFloat(this Random random, float lowerBound, float upperBound) {
+      return (float) (lowerBound + random.NextDouble()*(upperBound - lowerBound));
+    }
+
+
+  }
+}
