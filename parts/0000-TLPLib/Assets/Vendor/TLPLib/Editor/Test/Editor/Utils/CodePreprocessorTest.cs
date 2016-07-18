efea992a772f24c50d3cae2aee8b0c69048ee3cd
Karolis <karolisjc@gmail.com>
2016-07-18 18:23:10 +0300

    refactored

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Editor/Utils/CodePreprocessorTest.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Editor/Utils/CodePreprocessorTest.cs
@@ -1,14 +1,12 @@
 ﻿using System.Collections.Immutable;
-using com.tinylabproductions.TLPLib.Filesystem;
 using com.tinylabproductions.TLPLib.Test;
 using NUnit.Framework;
-using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using com.tinylabproductions.TLPLib.Extensions;
 
 namespace com.tinylabproductions.TLPLib.Editor.Utils {
   public abstract class CodePreprocessorTestBase {
-
     public const string CODE =
       @"#if PART_UNITYADS
 #if UNITY_ANDROID
@@ -52,152 +50,87 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
   }
 
   public class CodePreprocessorTestWritingPragma : CodePreprocessorTestBase {
-
     [Test]
-    public void whenHasPragma() {
-      var lines = Regex.Split(CODE_WITH_PRAGMA, "\r\n|\r|\n").ToImmutableArray();
+    public void WhenHasPragma() {
+      var lines = Regex.Split(CODE_WITH_PRAGMA, "\r\n|\r|\n").ToImmutableList();
       var actual = string.Join("\n", CodePreprocessor.checkAndWritePragma(lines).ToArray());
       actual.shouldEqual(CODE_WITH_PRAGMA);
     }
 
     [Test]
-    public void whenDoesntHavePragma() {
-      var lines = Regex.Split(CODE, "\r\n|\r|\n").ToImmutableArray();
+    public void WhenDoesntHavePragma() {
+      var lines = Regex.Split(CODE, "\r\n|\r|\n").ToImmutableList();
       var actual = string.Join("\n", CodePreprocessor.checkAndWritePragma(lines).ToArray());
       actual.shouldEqual(CODE_WITH_PRAGMA);
     }
 
     [Test]
-    public void whenHasPragmaInTheWrongPlace() {
-      var lines = Regex.Split(CODE_WITH_PRAGMA_WRONG_PLACE, "\r\n|\r|\n").ToImmutableArray();
+    public void WhenHasPragmaInTheWrongPlace() {
+      var lines = Regex.Split(CODE_WITH_PRAGMA_WRONG_PLACE, "\r\n|\r|\n").ToImmutableList();
       var actual = string.Join("\n", CodePreprocessor.checkAndWritePragma(lines).ToArray());
       actual.shouldEqual(CODE_WITH_PRAGMA);
     }
   }
 
   public class CodePreprocessorTestRemovingPragma : CodePreprocessorTestBase {
+    [Test]
+    public void WhenHasPragma() {
+      var lines = Regex.Split(CODE_WITH_PRAGMA, "\r\n|\r|\n").ToImmutableList();
+      CodePreprocessor.checkAndRemovePragma(lines).mkString("\n").shouldEqual(CODE);
+    }
 
     [Test]
-    public void whenHasPragma() {
-      var lines = Regex.Split(CODE_WITH_PRAGMA, "\r\n|\r|\n").ToImmutableArray();
+    public void WhenDoesntHavePragma() {
+      var lines = Regex.Split(CODE, "\r\n|\r|\n").ToImmutableList();
       var actual = string.Join("\n", CodePreprocessor.checkAndRemovePragma(lines).ToArray());
       actual.shouldEqual(CODE);
     }
 
     [Test]
-    public void whenDoesntHavePragma() {
-      var lines = Regex.Split(CODE, "\r\n|\r|\n").ToImmutableArray();
-      var actual = string.Join("\n", CodePreprocessor.checkAndRemovePragma(lines).ToArray());
-      actual.shouldEqual(CODE);
-    }
-
-    [Test]
-    public void whenHasPragmaInTheWrongPlace() {
-      var lines = Regex.Split(CODE_WITH_PRAGMA_WRONG_PLACE, "\r\n|\r|\n").ToImmutableArray();
+    public void WhenHasPragmaInTheWrongPlace() {
+      var lines = Regex.Split(CODE_WITH_PRAGMA_WRONG_PLACE, "\r\n|\r|\n").ToImmutableList();
       var actual = string.Join("\n", CodePreprocessor.checkAndRemovePragma(lines).ToArray());
       actual.shouldEqual(CODE);
     }
   }
 
   public class CodePreprocessorTestGetLastDirectiveIndex : CodePreprocessorTestBase {
-
     [Test]
-    public void whenHasPragma() {
-      var lines = Regex.Split(CODE_WITH_PRAGMA, "\r\n|\r|\n").ToImmutableArray();
-      var actual = CodePreprocessor.getLastDirectiveIndex(lines);
-      actual.get.shouldEqual(2);
+    public void WhenHasPragma() {
+      var lines = Regex.Split(CODE_WITH_PRAGMA, "\r\n|\r|\n").ToImmutableList();
+      lines.getLastDirectiveIndex().shouldBeSome(2);
     }
 
     [Test]
-    public void whenDoesntHavePragma() {
-      var lines = Regex.Split(CODE, "\r\n|\r|\n").ToImmutableArray();
-      var actual = CodePreprocessor.getLastDirectiveIndex(lines);
-      actual.get.shouldEqual(1);
+    public void WhenDoesntHavePragma() {
+      var lines = Regex.Split(CODE, "\r\n|\r|\n").ToImmutableList();
+      lines.getLastDirectiveIndex().shouldBeSome(1);
     }
 
     [Test]
-    public void whenHasPragmaInTheWrongPlace() {
-      var lines = Regex.Split(CODE_WITH_PRAGMA_WRONG_PLACE, "\r\n|\r|\n").ToImmutableArray();
-      var actual = CodePreprocessor.getLastDirectiveIndex(lines);
-      actual.get.shouldEqual(2);
+    public void WhenHasPragmaInTheWrongPlace() {
+      var lines = Regex.Split(CODE_WITH_PRAGMA_WRONG_PLACE, "\r\n|\r|\n").ToImmutableList();
+      lines.getLastDirectiveIndex().shouldBeSome(2);
     }
   }
 
   public class CodePreprocessorTestPragmaLineNumber : CodePreprocessorTestBase {
-
     [Test]
-    public void whenHasPragma() {
-      var lines = Regex.Split(CODE_WITH_PRAGMA, "\r\n|\r|\n").ToImmutableArray();
-      var actual = CodePreprocessor.pragmaLineNumber(lines, 5);
-      actual.get.shouldEqual(2);
+    public void WhenHasPragma() {
+      var lines = Regex.Split(CODE_WITH_PRAGMA, "\r\n|\r|\n").ToImmutableList();
+      lines.pragmaLineNumber().shouldBeSome(2);
     }
 
     [Test]
-    public void whenDoesntHavePragma() {
-      var lines = Regex.Split(CODE, "\r\n|\r|\n").ToImmutableArray();
-      var actual = CodePreprocessor.pragmaLineNumber(lines, 5);
-      actual.shouldBeNone();
+    public void WhenDoesntHavePragma() {
+      var lines = Regex.Split(CODE, "\r\n|\r|\n").ToImmutableList();
+      lines.pragmaLineNumber().shouldBeNone();
     }
 
     [Test]
-    public void whenHasPragmaInTheWrongPlace() {
-      var lines = Regex.Split(CODE_WITH_PRAGMA_WRONG_PLACE, "\r\n|\r|\n").ToImmutableArray();
-      var actual = CodePreprocessor.pragmaLineNumber(lines, 5);
-      actual.get.shouldEqual(1);
-    }
-  }
-
-
-  public class CodePreprocessorTestGetFilePaths : CodePreprocessorTestBase {
-    readonly PathStr p_rootPath, p_emptyDir, p_noCsFilesDir, p_noneCsFile;
-    readonly PathStr p_cs1, p_cs2, p_cs3, p_cs4;
-
-    public CodePreprocessorTestGetFilePaths() {
-      p_rootPath = new PathStr(Path.GetTempPath()) / "CodeProcessorTest";
-      var dirPath1 = new PathStr(Directory.CreateDirectory(p_rootPath / "TestDir1").FullName);
-      var dirPath2 = new PathStr(Directory.CreateDirectory(dirPath1 / "TestDir2").FullName);
-      p_emptyDir = new PathStr(Directory.CreateDirectory(dirPath1 / "TestDirEmpty").FullName);
-      p_noCsFilesDir = new PathStr(Directory.CreateDirectory(dirPath1 / "TestDirNoCs").FullName);
-      p_cs1 = createFile(p_rootPath / "testCs1.cs");
-      p_cs2 = createFile(dirPath2 / "testCs2.cs");
-      p_cs3 = createFile(dirPath1 / "testCs3.cs");
-      p_cs4 = createFile(dirPath1 / "testCs4.cs");
-      p_noneCsFile = createFile(new PathStr(p_noCsFilesDir / "testTxt1.txt"));
-    }
-
-    static PathStr createFile(PathStr path) {
-      File.Create(path).Close();
-      return path;
-    }
-
-    [Test]
-    public void whenManySubdirectories() {
-      var actual = CodePreprocessor.getFilePaths(p_rootPath, "*.cs").rightValue.get.ToImmutableHashSet();
-      actual.shouldEqual(ImmutableHashSet.Create(p_cs1, p_cs2, p_cs3, p_cs4));
-    }
-
-    [Test]
-    public void whenEmptyDir() {
-      var actual = CodePreprocessor.getFilePaths(p_emptyDir, "*.cs");
-      actual.leftValue.isDefined.shouldBeTrue();
-    }
-
-    [Test]
-    public void whenDirWithNoCsFiles() {
-      var actual = CodePreprocessor.getFilePaths(p_noCsFilesDir, "*.cs");
-      actual.leftValue.isDefined.shouldBeTrue();
-    }
-
-    [Test]
-    public void whenCsFile() {
-      var actual = CodePreprocessor.getFilePaths(p_cs1, "*.cs").rightValue.get.ToImmutableHashSet();
-      actual.shouldEqual(ImmutableHashSet.Create(p_cs1));
-    }
-
-    [Test]
-    public void whenNoneCsFile() {
-      var actual = CodePreprocessor.getFilePaths(p_noneCsFile, "*.cs");
-      actual.leftValue.isDefined.shouldBeTrue();
+    public void WhenHasPragmaInTheWrongPlace() {
+      var lines = Regex.Split(CODE_WITH_PRAGMA_WRONG_PLACE, "\r\n|\r|\n").ToImmutableList();
+      lines.pragmaLineNumber().shouldBeSome(1);
     }
   }
 }
