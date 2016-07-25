96c955d71185a1f25e55ea110a61bf08f4512cd2
Arturas Slajus <arturas@tinylabproductions.com>
2016-07-25 12:15:13 +0300

    QueryString.parseKV

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Net/QueryString.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Net/QueryString.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Text;
+using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Functional;
 
 namespace com.tinylabproductions.TLPLib.Net {
   public class QueryString {
+    static readonly char[] equalsSeparator = {'='};
+
     public static string build(IEnumerable<Tpl<string, string>> qsParams) {
       var sb = new StringBuilder();
       var isFirst = true;
@@ -15,6 +21,19 @@ namespace com.tinylabproductions.TLPLib.Net {
         sb.Append(Uri.EscapeDataString(qsParam._2));
       }
       return sb.ToString();
+    }
+
+    public static ImmutableList<Tpl<string, string>> parseKV(string str) {
+      if (str.isEmpty()) return ImmutableList<Tpl<string, string>>.Empty;
+
+      var parts = str.Split('&');
+      var list = parts.Select(part => {
+        var kv = part.Split(equalsSeparator, 2);
+        var key = Uri.UnescapeDataString(kv[0]);
+        var value = kv.Length == 2 ? Uri.UnescapeDataString(kv[1]) : "";
+        return F.t(key, value);
+      }).ToImmutableList();
+      return list;
     }
   }
 }
