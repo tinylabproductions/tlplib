f9755abe05e8b05b0ff6630d1b711e3c534905fc
Arturas Slajus <arturas@tinylabproductions.com>
2016-07-26 17:52:41 +0300

    A[]#ensureNotNull

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Utilities/ArrayUtilsTest.cs
@@ -0,0 +1,22 @@
+﻿using com.tinylabproductions.TLPLib.Test;
+using NUnit.Framework;
+
+namespace com.tinylabproductions.TLPLib.Utilities {
+  public class ArrayUtilsTestEnsureNotNull {
+    [Test]
+    public void WhenNotNull() {
+      var original = new[] {"foo"};
+      var actual = original.ensureNotNull();
+      (original == actual).shouldBeTrue("be same object");
+    }
+
+    [Test]
+    public void WhenNull() {
+      var actualStr = ((string[]) null).ensureNotNull();
+      actualStr.shouldEqual(new string[0]);
+
+      var actualInt = ((int[]) null).ensureNotNull();
+      actualInt.shouldEqual(new int[0]);
+    }
+  }
+}
