3271aba0eccb5b0cf36178eb04541b20e59c7a44
Arturas Slajus <arturas@tinylabproductions.com>
2016-08-08 19:06:55 +0300

    Update prefval tests

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Data/PrefValTest.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Data/PrefValTest.cs
@@ -4,10 +4,21 @@ using NUnit.Framework;
 using Random = UnityEngine.Random;
 
 namespace com.tinylabproductions.TLPLib.Data {
+  class PrefValTestRamStorage {
+    [Test]
+    public void ItShouldUpdateTheValueInRam() {
+      var key = $"{nameof(ItShouldUpdateTheValueInRam)}-{DateTime.Now.Ticks}";
+      var p = PrefVal.player.integer(key, 100);
+      p.value.shouldEqual(100);
+      p.value = 200;
+      p.value.shouldEqual(200);
+    }
+  }
+
   class PrefValTestDefaultValueStorage {
     [Test]
     public void ItShouldStoreDefaultValueUponCreation() {
-      var key = $"test-{DateTime.Now}";
+      var key = $"{nameof(ItShouldStoreDefaultValueUponCreation)}-{DateTime.Now.Ticks}";
       var p1 = PrefVal.player.integer(key, Random.Range(0, 100));
       var p2 = PrefVal.player.integer(key, p1.value + 1);
       p2.value.shouldEqual(p1.value);
@@ -15,7 +26,7 @@ namespace com.tinylabproductions.TLPLib.Data {
 
     [Test]
     public void ItShouldPersistDefaultValueToPrefs() {
-      var key = $"test-{DateTime.Now}";
+      var key = $"{nameof(ItShouldPersistDefaultValueToPrefs)}-{DateTime.Now.Ticks}";
       var p1 = PrefVal.player.integer(key, default(int));
       var p2 = PrefVal.player.integer(key, 10);
       p2.value.shouldEqual(p1.value);
