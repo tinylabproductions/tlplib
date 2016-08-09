b10d7cb30600ca84d6a043f0a9d1d007d968d891
Arturas Slajus <arturas@tinylabproductions.com>
2016-08-09 11:09:56 +0300

    Equality for Unit.

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/Unit.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/Unit.cs
@@ -1,6 +1,24 @@
-﻿namespace com.tinylabproductions.TLPLib.Functional {
-  public struct Unit {
+﻿using System;
+
+namespace com.tinylabproductions.TLPLib.Functional {
+  public struct Unit : IEquatable<Unit> {
     public static Unit instance { get; } = new Unit();
-    public override string ToString() { return "()"; }
+    public override string ToString() => "()";
+
+    #region Equality
+
+    public bool Equals(Unit other) => true;
+
+    public override bool Equals(object obj) {
+      if (ReferenceEquals(null, obj)) return false;
+      return obj is Unit && Equals((Unit) obj);
+    }
+
+    public override int GetHashCode() => nameof(Unit).GetHashCode();
+
+    public static bool operator ==(Unit left, Unit right) { return left.Equals(right); }
+    public static bool operator !=(Unit left, Unit right) { return !left.Equals(right); }
+
+    #endregion
   }
 }
