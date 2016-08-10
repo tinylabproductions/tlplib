454d6f8527ce7dd12e1362ae33cd1f4a6745c70f
Karolis <karolisjc@gmail.com>
2016-08-10 19:02:21 +0300

    gradients, outlines

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Utilities/GradientBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Smooth.Pools;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Code.UI {
+  public class GradientBase : BaseMeshEffect {
+
+    public virtual void ModifyVertices(List<UIVertex> vertexList) {
+
+    }
+    public override void ModifyMesh(VertexHelper vh) {
+      if (!this.IsActive()) return;
+      var verts = ListPool<UIVertex>.Instance.Borrow();
+      vh.GetUIVertexStream(verts);
+      ModifyVertices(verts);  // calls the old ModifyVertices which was used on pre 5.2
+      vh.AddUIVertexTriangleStream(verts);
+    }
+  }
+}
