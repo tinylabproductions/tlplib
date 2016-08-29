75aae00bf810dbbef62ed73d2aedbbdfa4080a96
Tadas Valutis <tadas@valutis.lt>
2016-08-29 09:32:24 +0300

    Float extension to round it to int.

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/FloatExts.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace Assets.Vendor.TLPLib.Extensions {
+  public static class FloatExts {
+    public static int toInt(this float number) => Mathf.RoundToInt(number);
+  }
+}
