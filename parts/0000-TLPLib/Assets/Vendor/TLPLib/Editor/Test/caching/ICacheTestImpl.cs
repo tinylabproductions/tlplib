c4e29a9cb8e199a136cb79d41c499d16c759fe8d
Arturas Slajus <arturas@tinylabproductions.com>
2016-09-05 16:55:41 +0300

    ICacheTestImpl: return same blobs for same names

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/caching/ICacheTestImpl.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/caching/ICacheTestImpl.cs
@@ -1,5 +1,12 @@
-﻿namespace com.tinylabproductions.TLPLib.caching {
+﻿using System.Collections.Generic;
+using com.tinylabproductions.TLPLib.Extensions;
+
+namespace com.tinylabproductions.TLPLib.caching {
   public class ICacheTestImpl<A> : ICache<A> {
-    public ICachedBlob<A> blobFor(string name) => new ICachedBlobTestImpl<A>(name);
+    readonly Dictionary<string, ICachedBlobTestImpl<A>> caches = 
+      new Dictionary<string, ICachedBlobTestImpl<A>>();
+
+    public ICachedBlob<A> blobFor(string name) => 
+      caches.getOrUpdate(name, () => new ICachedBlobTestImpl<A>(name));
   }
 }
