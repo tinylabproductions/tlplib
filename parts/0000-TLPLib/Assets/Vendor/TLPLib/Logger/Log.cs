93156390868218ece8d66d31e67255e807fe5b7e
Arturas Slajus <arturas@tinylabproductions.com>
2016-09-08 15:50:14 +0300

    ILog: log thread id, don't double check warning level (user has to check it)

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Logger/Log.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Logger/Log.cs
@@ -2,8 +2,10 @@
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Threading;
 using com.tinylabproductions.TLPLib.Components.DebugConsole;
 using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Threads;
 using com.tinylabproductions.TLPLib.Utilities;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
@@ -28,7 +30,7 @@ namespace com.tinylabproductions.TLPLib.Logger {
     static Log() {
       DConsole.instance.onShow += dc => {
         var r = dc.registrarFor("Default Logger");
-        r.register("level?", () => { Debug.Log($"current level = {defaultLogger.level}"); });
+        r.register("level?", () => defaultLogger.level);
         foreach (var l in EnumUtils.GetValues<Level>())
           r.register($"level={l}", () => defaultLogger.level = l);
       };
@@ -113,26 +115,33 @@ namespace com.tinylabproductions.TLPLib.Logger {
     public bool willLog(Log.Level level) => this.level >= level;
 
     public bool isVerbose => willLog(Log.Level.VERBOSE);
-    public void verbose(object o) { if (isVerbose) logVerbose($"[VERBOSE]> {o}"); }
+    public void verbose(object o) => logVerbose(line("VERBOSE", o));
     protected abstract void logVerbose(string s);
 
     public bool isDebug => willLog(Log.Level.DEBUG);
-    public void debug(object o) { if (isDebug) logDebug($"[DEBUG]> {o}"); }
+    public void debug(object o) => logDebug(line("DEBUG", o));
     protected abstract void logDebug(string s);
 
     public bool isInfo => willLog(Log.Level.INFO);
-    public void info(object o) { if (isInfo) logInfo($"[INFO]> {o}"); }
+    public void info(object o) => logInfo(line("INFO", o));
     protected abstract void logInfo(string s);
 
     public bool isWarn => willLog(Log.Level.WARN);
-    public void warn(object o) { if (isWarn) logWarn($"[WARN]> {o}"); }
+    public void warn(object o) => logWarn(line("WARN", o));
     protected abstract void logWarn(string s);
 
     public bool isError => willLog(Log.Level.ERROR);
-    public void error(Exception ex) { error(Log.exToStr(ex)); }
-    public void error(object o, Exception ex) { error(Log.exToStr(ex, o)); }
-    public void error(object o) { logError($"[ERROR]> {o}"); }
+    public void error(Exception ex) => error(Log.exToStr(ex));
+    public void error(object o, Exception ex) => error(Log.exToStr(ex, o));
+    public void error(object o) => logError(line("ERROR", o));
     protected abstract void logError(string s);
+
+    static string line(string level, object o) => $"[{thread}|{level}]> {o}";
+
+    static string thread { get {
+      var t = Thread.CurrentThread;
+      return t == OnMainThread.mainThread ? "Tm" : $"T{t.ManagedThreadId}";
+    } }
   }
 
   public class UnityLog : LogBase {
--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Threads/OnMainThread.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Threads/OnMainThread.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using com.tinylabproductions.TLPLib.Concurrent;
 using com.tinylabproductions.TLPLib.Logger;
 
@@ -7,10 +8,12 @@ namespace com.tinylabproductions.TLPLib.Threads {
   /* Helper class to queue things from other threads to be ran on the main
    * thread. */
   public class OnMainThread {
-    private static readonly Queue<Action> actions = new Queue<Action>();
+    static readonly Queue<Action> actions = new Queue<Action>();
+    public static readonly Thread mainThread;
 
     /* Initialization. */
     static OnMainThread() {
+      mainThread = Thread.CurrentThread;
       ASync.EveryFrame(onUpdate);
     }
 
@@ -19,8 +22,10 @@ namespace com.tinylabproductions.TLPLib.Threads {
     public static void init() { }
 
     /* Run the given action in the main thread. */
-    public static void run(Action action)
-    { lock (actions) { actions.Enqueue(action); } }
+    public static void run(Action action) {
+      if (Thread.CurrentThread == mainThread) action();
+      else lock (actions) { actions.Enqueue(action); }
+    }
 
     private static bool onUpdate() {
       while (true) {
