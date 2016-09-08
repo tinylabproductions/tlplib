a95d511563c210657f4a02c9b2f34ab1002c4588
Arturas Slajus <arturas@tinylabproductions.com>
2016-09-08 11:03:19 +0300

    byte[]#toUShort

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/ByteArrayExts.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/ByteArrayExts.cs
@@ -7,5 +7,10 @@ namespace com.tinylabproductions.TLPLib.Extensions {
       try { return F.scs(BitConverter.ToInt32(data, startIndex)); }
       catch (Exception e) { return F.err<int>(e); }
     }
+
+    public static Try<ushort> toUShort(this byte[] data, int startIndex = 0) {
+      try { return F.scs(BitConverter.ToUInt16(data, startIndex)); }
+      catch (Exception e) { return F.err<ushort>(e); }
+    }
   }
 }
