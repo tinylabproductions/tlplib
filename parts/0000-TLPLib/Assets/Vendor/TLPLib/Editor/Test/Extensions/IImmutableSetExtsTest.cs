30e08474f5cf24eaa85ea1c0f89b697368a1d48e
Arturas Slajus <arturas@tinylabproductions.com>
2016-09-08 11:03:11 +0300

    ImmutableHashSet<A>#toggle

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Test/Extensions/IImmutableSetExtsTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Immutable;
+using com.tinylabproductions.TLPLib.Test;
+using NUnit.Framework;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public class ImmutableHashSetExtsTestToggle {
+    [Test]
+    public void WhenExists() =>
+      ImmutableHashSet.Create(1, 2, 3).toggle(2).shouldEqual(ImmutableHashSet.Create(1, 3));
+
+    [Test]
+    public void WhenDoesNotExist() =>
+      ImmutableHashSet.Create(1, 3).toggle(2).shouldEqual(ImmutableHashSet.Create(1, 2, 3));
+  }
+}
---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/IImmutableSetExts.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Immutable;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class ImmutableHashSetExts {
+    public static ImmutableHashSet<A> toggle<A>(this ImmutableHashSet<A> hs, A a) =>
+      hs.Contains(a) ? hs.Remove(a) : hs.Add(a);
+  }
+}
