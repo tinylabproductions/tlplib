e3f4d669913d2b1f3307a8c6fdc456a8f98d9a98
Karolis <karolisjc@gmail.com>
2016-09-22 11:33:15 +0300

    bool and particle system exts

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/ParticleSystemExts.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.Vendor.TLPLib.Extensions {
+  public static class ParticleSystemExts {
+    public static void setEmmissionEnabled(this ParticleSystem particleSystem, bool enabled) {
+      var emmission = particleSystem.emission;
+      emmission.enabled = enabled;
+    }
+  }
+}
