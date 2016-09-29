93a837d5aa8f76be1bd0120b226f7754ff944a8a
Arturas Slajus <arturas@tinylabproductions.com>
2016-09-29 18:48:31 +0300

    Several Texture2D related extensions.

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/Texture2DExts.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/Texture2DExts.cs
@@ -10,5 +10,38 @@ namespace com.tinylabproductions.TLPLib.Extensions {
       texture.SetPixels(fillColorArray);
       texture.Apply();
     }
+
+    public static void invert(this Color[] pixels, Color[] pixelsTo=null) {
+      pixelsTo = pixelsTo ?? pixels;
+      for (var idx = 0; idx < pixels.Length; idx++) {
+        var current = pixels[idx];
+        var inverted = new Color(1 - current.r, 1 - current.g, 1 - current.b, current.a);
+        pixelsTo[idx] = inverted;
+      }
+    }
+
+    public static Color[] inverted(this Color[] pixels) {
+      var newPixels = new Color[pixels.Length];
+      pixels.invert(newPixels);
+      return newPixels;
+    }
+
+    public static void invert(this Texture2D texture) {
+      var pixels = texture.GetPixels();
+      pixels.invert();
+      texture.SetPixels(pixels);
+      texture.Apply();
+    }
+
+    public static Texture2D newWithSameAttrs(this Texture2D texture, Color[] pixels=null) {
+      var t = new Texture2D(
+        texture.width, texture.height, texture.format, texture.mipmapCount != 0
+      );
+      if (pixels != null) {
+        t.SetPixels(pixels);
+        t.Apply();
+      }
+      return t;
+    }
   }
 }
