48e8cc6277415abbc28a8fbd67d0097af07dbd07
gyts <gytis.stanciukas@gmail.com>
2016-09-30 12:43:55 +0300

    WIP

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Components/ObjectClone.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class ObjectClone : MonoBehaviour {
+  public Transform parent;
+  public Object prefab;
+}
---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Components/ObjectCloneEditor.cs
@@ -0,0 +1,42 @@
+﻿using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Functional;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor( typeof(ObjectClone) )]
+public class ObjectCloneEditor : Editor {
+  GameObject objectToMoveAround;
+
+  void OnSceneGUI() {
+    var mono = target as ObjectClone;
+    var currentEvent = Event.current;
+
+    if (currentEvent.type == EventType.Layout) { HandleUtility.AddDefaultControl(0); }
+
+    if (currentEvent.control && mono?.prefab) {
+      if (objectToMoveAround == null) {
+        objectToMoveAround = (GameObject)Instantiate(mono.prefab, posInWorld(currentEvent.mousePosition), Quaternion.identity);
+        objectToMoveAround.transform.parent = mono.parent;
+      }
+    }
+    else {
+      if (objectToMoveAround != null) DestroyImmediate(objectToMoveAround);
+    }
+
+    if (currentEvent.type == EventType.MouseUp) {
+      objectToMoveAround = null;
+    }
+
+    if (currentEvent.type == EventType.MouseMove) {
+      var cursorPos = currentEvent.mousePosition;
+      var worldPos = posInWorld(cursorPos);
+      if (objectToMoveAround != null) {
+        var tempPos = worldPos;
+        tempPos.z = 0;
+        objectToMoveAround.transform.position = tempPos;
+      }
+    }
+  }
+
+  Vector3 posInWorld(Vector3 mousePosition) { return HandleUtility.GUIPointToWorldRay(mousePosition).origin; }
+}
