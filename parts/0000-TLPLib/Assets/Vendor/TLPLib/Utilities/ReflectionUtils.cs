c9c2cc1cb08c701541599f2bed224011f2f4c6a0
gyts <gytis.stanciukas@gmail.com>
2016-10-18 10:41:07 +0300

    ReflectionUtils#hasAttribute

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Utilities/ReflectionUtils.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace com.tinylabproductions.TLPLib.Utilities {
+  public static class ReflectionUtils {
+    public static bool hasAttribute<T>(this MemberInfo mi) {
+      return mi.GetCustomAttributes(typeof(T), false).Any();
+    }
+  }
+}
