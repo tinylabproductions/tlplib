8793e3c47a79778655bd85a35d05dd760efe3cf4
gyts <gytis.stanciukas@gmail.com>
2016-10-25 14:11:45 +0300

    FlickerColorOnEnable

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/FlickerColorOnEnable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using com.tinylabproductions.TLPLib.Components.Interfaces;
+using com.tinylabproductions.TLPLib.Extensions;
+using UnityEngine;
+
+namespace com.tinylabproductions.Plugins.Vendor.TLPLib.Utilities {
+  public class FlickerColorOnEnable : MonoBehaviour, IMB_Awake, IMB_OnEnable {
+    public Color flickeringColor;
+    public int ammountOfFlickers = 5;
+    public float flickeringRate = .15f;
+
+    public void Awake() { enabled = false; }
+
+    public void OnEnable() { StartCoroutine(flicker()); }
+
+    IEnumerator flicker() {
+      var opt = gameObject.GetComponentInChildren<SpriteRenderer>().opt();
+      foreach (var op in opt) {
+        var originalColor = op.color;
+
+        for (var i = 0; i < ammountOfFlickers; i++) {
+          setAllSpritesColor(flickeringColor);
+          yield return new WaitForSeconds(flickeringRate);
+
+          setAllSpritesColor(originalColor);
+          yield return new WaitForSeconds(flickeringRate);
+        }
+      }
+      enabled = false;
+    }
+
+    void setAllSpritesColor(Color color) {
+      var sprites = gameObject.GetComponentsInChildren<SpriteRenderer>();
+      foreach (var sprite in sprites) sprite.color = color;
+    }
+  }
+}
