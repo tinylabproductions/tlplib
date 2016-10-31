b18286c4986975ee2df8114b6186fd45c4559df1
Arturas Slajus <arturas@tinylabproductions.com>
2016-10-31 13:01:48 +0200

    StandardCharsets is not available in old androids.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Android/Bindings/java/lang/String.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Android/Bindings/java/lang/String.cs
@@ -8,7 +8,7 @@ namespace com.tinylabproductions.TLPLib.Android.Bindings.java.lang {
     public String(AndroidJavaObject java) : base(java) {}
 
     public String(string s) : this(new AndroidJavaObject(
-      "java.lang.String", Encoding.UTF8.GetBytes(s), StandardCharsets.UTF_8.java
+      "java.lang.String", Encoding.UTF8.GetBytes(s), Charset.UTF_8.java
     )) {}
   }
 }
