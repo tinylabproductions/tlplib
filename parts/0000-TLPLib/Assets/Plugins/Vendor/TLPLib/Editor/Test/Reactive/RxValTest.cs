01ee3eedfa9c34e13cdeb3db1a77d0d42e0c232f
Arturas Slajus <arturas@tinylabproductions.com>
2016-11-03 18:53:45 +0200

    BREAKING: IEnumerable<IRxVal<A>>#firstThat -> #anyThat
    optimized and wrote tests

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Test/Reactive/RxValTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Test;
+using NUnit.Framework;
+
+namespace com.tinylabproductions.TLPLib.Reactive {
+  public class RxValTestFirstThat {
+    [Test]
+    public void WhenEmpty() => 
+      Enumerable.Empty<IRxVal<int>>().anyThat(_ => true).value.shouldBeNone();
+
+    [Test]
+    public void WhenFromSingleItem() {
+      var rx = RxRef.a(3);
+      var rx2 = new[] {rx}.anyThat(i => i % 2 != 0);
+      rx2.value.shouldBeSome(3);
+      rx.value = 2;
+      rx2.value.shouldBeNone();
+      rx.value = 5;
+      rx2.value.shouldBeSome(5);
+    }
+
+    [Test]
+    public void WhenMultipleItems() {
+      Fn<int, bool> predicate = i => i % 2 != 0;
+      Fn<Option<int>, bool> matchPredicate = _ => _.exists(predicate);
+      var rx1 = RxRef.a(3);
+      var rx2 = RxRef.a(4);
+      var dst = new[] {rx1, rx2}.anyThat(predicate);
+      dst.value.shouldMatch(matchPredicate);
+      rx1.value = 2;
+      dst.value.shouldBeNone();
+      rx2.value = 5;
+      dst.value.shouldMatch(matchPredicate);
+      rx1.value = 1;
+      dst.value.shouldMatch(matchPredicate);
+    }
+  }
+
+  public class RxValTestAnyOf {
+    [Test]
+    public void WhenEmptySearchForTrue() =>
+      Enumerable.Empty<IRxVal<bool>>().anyOf(searchFor: true).value.shouldBeFalse();
+
+    [Test]
+    public void WhenEmptySearchForFalse() =>
+      Enumerable.Empty<IRxVal<bool>>().anyOf(searchFor: false).value.shouldBeFalse();
+
+    [Test]
+    public void WhenFromSingleItemSearchForTrue() {
+      var rx = RxRef.a(true);
+      var dst = new[] {rx}.anyOf(searchFor: true);
+      dst.value.shouldBeTrue();
+      rx.value = false;
+      dst.value.shouldBeFalse();
+      rx.value = true;
+      dst.value.shouldBeTrue();
+    }
+
+    [Test]
+    public void WhenFromSingleItemSearchForFalse() {
+      var rx = RxRef.a(false);
+      var dst = new[] {rx}.anyOf(searchFor: false);
+      dst.value.shouldBeTrue();
+      rx.value = true;
+      dst.value.shouldBeFalse();
+      rx.value = false;
+      dst.value.shouldBeTrue();
+    }
+
+    [Test]
+    public void WhenMultipleItemsSearchForTrue() {
+      var rx1 = RxRef.a(true);
+      var rx2 = RxRef.a(false);
+      var dst = new[] {rx1, rx2}.anyOf(searchFor: true);
+      dst.value.shouldBeTrue();
+      rx1.value = false;
+      dst.value.shouldBeFalse();
+      rx2.value = true;
+      dst.value.shouldBeTrue();
+      rx1.value = true;
+      dst.value.shouldBeTrue();
+    }
+
+    [Test]
+    public void WhenMultipleItemsSearchForFalse() {
+      var rx1 = RxRef.a(false);
+      var rx2 = RxRef.a(true);
+      var dst = new[] {rx1, rx2}.anyOf(searchFor: false);
+      dst.value.shouldBeTrue();
+      rx1.value = true;
+      dst.value.shouldBeFalse();
+      rx2.value = false;
+      dst.value.shouldBeTrue();
+      rx1.value = false;
+      dst.value.shouldBeTrue();
+    }
+  }
+}
--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Reactive/RxVal.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Reactive/RxVal.cs
@@ -103,27 +103,34 @@ namespace com.tinylabproductions.TLPLib.Reactive {
       return val;
     }
 
-    // TODO: test
-    /* Returns first value that satisfies the predicate. */
-    public static IRxVal<Option<A>> firstThat<A>(this IEnumerable<IRxVal<A>> vals, Fn<A, bool> predicate) {
+    /* Returns any value that satisfies the predicate. Order is not guaranteed. */
+    public static IRxVal<Option<A>> anyThat<A>(this IEnumerable<IRxVal<A>> vals, Fn<A, bool> predicate) {
       var val = RxRef.a(F.none<A>());
+      var dict = new Dictionary<IRxVal<A>, A>();
 
-      // TODO: this is probably suboptimal.
-      Action rescan = () => {
-        foreach (var rxVal in vals.Where(rxVal => predicate(rxVal.value))) {
-          val.value = F.some(rxVal.value);
-          return;
-        }
-        val.value = F.none<A>();
-      };
+      foreach (var rx in vals)
+        rx.subscribe(a => {
+          var matched = predicate(a);
 
-      subscribeToRescans(vals, rescan);
+          if (matched) {
+            dict[rx] = a;
+            if (val.value.isEmpty) val.value = a.some();
+          }
+          else {
+            dict.Remove(rx);
+            if (val.value.isDefined) {
+              val.value = 
+                dict.isEmpty() 
+                  ? Option<A>.None 
+                  : dict[dict.Keys.First()].some();
+            }
+          }
+        });
       return val;
     }
     
-    // TODO: test
-    public static IRxVal<bool> anyOf(this IEnumerable<IRxVal<bool>> vals, bool searchForTrue=true) => 
-      vals.firstThat(b => searchForTrue ? b : !b).map(_ => _.isDefined);
+    public static IRxVal<bool> anyOf(this IEnumerable<IRxVal<bool>> vals, bool searchFor=true) => 
+      vals.anyThat(b => searchFor ? b : !b).map(_ => _.isDefined);
 
     // TODO: test
     /**
