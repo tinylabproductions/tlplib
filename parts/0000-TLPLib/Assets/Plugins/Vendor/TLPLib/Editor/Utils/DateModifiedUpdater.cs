d6db064d721dc339f2bfe08b18298d5a87b93c05
Karolis <karolisjc@gmail.com>
2016-11-03 13:37:31 +0200

    changes date modified when renamed

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/DateModifiedUpdater.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+// ReSharper disable ParameterTypeCanBeEnumerable.Local
+// ReSharper disable SuggestBaseTypeForParameter
+
+public class DateModifiedUpdater : AssetPostprocessor {
+  // ReSharper disable once UnusedMember.Local
+  static void OnPostprocessAllAssets(
+    string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths
+  ) {
+    foreach (var t in movedAssets) {
+      var filePath = Application.dataPath + t.Substring(6);
+      File.SetLastWriteTime(filePath, DateTime.Now);
+      File.SetLastWriteTime($"{filePath}.meta", DateTime.Now);
+    }
+  }
+}
