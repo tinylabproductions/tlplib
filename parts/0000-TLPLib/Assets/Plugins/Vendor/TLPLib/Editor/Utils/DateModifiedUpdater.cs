73c5ee3f8eb4d6be10f458d3fd955091459ec99d
Karolis <karolisjc@gmail.com>
2016-11-03 13:59:14 +0200

    removed comment

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/DateModifiedUpdater.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/DateModifiedUpdater.cs
@@ -10,7 +10,6 @@ public class DateModifiedUpdater : AssetPostprocessor {
     string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths
   ) {
     foreach (var relativePath in movedAssets) {
-      // Substring is needed to remove "Assets" from the start of the path
       File.SetLastWriteTime(relativePath, DateTime.Now);
       File.SetLastWriteTime($"{relativePath}.meta", DateTime.Now);
     }
