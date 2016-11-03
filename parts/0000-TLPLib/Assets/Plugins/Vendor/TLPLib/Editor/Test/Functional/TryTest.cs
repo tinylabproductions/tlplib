86afffc9f4f3836f4aa0b509b0831aa3d8812247
Arturas Slajus <arturas@tinylabproductions.com>
2016-11-03 18:51:36 +0200

    Try<A> overhaul and test for #flatMap

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Test/Functional/TryTest.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Test/Functional/TryTest.cs
@@ -3,29 +3,45 @@ using com.tinylabproductions.TLPLib.Test;
 using NUnit.Framework;
 
 namespace com.tinylabproductions.TLPLib.Functional {
-  class TestException : Exception {}
+  public class TestException : Exception {}
 
-  public class TryTestMap {
-    static readonly TestException ex = new TestException();
+  public abstract class TryTestBase {
+    public static readonly TestException ex = new TestException();
+  }
 
-    [Test]
-    public void ErrorToGood() {
+  public class TryTestMap : TryTestBase {
+    [Test] public void ErrorToGood() => 
       new Try<int>(ex).map(a => a * 2).shouldBeError(ex.GetType());
-    }
 
-    [Test]
-    public void ErrorToError() {
+    [Test] public void ErrorToError() => 
       new Try<int>(ex).map<int>(a => { throw new Exception(); }).shouldBeError(ex.GetType());
-    }
 
-    [Test]
-    public void GoodToGood() {
+    [Test] public void GoodToGood() => 
       new Try<int>(1).map(a => a * 2).shouldBeSuccess(2);
-    }
 
-    [Test]
-    public void GoodToError() {
+    [Test] public void GoodToError() => 
       new Try<int>(1).map<int>(a => { throw ex; }).shouldBeError(ex.GetType());
-    }
+  }
+
+  public class TryTestFlatMap : TryTestBase {
+    static readonly ArgumentException ex2 = new ArgumentException("arg ex");
+
+    [Test] public void ErrorToGood() =>
+      new Try<int>(ex).flatMap(a => new Try<string>(a.ToString())).shouldBeError(ex.GetType());
+
+    [Test] public void ErrorToError() =>
+      new Try<int>(ex).flatMap(a => new Try<string>(ex2)).shouldBeError(ex.GetType());
+
+    [Test] public void ErrorToExceptionInMapper() =>
+      new Try<int>(ex).flatMap<string>(a => { throw ex2; }).shouldBeError(ex.GetType());
+
+    [Test] public void GoodToGood() =>
+      new Try<int>(1).flatMap(i => new Try<string>(i.ToString())).shouldBeSuccess("1");
+
+    [Test] public void GoodToError() =>
+      new Try<int>(1).flatMap(i => new Try<string>(ex2)).shouldBeError(ex2.GetType());
+
+    [Test] public void GoodToExceptionInMapper() =>
+      new Try<int>(1).flatMap<string>(i => { throw ex2; }).shouldBeError(ex2.GetType());
   }
 }
--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Functional/Try.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Functional/Try.cs
@@ -9,8 +9,8 @@ namespace com.tinylabproductions.TLPLib.Functional {
 #endif
 	Try<A> {
 
-    private readonly A _value;
-    private readonly Exception _exception;
+    readonly A _value;
+    readonly Exception _exception;
 
 #if ENABLE_IL2CPP
 	public Try() {}
@@ -26,38 +26,44 @@ namespace com.tinylabproductions.TLPLib.Functional {
       _exception = ex;
     }
 
-    public bool isSuccess { get { return _exception == null; } }
-    public bool isError { get { return _exception != null; } }
+    public bool isSuccess => _exception == null;
+    public bool isError => _exception != null;
 
     public Option<A> value => isSuccess ? F.some(_value) : F.none<A>();
     public Option<A> toOption => value;
     public Option<Exception> exception => isSuccess ? F.none<Exception>() : F.some(_exception);
 
-    public Either<Exception, A> toEither =>
-      fold(Either<Exception, A>.Right, Either<Exception, A>.Left);
+    public A getOrThrow => isSuccess ? _value : F.throws<A>(_exception);
+    public A __unsafeGet => _value;
+    public Exception __unsafeException => _exception;
 
-    public B fold<B>(Fn<A, B> onValue, Fn<Exception, B> onException) {
-      return isSuccess ? onValue(_value) : onException(_exception);
-    }
+    public Either<Exception, A> toEither =>
+      isSuccess ? Either<Exception, A>.Right(_value) : Either<Exception, A>.Left(_exception);
+
+    public B fold<B>(Fn<A, B> onValue, Fn<Exception, B> onException) => 
+      isSuccess ? onValue(_value) : onException(_exception);
 
     public void voidFold(Act<A> onValue, Act<Exception> onException) {
       if (isSuccess) onValue(_value); else onException(_exception);
     }
 
     public Try<B> map<B>(Fn<A, B> onValue) {
-      return flatMap(a => F.doTry(() => onValue(a)));
+      if (isSuccess) {
+        try { return new Try<B>(onValue(_value)); }
+        catch (Exception e) { return new Try<B>(e); } 
+      }
+      return new Try<B>(_exception);
     }
 
-    public Try<B> flatMap<B>(Fn<A, Try<B>> onValue)
-    { return isSuccess ? onValue(_value) : F.err<B>(_exception); }
-
-    public A getOrThrow => isSuccess ? _value : F.throws<A>(_exception);
-
-    public A __unsafeGet => _value;
-    public Exception __unsafeException => _exception;
-
-    public override string ToString() {
-      return isSuccess ? "Success(" + _value + ")" : "Error(" + _exception + ")";
+    public Try<B> flatMap<B>(Fn<A, Try<B>> onValue) {
+      if (isSuccess) {
+        try { return onValue(_value); }
+        catch (Exception e) { return new Try<B>(e); }
+      }
+      return new Try<B>(_exception);
     }
+
+    public override string ToString() => 
+      isSuccess ? $"Success({_value})" : $"Error({_exception})";
   }
 }
