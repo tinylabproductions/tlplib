7f09251eaa8a277d3bda5bebd2e2122e23f58364
Arturas Slajus <arturas@tinylabproductions.com>
2016-11-03 18:51:58 +0200

    BuildTarget#toRuntimePlatform()

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/BuildTargetExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/BuildTargetExts.cs
@@ -1,9 +1,56 @@
 ﻿#if UNITY_EDITOR
 using System;
 using UnityEditor;
+using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Editor.Extensions {
   public static class BuildTargetExts {
+    public static RuntimePlatform toRuntimePlatform(this BuildTarget t) {
+      switch (t) {
+        case BuildTarget.StandaloneOSXUniversal:
+        case BuildTarget.StandaloneOSXIntel:
+        case BuildTarget.StandaloneOSXIntel64:
+          return RuntimePlatform.OSXPlayer;
+        case BuildTarget.StandaloneWindows:
+        case BuildTarget.StandaloneWindows64:
+          return RuntimePlatform.WindowsPlayer;
+        case BuildTarget.StandaloneLinux64:
+        case BuildTarget.StandaloneLinuxUniversal:
+        case BuildTarget.StandaloneLinux:
+          return RuntimePlatform.LinuxPlayer;
+        case BuildTarget.iOS: return RuntimePlatform.IPhonePlayer;
+        case BuildTarget.PS3: return RuntimePlatform.PS3;
+        case BuildTarget.XBOX360: return RuntimePlatform.XBOX360;
+        case BuildTarget.Android: return RuntimePlatform.Android;
+        case BuildTarget.WebGL: return RuntimePlatform.WebGLPlayer;
+        case BuildTarget.Tizen: return RuntimePlatform.TizenPlayer;
+        case BuildTarget.PSP2: return RuntimePlatform.PSP2;
+        case BuildTarget.PS4: return RuntimePlatform.PS4;
+        case BuildTarget.PSM: return RuntimePlatform.PSM;
+        case BuildTarget.XboxOne: return RuntimePlatform.XboxOne;
+        case BuildTarget.SamsungTV: return RuntimePlatform.SamsungTVPlayer;
+        case BuildTarget.WiiU: return RuntimePlatform.WiiU;
+        case BuildTarget.tvOS: return RuntimePlatform.tvOS;
+#if !UNITY_5_4_OR_NEWER
+        case BuildTarget.WP8Player: return RuntimePlatform.WP8Player;
+        case BuildTarget.BlackBerry: return RuntimePlatform.BlackBerryPlayer;
+        case BuildTarget.StandaloneGLESEmu:
+#endif
+        case BuildTarget.WebPlayer:
+        case BuildTarget.WebPlayerStreamed:
+        case BuildTarget.Nintendo3DS:
+        case BuildTarget.WSAPlayer:
+          throw new ArgumentOutOfRangeException(
+            nameof(t), t, $"Can't convert to {nameof(RuntimePlatform)}"
+          );
+
+        default:
+          throw new ArgumentOutOfRangeException(
+            nameof(t), t, $"Are you using obsolete {nameof(BuildTarget)}?"
+          );
+      }
+    }
+
     public static BuildTargetGroup toGroup(this BuildTarget t) {
       switch (t) {
         case BuildTarget.StandaloneOSXUniversal: return BuildTargetGroup.Standalone;
