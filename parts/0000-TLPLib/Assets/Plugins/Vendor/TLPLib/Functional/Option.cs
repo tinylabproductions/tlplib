fa970d159f2977661f37792c6eb728e37ebff4a0
Arturas Slajus <arturas@tinylabproductions.com>
2016-11-11 16:03:43 +0200

    Made Option<A>#getOrElse an extension method to help with IL2CPP.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Functional/Option.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Functional/Option.cs
@@ -5,16 +5,6 @@ using System.Linq;
 using com.tinylabproductions.TLPLib.Extensions;
 
 namespace com.tinylabproductions.TLPLib.Functional {
-/**
- * Hack to glue-on contravariant type parameter requirements
- *
- * http://stackoverflow.com/questions/1188354/can-i-specify-a-supertype-relation-in-c-sharp-generic-constraints
- *
- * Beware that this causes boxing for value types.
- *
- * Also Mono compiler seems to be a lot happier with generic extension
- * methods, than instance ones.
- **/
 public static class Option {
   /** 
    * Options are classes on iOS and if we use default(Option<A>) as a 
@@ -51,6 +41,25 @@ public static class Option {
 
   public static Option<Base> cast<Child, Base>(this Option<Child> o) where Child : Base
     { return o.isDefined ? F.some((Base) o.get) : F.none<Base>(); }
+
+  /**
+   * getOrElse is written as an extension method to make it easier to use in IL2CPP builds
+   * where it is a class.
+   */
+
+  public static A getOrElse<A>(this Option<A> opt, Fn<A> orElse) {
+#if ENABLE_IL2CPP
+    if (opt == null) return orElse();
+#endif
+    return opt.isDefined ? opt.get : orElse();
+  }
+
+  public static A getOrElse<A>(this Option<A> opt, A orElse) {
+#if ENABLE_IL2CPP
+    if (opt == null) return orElse;
+#endif
+    return opt.isDefined ? opt.get : orElse;
+  }
 }
 
 public
@@ -63,11 +72,11 @@ public
 {
   public static Option<A> None { get; } = new Option<A>();
 
-  private readonly A value;
+  readonly A value;
   public readonly bool isSome;
 
 #if ENABLE_IL2CPP
-  public Option() {}
+  Option() {}
 #endif
 
   public Option(A value) : this() {
@@ -217,14 +226,6 @@ public
 
   public IEnumerable<A> asEnum() {
     return isDefined ? get.Yield() : Enumerable.Empty<A>();
-  }
-
-  public A getOrElse(Fn<A> orElse) {
-    return isDefined ? get : orElse();
-  }
-
-  public A getOrElse(A orElse) {
-    return isDefined ? get : orElse;
   }
 
   public Option<A> createOrTap(Fn<A> ifEmpty, Act<A> ifNonEmpty) {
