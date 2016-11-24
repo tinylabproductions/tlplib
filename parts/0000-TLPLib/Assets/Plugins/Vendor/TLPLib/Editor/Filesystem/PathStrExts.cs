8982b424af1da3fa51a3b6aa1c9aba8fea328367
Arturas Slajus <arturas@tinylabproductions.com>
2016-11-24 11:53:22 +0200

    (Editor) PathStr#toAbsoluteRelativeToProjectDir

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Filesystem/PathStrExts.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Filesystem {
+  public static class PathStrExts {
+    public static PathStr toAbsoluteRelativeToProjectDir(this PathStr path) {
+      var full = path.path.Contains(':') || path.path.StartsWith("/")
+        ? path
+        : Application.dataPath + "/../" + path;
+
+      return new PathStr(Path.GetFullPath(full));
+    }
+  }
+}
