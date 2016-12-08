33b39c3ffd945e1bd7978fb7831c7cc4ab19690c
Astro75 <evaldas757@gmail.com>
2016-12-08 11:51:33 +0200

    DebugDraw.circle

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/DebugDraw.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Utilities {
+  public static class DebugDraw {
+    [Conditional("UNITY_EDITOR")]
+    public static void circle(Vector3 pos, float radius, Color color, float duration, int segments = 20) {
+      var current = pos + new Vector3(radius, 0);
+      var segmentAngle = 2 * Mathf.PI / segments;
+      for (var i = 1; i <= segments; i++) {
+        var next = pos + radius * new Vector3(Mathf.Cos(segmentAngle * i), Mathf.Sin(segmentAngle * i));
+        UnityEngine.Debug.DrawLine(current, next, color, duration);
+        current = next;
+      }
+    }
+  }
+}
