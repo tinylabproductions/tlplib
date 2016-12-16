3e36e27afd50ee0179e26736fff7cb22eb509ddc
Astro75 <evaldas757@gmail.com>
2016-12-16 09:32:08 +0200

    StandardInterstitial#onLoad

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Android/Ads/StandardInterstitial.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Android/Ads/StandardInterstitial.cs
@@ -1,13 +1,22 @@
-﻿using UnityEngine;
+﻿using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Android.Ads {
 #if UNITY_ANDROID
   public class StandardInterstitial {
+    Subject<Unit> _onLoad = new Subject<Unit>();
+
     protected readonly AndroidJavaObject java;
+
+    public IObservable<Unit> onLoad => _onLoad;
 
     public StandardInterstitial(AndroidJavaObject java) { this.java = java; }
 
-    public void load() { java.Call("load"); }
+    public void load() {
+      java.Call("load");
+      _onLoad.push(F.unit);
+    }
     public bool ready => java.Call<bool>("isReady");
     public void show() { java.Call("show"); }
   }
