e8306701eb312e0a34e9f5cbc4e50f2a2de5569f
Arturas Slajus <arturas@tinylabproductions.com>
2016-12-20 15:56:17 +0200

    ImmutableArray<From>#map

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/ImmutableArrayExts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class ImmutableArrayExts {
+    public static ImmutableArray<To> map<From, To>(
+      this ImmutableArray<From> source, Fn<From, To> mapper
+    ) {
+      var target = ImmutableArray.CreateBuilder<To>(source.Length);
+      for (var i = 0; i < source.Length; i++) target.Add(mapper(source[i]));
+      return target.MoveToImmutable();
+    }
+  }
+}
