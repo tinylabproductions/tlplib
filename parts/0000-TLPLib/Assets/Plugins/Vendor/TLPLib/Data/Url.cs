fa57eeff9598f3ec8f3f203d3d0aff2955ba3d31
Arturas Slajus <arturas@tinylabproductions.com>
2016-12-21 18:58:49 +0200

    Url data type

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Data/Url.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using com.tinylabproductions.TLPLib.Data.typeclasses;
+
+namespace com.tinylabproductions.TLPLib.Data {
+  /** Stupid tag on string. Because System.Uri is heavy. */
+  public struct Url : IStr, IEquatable<Url> {
+    public readonly string url;
+
+    public Url(string url) { this.url = url; }
+
+    public override string ToString() => $"{nameof(Url)}({url})";
+
+    #region Equality
+
+    public bool Equals(Url other) {
+      return string.Equals(url, other.url);
+    }
+
+    public override bool Equals(object obj) {
+      if (ReferenceEquals(null, obj)) return false;
+      return obj is Url && Equals((Url) obj);
+    }
+
+    public override int GetHashCode() {
+      return (url != null ? url.GetHashCode() : 0);
+    }
+
+    public static bool operator ==(Url left, Url right) { return left.Equals(right); }
+    public static bool operator !=(Url left, Url right) { return !left.Equals(right); }
+
+    #endregion
+
+    public string asString() => url;
+  }
+}
