3bfa1fa2589aeef6efbe8aadc2d76ed95c47e03a
Arturas Slajus <x11@arturaz.net>
2016-12-22 11:30:45 +0200

    long#toIntClamped

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/LongExts.cs
@@ -0,0 +1,9 @@
+﻿namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class LongExts {
+    public static int toIntClamped(this long l) {
+      if (l > int.MaxValue) return int.MaxValue;
+      if (l < int.MinValue) return int.MinValue;
+      return (int) l;
+    }
+  }
+}
