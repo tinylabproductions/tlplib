0984386c504129c5ce1d9efab2b03a2ccd50d09b
gyts <gytis.stanciukas@gmail.com>
2017-01-03 18:16:26 +0200

    Type2.getType

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/reflection/Type2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+using com.tinylabproductions.TLPLib.Functional;
+
+namespace com.tinylabproductions.TLPLib.reflection {
+  public static class Type2 {
+    public static Either<string, Type> getType(string name) =>
+      F.opt(Type.GetType(name)).toRight($"Can't find type '{name}'");
+  }
+}
