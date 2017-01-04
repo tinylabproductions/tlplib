8b35b495a79ac67f859fee206d871b0ed1aafe8e
Arturas Slajus <arturas@tinylabproductions.com>
2017-01-04 14:10:23 +0200

    Fn<A, B>#andThen(Fn<B, C>)

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/FuncExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/FuncExts.cs
@@ -2,8 +2,8 @@
 
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class FuncExts {
-    public static Act<A> andThen<A, B>(this Fn<A, B> f, Act<B> a) {
-      return value => a(f(value));
-    }
+    public static Act<A> andThen<A, B>(this Fn<A, B> f, Act<B> a) => value => a(f(value));
+    public static Fn<A, C> andThen<A, B, C>(this Fn<A, B> f, Fn<B, C> f1) => 
+      value => f1(f(value));
   }
 }
