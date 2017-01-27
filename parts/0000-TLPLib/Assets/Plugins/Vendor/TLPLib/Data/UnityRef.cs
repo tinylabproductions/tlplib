59196f9f9590af18f0e4ddd36430476b18a60150
Astro75 <evaldas757@gmail.com>
2017-01-27 12:59:55 +0200

    UnityRef

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Data/UnityRef.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace com.tinylabproductions.TLPLib.Data {
+  public static class UnityRef {
+    public static UnityRef<A> a<A>(A a) where A : UnityEngine.Object =>
+      new UnityRef<A>(a); 
+  }
+
+  public class UnityRef<A> : IDisposable where A : UnityEngine.Object {
+    public A reference { get; private set; }
+
+    public UnityRef(A reference) {
+      this.reference = reference;
+    }
+
+    public override string ToString() => $"{nameof(UnityRef<A>)}({reference})";
+
+    public void Dispose() => reference = null;
+
+    public static implicit operator A(UnityRef<A> r) => r.reference;
+  }
+}
