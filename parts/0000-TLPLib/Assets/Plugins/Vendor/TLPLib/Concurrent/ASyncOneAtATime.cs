7522bea666f1cc648fc40f9c79155421a23cdaa2
Arturas Slajus <x11@arturaz.net>
2017-02-16 22:01:29 +0200

    BREAKING: ASyncOneAtATimeQueue -> ASyncNAtATimeQueue

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Concurrent/ASyncOneAtATime.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Concurrent/ASyncOneAtATime.cs
@@ -1,21 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace com.tinylabproductions.TLPLib.Concurrent {
   /* Execute asynchronous things one at a time. Useful for wrapping not 
    * concurrent event based apis to futures. */
-  public class ASyncOneAtATimeQueue<Params, Return> {
-    Future<Return> theFuture = Future.successful(default(Return));
+  public static class ASyncNAtATimeQueue {
+    public static ASyncNAtATimeQueue<Params, Return> a<Params, Return>(
+      Fn<Params, Future<Return>> execute, ushort maxTasks = 1
+    ) => new ASyncNAtATimeQueue<Params,Return>(maxTasks, execute);
 
-    readonly Act<Params, Promise<Return>> doQuery;
+    public static ASyncNAtATimeQueue<Params, Return> a<Params, Return>(
+      Act<Params, Promise<Return>> execute, ushort maxTasks = 1
+    ) => new ASyncNAtATimeQueue<Params,Return>(
+      maxTasks, 
+      p => Future<Return>.async(promise => execute(p, promise))
+    );
+  }
 
-    public ASyncOneAtATimeQueue(Act<Params, Promise<Return>> doQuery) {
-      this.doQuery = doQuery;
+  public class ASyncNAtATimeQueue<Params, Return> {
+    struct QueueEntry {
+      public readonly Params p;
+      public readonly Promise<Return> promise;
+
+      public QueueEntry(Params p, Promise<Return> promise) {
+        this.p = p;
+        this.promise = promise;
+      }
     }
 
-    public Future<Return> query(Params parameters) => 
-      Future<Return>.async((p, f) => {
-        theFuture.onComplete(_ => doQuery(parameters, p));
-        theFuture = f;
-      });
+    readonly Queue<QueueEntry> queue = new Queue<QueueEntry>();
+    readonly uint maxTasks;
+    readonly Fn<Params, Future<Return>> execute;
+
+    public uint running { get; private set; }
+    public uint queued => (uint) queue.Count;
+
+    public ASyncNAtATimeQueue(uint maxTasks, Fn<Params, Future<Return>> execute) {
+      this.maxTasks = maxTasks;
+      this.execute = execute;
+    }
+
+    public Future<Return> enqueue(Params p) {
+      if (running < maxTasks) return runTask(p);
+
+      Promise<Return> promise;
+      var f = Future.async(out promise);
+      queue.Enqueue(new QueueEntry(p, promise));
+      return f;
+    }
+
+    void taskCompleted() {
+      running--;
+      if (queued == 0) return;
+      var entry = queue.Dequeue();
+      runTask(entry.p).onComplete(entry.promise.complete);
+    }
+
+    Future<Return> runTask(Params p) {
+      running++;
+      var f = execute(p);
+      f.onComplete(_ => taskCompleted());
+      return f;
+    }
   }
 }
