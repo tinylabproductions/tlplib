91b04390ceabc57c118aca01439b1cf8d0254d1d
Astro75 <evaldas757@gmail.com>
2017-02-22 19:20:28 +0200

    StandardInterstitial: removed stupid onLoad

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Android/Ads/StandardInterstitial.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Android/Ads/StandardInterstitial.cs
@@ -11,17 +11,11 @@ namespace com.tinylabproductions.TLPLib.Android.Ads {
 
 #if UNITY_ANDROID
   public class StandardInterstitial : IStandardInterstitial {
-    readonly Subject<Unit> _onLoad = new Subject<Unit>();
-    public IObservable<Unit> onLoad => _onLoad;
-
     protected readonly AndroidJavaObject java;
 
     public StandardInterstitial(AndroidJavaObject java) { this.java = java; }
 
-    public void load() {
-      java.Call("load");
-      _onLoad.push(F.unit);
-    }
+    public void load() => java.Call("load");
     public bool ready => java.Call<bool>("isReady");
     public void show() => java.Call("show");
   }
