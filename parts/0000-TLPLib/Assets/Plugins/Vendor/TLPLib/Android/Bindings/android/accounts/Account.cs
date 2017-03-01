19d9a1b94ab6c7db8195c99d3fd3f579fc84de67
Tadas Valutis <tadas@valutis.lt>
2017-03-01 14:42:15 +0200

    AccountManger and Account java Class

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Android/Bindings/android/accounts/Account.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Android.Bindings.android.accounts {
+  public class Account : Binding {
+    public Account(AndroidJavaObject java) : base(java) {}
+
+    public string name => java.Get<string>("name");
+
+    public string type => java.Get<string>("type");
+  }
+}
