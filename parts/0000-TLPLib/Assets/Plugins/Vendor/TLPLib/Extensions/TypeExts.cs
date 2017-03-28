4cad4df9945eb1f3b695a1229ce9db68483780f8
Astro75 <evaldas757@gmail.com>
2017-03-28 19:35:19 +0300

    FieldInfo.isSerializable, Type.getAllFields

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/TypeExts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class TypeExts {
+    // http://stackoverflow.com/questions/1155529/not-getting-fields-from-gettype-getfields-with-bindingflag-default/1155549#1155549
+    public static IEnumerable<FieldInfo> getAllFields(this Type t) {
+      if (t == null) return Enumerable.Empty<FieldInfo>();
+
+      const BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic |
+                                 BindingFlags.Instance |
+                                 BindingFlags.DeclaredOnly;
+      return t.GetFields(flags).Concat(getAllFields(t.BaseType));
+    }
+  }
+}
