2664cfa7d60a74fff4c9a37f903fa6c0ec39a37d
Arturas Slajus <arturas@tinylabproductions.com>
2017-04-21 15:32:39 +0300

    Fix IObservable<A>#subscribeWhileAlive

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/ObservableExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/ObservableExts.cs
@@ -1,16 +1,31 @@
 ﻿using System;
+using com.tinylabproductions.TLPLib.Components.Forwarders;
 using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class ObservableExts {
-    public static ISubscription subscribeWhileAlive<A>(this IObservable<A> obs, UnityEngine.Object obj, Act<A> onChange) {
-      return obs.subscribe((val, sub) => {
+    // TODO: test
+    public static ISubscription subscribeWhileAlive<A>(
+      this IObservable<A> obs, GameObject obj, Act<A> onChange
+    ) {
+      var onDestroySub = Subscription.empty;
+      var subscription = obs.subscribe((val, sub) => {
         if (!obj) {
+          onDestroySub.unsubscribe();
           sub.unsubscribe();
           return;
         }
         onChange(val);
       });
+      onDestroySub = 
+        obj.EnsureComponent<OnDestroyForwarder>().onDestroy
+        .subscribeForOneEvent(_ => subscription.unsubscribe());
+      return subscription;
     }
+
+    public static ISubscription subscribeWhileAlive<A>(
+      this IObservable<A> obs, MonoBehaviour behaviour, Act<A> onChange
+    ) => obs.subscribeWhileAlive(behaviour.gameObject, onChange);
   }
 }
