2ed1e4880f023ea0641e85662b3535b05236ec57
Ignotas <ignotas.m@gmail.com>
2017-05-09 15:54:26 +0300

    PackageInfo.cs, Iterator.cs, List.cs, PlatformPackageManager.cs additions

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Android/Bindings/java/util/ArrayList.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Android/Bindings/java/util/ArrayList.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Android.Bindings.java.util {
-  public class ArrayList : Binding {
+  public class ArrayList : List {
     public ArrayList(AndroidJavaObject java) : base(java) {}
     public ArrayList(int capacity) 
       : this(new AndroidJavaObject("java.util.ArrayList", capacity)) {}
@@ -12,9 +12,6 @@ namespace com.tinylabproductions.TLPLib.Android.Bindings.java.util {
     public ArrayList(IEnumerable<AndroidJavaObject> enumerable, int capacity = 0) : this(capacity) {
       foreach (var elem in enumerable) add(elem);
     }
-
-    public void add(int location, AndroidJavaObject o) => java.Call("add", location, o);
-    public bool add(AndroidJavaObject o) => java.Call<bool>("add", o);
   }
 }
 #endif
---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Platform/PlatformPackageManager.cs
@@ -0,0 +1,53 @@
+﻿#if UNITY_ANDROID
+using System.Collections.Immutable;
+using System.Linq;
+using com.tinylabproductions.TLPLib.Android;
+using com.tinylabproductions.TLPLib.Android.Bindings.android.content.pm;
+
+#endif
+
+namespace com.tinylabproductions.TLPLib.Platform {
+  public interface IPlatformPackageManager {
+    bool hasAppInstalled(string bundleIdentifier);
+    bool hasSystemF
+  }
+
+  public static class PlatformPackageManager {
+    /// <summary>
+    /// Checks for active platform, and returns platform specific package manager
+    /// </summary>
+    /// <returns>Platform specific package manager</returns>
+    public static IPlatformPackageManager a() {
+#if UNITY_EDITOR
+      return new NoOpPlatformPackageManager();
+#elif UNITY_ANDROID
+      return new AndroidPlatformPackageManager();
+#else
+      return new NoOpPlatformPackageManager();
+#endif
+    }
+  }
+
+  class NoOpPlatformPackageManager : IPlatformPackageManager {
+    public bool hasAppInstalled(string bundleIdentifier) => false;
+  }
+
+#if UNITY_ANDROID
+  class AndroidPlatformPackageManager : IPlatformPackageManager {
+    readonly PackageManager androidPackageManager;
+    readonly ImmutableList<string> packageNames;
+
+    public AndroidPlatformPackageManager() {
+      androidPackageManager = AndroidActivity.packageManager;
+      //Cache all of the packet names for better performance
+      packageNames = androidPackageManager
+        .getInstalledPackages(PackageManager.GetPackageInfoFlags.GET_ACTIVITIES)
+        .Select(package => package.packageName)
+        .ToImmutableList();
+    }
+
+    public bool hasSystemFeature(string feature) => androidPackageManager.hasSystemFeature(feature);
+    public bool hasAppInstalled(string bundleIdentifier) => packageNames.Contains(bundleIdentifier);
+  }
+#endif
+}
