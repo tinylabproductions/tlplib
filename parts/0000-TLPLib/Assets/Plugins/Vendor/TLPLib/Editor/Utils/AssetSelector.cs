2bb78f7b937972c713ac08bbf339dd16453b8d50
Astro75 <evaldas757@gmail.com>
2017-05-16 12:24:32 +0300

    AssetSelector: Select only exact type.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/AssetSelector.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/AssetSelector.cs
@@ -31,32 +31,42 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
         if (type == null) return;
         if (type.canBeUnityComponent()) {
           if (GUILayout.Button("Select all")) {
-            try {
-              EditorUtility.DisplayProgressBar("Working", "Please wait...", 0);
-              var objects = AssetDatabase.FindAssets("t:prefab")
-                .Select(AssetDatabase.GUIDToAssetPath)
-                .Select(path => AssetDatabase.LoadAssetAtPath(path, type))
-                .Where(c => c)
-                .Select(c => {
-                  foreach (var _ in F.opt(c as Component)) return _.gameObject;
-                  foreach (var _ in F.opt(c as MonoBehaviour)) return _.gameObject;
-                  throw new Exception($"Unrecognized type {c.GetType()} on component {c}");
-                })
-                .ToArray();
-              Log.info($"Total objects found: {objects.Length}");
-              foreach (var obj in objects) {
-                Log.info(AssetDatabase.GetAssetPath(obj), obj);
-              }
-              Selection.objects = objects;
-            }
-            finally {
-              EditorUtility.ClearProgressBar();
-            }
+            Selection.objects = findObjects(type, true);
+          }
+          if (GUILayout.Button("Select only exact type")) {
+            Selection.objects = findObjects(type, false);
           }
         }
         else {
           GUILayout.Label("Type should be MonoBehaviour, Component or interface");
         }
+      }
+    }
+
+    static GameObject[] findObjects(Type type, bool includeDerived) {
+      try {
+        EditorUtility.DisplayProgressBar("Working", "Please wait...", 0);
+        var objects = AssetDatabase.FindAssets("t:prefab")
+          .Select(AssetDatabase.GUIDToAssetPath)
+          .Select(path => AssetDatabase.LoadAssetAtPath(path, type))
+          .Where(c => {
+            if (c) return includeDerived || c.GetType() == type;
+            return false;
+          })
+          .Select(c => {
+            foreach (var _ in F.opt(c as Component)) return _.gameObject;
+            foreach (var _ in F.opt(c as MonoBehaviour)) return _.gameObject;
+            throw new Exception($"Unrecognized type {c.GetType()} on component {c}");
+          })
+          .ToArray();
+        Log.info($"Total objects found: {objects.Length}");
+        foreach (var obj in objects) {
+          Log.info(AssetDatabase.GetAssetPath(obj), obj);
+        }
+        return objects;
+      }
+      finally {
+        EditorUtility.ClearProgressBar();
       }
     }
   }
