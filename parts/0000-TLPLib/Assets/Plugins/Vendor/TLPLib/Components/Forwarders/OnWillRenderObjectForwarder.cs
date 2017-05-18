7d3f1c4b8942ce0a632713ffb745995fc6e0d38b
Arturas Slajus <arturas@tinylabproductions.com>
2017-05-18 12:24:00 +0300

    OnRenderObjectForwarder, OnWillRenderObjectForwarder

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/OnWillRenderObjectForwarder.cs
@@ -0,0 +1,18 @@
+﻿using com.tinylabproductions.TLPLib.Components.Interfaces;
+using com.tinylabproductions.TLPLib.Logger;
+using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Components.Forwarders {
+  public class OnWillRenderObjectForwarder : MonoBehaviour, IMB_OnWillRenderObject {
+    readonly Subject<Camera> subject = new Subject<Camera>();
+    public IObservable<Camera> onWillRenderObject => subject;
+
+    public void OnWillRenderObject() {
+      if (Log.isVerbose) Log.verbose(
+        $"{nameof(OnWillRenderObjectForwarder)} this = {this}, camera current = {Camera.current}"
+      );
+      subject.push(Camera.current);
+    }
+  }
+}
