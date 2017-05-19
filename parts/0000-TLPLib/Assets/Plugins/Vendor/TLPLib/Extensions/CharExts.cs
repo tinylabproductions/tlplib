9f1f91a86e4beea4f46c60be221561b494b3a82b
Arturas Slajus <arturas@tinylabproductions.com>
2017-05-19 12:01:13 +0300

    Fix char#isNumeric

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/CharExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/CharExts.cs
@@ -3,7 +3,7 @@
     public static bool isAlphabetic(this char c) =>
       (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 
-    public static bool isNumeric(this char c) => c >= 0 && c <= 9;
+    public static bool isNumeric(this char c) => c >= '0' && c <= '9';
 
     public static bool isAlphaNumeric(this char c) => c.isAlphabetic() || c.isNumeric();
   }
