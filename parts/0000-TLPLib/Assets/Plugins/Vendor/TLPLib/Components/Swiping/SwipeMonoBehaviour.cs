f04184e354e272f266068905339d9ce689f1927b
Arturas Slajus <x11@arturaz.net>
2017-05-20 10:55:24 +0300

    Cleaned up SwipeMonoBehaviour

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Swiping/SwipeMonoBehaviour.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Swiping/SwipeMonoBehaviour.cs
@@ -5,10 +5,8 @@ using UnityEngine.EventSystems;
 namespace com.tinylabproductions.TLPLib.Components.Swiping {
   public class SwipeMonoBehaviour : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler {
     public event Action<Vector2> swipeEnded;
-    public event Action swipedLeft;
-    public event Action swipedRigth;
-    public event Action swipedUp;
-    public event Action swipedDown;
+    public event Action swipedLeft, swipedRigth, swipedUp, swipedDown;
+
     Vector2 dragBeginPos;
 
     public void OnBeginDrag(PointerEventData eventData) {
@@ -18,30 +16,20 @@ namespace com.tinylabproductions.TLPLib.Components.Swiping {
 
     public void OnEndDrag(PointerEventData eventData) {
       if (!mayDrag(eventData)) return;
+
       var delta = eventData.position - dragBeginPos;
       swipeEnded?.Invoke(delta);
-      if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y)) {
-        if (delta.x > 0) {
-          swipedLeft?.Invoke();
-        }
-        else {
-          swipedRigth?.Invoke();
-        }
-      }
-      else {
-        if (delta.y > 0) {
-          swipedDown?.Invoke();
-        }
-        else {
-          swipedUp?.Invoke();
-        }
-      }
+      if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+        (delta.x > 0 ? swipedLeft : swipedRigth)?.Invoke();
+      else
+        (delta.y > 0 ? swipedDown : swipedUp)?.Invoke();
     }
 
     public void OnDrag(PointerEventData eventData) {
       // Required for BeginDrag / EndDrag to work
     }
 
-    static bool mayDrag(PointerEventData eventData) { return eventData.button == PointerEventData.InputButton.Left; }
+    static bool mayDrag(PointerEventData eventData) => 
+      eventData.button == PointerEventData.InputButton.Left;
   }
 }
