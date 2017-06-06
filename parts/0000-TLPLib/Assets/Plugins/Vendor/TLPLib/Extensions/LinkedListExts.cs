5a619ca139517cd48e119e0e7fe351a27456afeb
Ignotas <ignotas.m@gmail.com>
2017-06-06 11:02:55 +0300

    Reformating

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/LinkedListExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/LinkedListExts.cs
@@ -4,16 +4,10 @@ using com.tinylabproductions.TLPLib.Functional;
 
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class LinkedListExts {
-    public static bool isEmpty<A>(this ReadOnlyLinkedList<A> list) {
-      return list.Count == 0;
-    }
-
-    public static bool isEmpty<A>(this LinkedList<A> list) {
-      return list.Count == 0;
-    }
+    public static bool isEmpty<A>(this ReadOnlyLinkedList<A> list) => list.Count == 0;
+    public static bool isEmpty<A>(this LinkedList<A> list) => list.Count == 0;
 
     /* Removes first element and returns it. */
-
     public static Option<A> shift<A>(this LinkedList<A> list) {
       var a = F.opt(list.First);
       list.RemoveFirst();
@@ -21,14 +15,14 @@ namespace com.tinylabproductions.TLPLib.Extensions {
     }
 
     /* Removes last element and returns it. */
-
     public static Option<A> pop<A>(this LinkedList<A> list) {
       var a = F.opt(list.Last);
       list.RemoveLast();
       return a.map(_ => _.Value);
     }
 
-    public static LinkedListNode<A> nextOrFirst<A>(this LinkedListNode<A> node)
-      => node.Next ?? node.List.First;
+    public static LinkedListNode<A> nextOrFirst<A>(
+      this LinkedListNode<A> node
+    ) => node.Next ?? node.List.First;
   }
 }
