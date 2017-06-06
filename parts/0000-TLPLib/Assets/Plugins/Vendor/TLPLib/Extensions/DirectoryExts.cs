84802e19d4cb83decd45f6402bbe67b83bfbc2fb
Ignotas <ignotas.m@gmail.com>
2017-06-06 10:59:27 +0300

    Added methods for looping a linked list and for completely deleting a directory and it's contents

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/DirectoryExts.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using com.tinylabproductions.TLPLib.Filesystem;
+
+namespace Plugins.Vendor.TLPLib.Extensions {
+  public static class DirectoryExts {
+    public static void completeDelete(PathStr path) {
+      var files = Directory.GetFiles(path);
+      var dirs = Directory.GetDirectories(path);
+
+      foreach (var file in files) {
+        File.SetAttributes(file, FileAttributes.Normal);
+        File.Delete(file);
+      }
+
+      foreach (var dir in dirs) completeDelete(PathStr.a(dir));
+      
+      Directory.Delete(path, false);
+    }
+  }
+}
