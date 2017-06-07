41a6502bd10e72eaceaacf569222d1c3620f9ad3
Ignotas <ignotas.m@gmail.com>
2017-06-07 10:40:55 +0300

    Added generic methods for ResourceReference loading - normal, async, unsafe.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/ResourceReference/ResourceReference.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/ResourceReference/ResourceReference.cs
@@ -40,29 +40,59 @@ namespace com.tinylabproductions.TLPLib.ResourceReference {
       return so;
     }
 #endif
+    static string notFound(string path) => $"Resource not found: {path}";
 
-    public static Option<A> load<A>(PathStr loadPath) where A : Object {
+    public static Either<string, A> load<A>(PathStr loadPath) where A : Object {
       var path = loadPath.toUnityPath();
       var csr = Resources.Load<ResourceReference<A>>(path);
-      return csr ? csr.reference.some() : Option<A>.None;
+      return csr 
+        ? F.right<string, A>(csr.reference) 
+        : F.left<string, A>(notFound(path));
     }
 
-    public static Tpl<ResourceRequest, Future<Option<A>>> loadAsync<A>(
+    public static A loadOrThrow<A>(PathStr loadPath) where A : Object {
+      var loaded = load<A>(loadPath);
+      if (loaded.isLeft) throw new Exception(loaded.leftOrThrow);
+      return loaded.rightOrThrow;
+    }
+
+    public static Tpl<ResourceRequest, Future<Either<string, A>>> loadAsync<A>(
       PathStr loadPath
     ) where A : Object {
       var path = loadPath.toUnityPath();
       var request = Resources.LoadAsync<ResourceReference<A>>(path);
-      return F.t(request, Future<Option<A>>.async(
-        p => ASync.StartCoroutine(waitForLoadCoroutine(request, p))
+      return F.t(request, Future<Either<string, A>>.async(
+        p => ASync.StartCoroutine(waitForLoadCoroutine(request, p, path))
       ));
     }
 
+    /// <summary>
+    /// Tries to load a resource asynchronously.
+    /// If it fails, it logs an error and returns an unfulfilled future.
+    /// Otherwise returns fullfilled future with the requested resource. 
+    /// </summary>
+    public static Tpl<ResourceRequest, Future<A>> loadAsyncUnsafe<A>(
+      PathStr loadPath
+    ) where A : Object {
+      var loadResult = loadAsync<A>(loadPath);
+      var throwOrReturnFtr = loadResult._2.flatMap(errorOrResource => {
+        if (errorOrResource.isRight) return Future<A>.successful(errorOrResource.rightOrThrow);
+        Log.error(errorOrResource.leftOrThrow);
+        return Future<A>.unfulfilled;
+      });
+
+      return F.t(loadResult._1, throwOrReturnFtr);
+    }
+
     static IEnumerator waitForLoadCoroutine<A>(
-      ResourceRequest request, Promise<Option<A>> p
+      ResourceRequest request, Promise<Either<string, A>> p, string path
     ) where A : Object {
       yield return request;
       var csr = (ResourceReference<A>) request.asset;
-      p.complete(csr ? csr.reference.some() : Option<A>.None);
+      p.complete(csr
+        ? F.right<string, A>(csr.reference)
+        : F.left<string, A>(notFound(path))
+      );
     }
   }
 }
