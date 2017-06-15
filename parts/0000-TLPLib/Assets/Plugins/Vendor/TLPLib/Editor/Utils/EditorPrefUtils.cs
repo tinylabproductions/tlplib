ad873a1d7201b4c2b54349c049679304da178d3c
Arturas Slajus <arturas@tinylabproductions.com>
2017-06-15 16:51:13 +0300

    EditorPrefUtils.cs

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/EditorPrefUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using com.tinylabproductions.TLPLib.Logger;
+using UnityEditor;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Editor.Utils {
+  public static class EditorPrefUtils {
+    [MenuItem("Tools/Prefs/Clear PlayerPrefs")]
+    public static void clearPlayerPrefs() {
+      if (EditorUtility.DisplayDialog("Clear player prefs?", "Are you sure?", "Clear", "Cancel")) {
+        PlayerPrefs.DeleteAll();
+        Log.info("Player prefs cleared");
+      }
+    }
+
+    [MenuItem("Tools/Prefs/Clear EditorPrefs")]
+    public static void clearEditorPrefs() {
+      Fn<string, bool> check = title =>
+        EditorUtility.DisplayDialog(
+          "Clear editor prefs?",
+          $"{title}\n\n" +
+          $"This will delete all the settings you have set in Unity Editor " +
+          $"(including SDK paths, color schemes and any data that custom plugins might have saved)\n\n" +
+          $"!!! Use this as LAST RESORT only !!!",
+          "Wipe them clean", "Cancel, I'm scared"
+        );
+
+      if (
+        check("Are you sure?") 
+        && check("Are you really sure?") 
+        && check("You're sure you understand the consequences?")
+      ) {
+        EditorPrefs.DeleteAll();
+        Log.info("Editor prefs cleared. Good luck with that.");
+      }
+    }
+  }
+}
