b70b35af9f82ebd26ae3a32b87bd51bb4863e890
Karolis <karolisjc@gmail.com>
2017-06-19 10:26:49 +0300

    ordinal string ext

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/UIntExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/UIntExts.cs
@@ -8,5 +8,19 @@
       if (b > 0 && uint.MaxValue - a < b) return uint.MaxValue;
       return (uint) (a + b);
     }
+
+    public static string toOrdinalString(this uint number) {
+      var div = number % 100;
+      if ((div >= 11) && (div <= 13)) {
+        return $"{number}th";
+      }
+
+      switch (number % 10) {
+        case 1: return $"{number}st";
+        case 2: return $"{number}nd";
+        case 3: return $"{number}rd";
+        default: return $"{number}th";
+      }
+    }
   }
 }
