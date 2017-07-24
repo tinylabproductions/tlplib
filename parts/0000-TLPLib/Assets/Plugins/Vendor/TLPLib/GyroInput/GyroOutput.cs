6e6c88d0711f8ed0aaf446f23aa5cb68cb062cec
Ignotas <ignotas.m@gmail.com>
2017-07-24 16:35:15 +0300

    Gyro offset

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/GyroInput/GyroOutput.cs
@@ -0,0 +1,19 @@
+﻿using com.tinylabproductions.TLPLib.Components.Interfaces;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.tinylabproductions.TLPLib.GyroInput {
+  public class GyroOutput : MonoBehaviour, IMB_Update {
+#pragma warning disable 649
+    [SerializeField] Text text;
+#pragma warning restore 649
+
+    public void Update() {
+      var gyro = Input.gyro;
+      text.text =
+        $"Gyro attitude {gyro.attitude}\n" +
+        $"Gyro rotation rate unbiased {gyro.rotationRateUnbiased}\n" +
+        $"Gyro offset {GyroOffset.instance.offset}";
+    }
+  }
+}
---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/GyroInput/GyroParallaxed.cs
@@ -0,0 +1,16 @@
+﻿using com.tinylabproductions.TLPLib.Components.Interfaces;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.GyroInput {
+  public class GyroParallaxed : MonoBehaviour, IMB_Update {
+#pragma warning disable 649
+    [SerializeField] float zValue;
+#pragma warning restore 649
+    public bool enabled = true;
+    public void Update() {
+      if (enabled) {
+        transform.localPosition = GyroOffset.instance.offset * -zValue;
+      }
+    }
+  }
+}
