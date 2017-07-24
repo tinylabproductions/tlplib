76cb73e4e44c3b09f882c06d326fe5b8411f1fa1
IgnotasM <ignotas.m@gmail.com>
2017-07-24 17:40:36 +0300

    Gyro offset (#79)
    * Gyro offset
    
    * Fixes
    
    * Removed unnecessary code
    
    * GyroInput optimisation

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/GyroInput/GyroOffset.cs
@@ -0,0 +1,64 @@
+﻿using com.tinylabproductions.TLPLib.Concurrent;
+using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Functional;
+using UnityEngine;
+using Coroutine = com.tinylabproductions.TLPLib.Concurrent.Coroutine;
+
+namespace com.tinylabproductions.TLPLib.GyroInput {
+  public class GyroOffset {
+    public static readonly GyroOffset instance = new GyroOffset();
+
+    readonly Gyroscope gyro = Input.gyro;
+    public Vector2 offset { get; private set; } = Vector2.zero;
+    public Vector2 axisLocks = Vector2.one;
+    public float friction = 0.01f;
+
+    Option<Coroutine> updateCoroutine = Option<Coroutine>.None;
+
+    GyroOffset() {
+      // Does side effects.
+      enabled = true;
+    }
+
+    bool _enabled;
+    public bool enabled {
+      get { return _enabled; }
+      set {
+        _enabled = value;
+
+        if (value) {
+          // Make sure gyro is enabled. It is needed for us to work.
+          gyro.enabled = true;
+          if (updateCoroutine.isNone) updateCoroutine = ASync.EveryFrame(update).some();
+        }
+        else {
+          offset = Vector2.zero;
+          foreach (var c in updateCoroutine) {
+            c.stop();
+            updateCoroutine = updateCoroutine.none;
+          }
+        }
+      }
+    }
+
+    bool update() {
+      if (gyro.enabled) calculateOffset(gyro);
+      return true;
+    }
+
+    void calculateOffset(Gyroscope gyro) {
+      // We get gyro rotation rate in radians / sec
+      var gyroRate = gyro.rotationRateUnbiased;
+      // We sum the offsets as we want them to be applied frame after frame
+      offset += new Vector2(-gyroRate.z, -gyroRate.x) * Time.deltaTime;
+      // We clamp the offset to certain values to make sure to lock the camera's paralax effect
+      // Only to a certain sphere area
+      offset = new Vector2(
+        Mathf.Clamp(offset.x, -axisLocks.x, axisLocks.x),
+        Mathf.Clamp(offset.y, -axisLocks.y, axisLocks.y)
+      );
+
+      offset = Vector2.Lerp(offset, Vector2.zero, friction);
+    }
+  }
+}
