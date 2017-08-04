19e55784fe4f414688b660e9b25d5a1e2776663d
Astro75 <evaldas757@gmail.com>
2017-08-04 19:31:00 +0300

    WIP: UnityActivity

---
+++ parts/0000-TLPLib/Assets/Plugins/Android/tlplib/src/com/tinylabproductions/tlplib/UnityActivity.java
@@ -0,0 +1,35 @@
+package com.tinylabproductions.tlplib;
+
+import android.content.Intent;
+import android.util.Log;
+import com.unity3d.player.UnityPlayerActivity;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UnityActivity extends UnityPlayerActivity {
+    public interface IActivityResult {
+        void onActivityResult(int requestCode, int resultCode, Intent data);
+    }
+
+    Set<IActivityResult> activityResultListeners = new HashSet<>();
+
+    public void subscribeOnActivityResult(IActivityResult f) {
+        activityResultListeners.add(f);
+    }
+
+    public void unsubscribeOnActivityResult(IActivityResult f) {
+        if (activityResultListeners.contains(f)) activityResultListeners.remove(f);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        for (IActivityResult f : activityResultListeners) {
+            try {
+                f.onActivityResult(requestCode, resultCode, data);
+            } catch (Exception e) {
+                Log.e(Tag.TAG, "Error executing onActivityResult subscriber");
+            }
+        }
+    }
+}
