58d3b2d022b9ad03010dcac46f509e506d3ec0a7
Karolis <karolisjc@gmail.com>
2017-08-21 19:00:37 +0300

    const fix

--- parts/0000-TLPLib/Assets/Plugins/Android/tlplib/src/com/tinylabproductions/tlplib/UnityActivity.java
+++ parts/0000-TLPLib/Assets/Plugins/Android/tlplib/src/com/tinylabproductions/tlplib/UnityActivity.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class UnityActivity extends UnityPlayerActivity {
-    public const int REQUEST_CODE_BASE = 1000000;
+    public final int REQUEST_CODE_BASE = 1000000;
     private int requestCode = REQUEST_CODE_BASE;
 
     public interface IActivityResult {
