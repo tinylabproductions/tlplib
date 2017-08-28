136da501de5ff03490fccd8d3456f722faa96378
Arturas Slajus <arturas@tinylabproductions.com>
2017-08-28 16:45:29 +0300

    Cleaned up gradient component code.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/gradient/GradientBase.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/gradient/GradientBase.cs
@@ -3,23 +3,20 @@ using Smooth.Pools;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Assets.Code.UI {
+namespace com.tinylabproductions.TLPLib.Components {
   public class GradientBase : BaseMeshEffect {
-
     #region Unity Serialized Fields
 
 #pragma warning disable 649
-    [SerializeField]
-    protected GradientHelper.GradientType type = GradientHelper.GradientType.Vertical;
+    [SerializeField] protected GradientHelper.GradientType type = GradientHelper.GradientType.Vertical;
 #pragma warning restore 649
 
     #endregion
 
-    public virtual void ModifyVertices(List<UIVertex> vertexList) {
+    public virtual void ModifyVertices(List<UIVertex> vertexList) {}
 
-    }
     public override void ModifyMesh(VertexHelper vh) {
-      if (!this.IsActive()) return;
+      if (!IsActive()) return;
       var verts = ListPool<UIVertex>.Instance.Borrow();
       vh.GetUIVertexStream(verts);
       ModifyVertices(verts);  // calls the old ModifyVertices which was used on pre 5.2
---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/gradient/GradientSimple.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace com.tinylabproductions.TLPLib.Components.gradient {
+  [AddComponentMenu("UI/Effects/Gradient")]
+  public class GradientSimple : GradientBase {
+    public Color32 topColor = Color.white, bottomColor = Color.black;
+
+    public override void ModifyVertices(List<UIVertex> vertexList) {
+      GradientHelper.modifyVertices(vertexList, (c, t) => Color32.Lerp(bottomColor, topColor, t), type);
+    }
+  }
+}
