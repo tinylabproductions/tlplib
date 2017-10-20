eab7ec065ee0f75676444707c02e8943ba02af16
Arturas Slajus <arturas@tinylabproductions.com>
2017-10-20 16:12:51 +0300

    int#toUIntClamped

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/IntExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/IntExts.cs
@@ -1,6 +1,7 @@
 ﻿namespace com.tinylabproductions.TLPLib.Extensions {
   public static class IntExts {
     public static long toLong(this int i) => i;
+    public static uint toUIntClamped(this int a) => a < 0 ? 0u : (uint) a;
 
     public static string toMinSecString(this int seconds) => $"{seconds / 60}:{seconds % 60:00}";
   }
