20f602635d41952bf21211ef92a00913d45eedcd
gyts <gytis.stanciukas@gmail.com>
2017-10-25 17:52:19 +0300

    skew with float extension method

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/FloatExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/FloatExts.cs
@@ -3,5 +3,7 @@
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class FloatExts {
     public static int roundToInt(this float number) => Mathf.RoundToInt(number);
+
+    public static bool approx0(this float number) => Mathf.Approximately(number, 0);
   }
 }
