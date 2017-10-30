b5f77ba2b24269b981441433adf60f678e91b007
gyts <gytis.stanciukas@gmail.com>
2017-10-30 17:51:42 +0200

    scene manager utils

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/EditorSceneManagerUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using com.tinylabproductions.TLPLib.Utilities;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace com.tinylabproductions.TLPLib.Editor.Utils {
+  public static class EditorSceneManagerUtils {
+    public static A withScene<A>(string scenePath, Fn<Scene, A> f) {
+      var isLoaded = SceneManagerUtils.loadedScenes.Any(s => s.path == scenePath);
+      var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Additive);
+      var ret = f(scene);
+      if (!isLoaded) SceneManager.UnloadSceneAsync(scene);
+      return ret;
+    }
+  }
+}
