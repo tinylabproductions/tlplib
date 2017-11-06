74abf4c0ddc9651c2b220c5bac9d6883b1d38cdc
Donatas <laurinavicius.donatas@gmail.com>
2017-11-06 14:58:19 +0200

    Moved scripts from fkr

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/UI/UIDownScaleBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Assets.Code.Utils;
+using com.tinylabproductions.TLPLib.Components.Interfaces;
+using UnityEngine;
+using UnityEngine.EventSystems;
+namespace com.tinylabproductions.TLPLib.UI {
+  public abstract class UIDownScaleBase : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IMB_Awake {
+    public float scale = 1.1f;
+    [Header("Optional, default to this.transform")]
+    public Transform target;
+
+    protected Vector3 previous;
+    protected bool isDown;
+    protected new SinusoidScale animation;
+
+    public abstract Vector3 targetLocalScale { get; set; }
+    public abstract Action onPointerUp { get; }
+
+    public void Awake() {
+      if (!target) target = transform;
+      animation = target.GetComponent<SinusoidScale>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+      if (eventData.button != PointerEventData.InputButton.Left) return;
+      pointerDown();
+    }
+
+    public void pointerDown() {
+      if (!isDown) {
+        isDown = true;
+        previous = targetLocalScale;
+        targetLocalScale *= scale;
+        if (animation != null) animation.enabled = !isDown;
+      }
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+      if (eventData.button != PointerEventData.InputButton.Left) return;
+      pointerUp();
+    }
+
+    public virtual void pointerUp() {
+      if (isDown) {
+        isDown = false;
+        targetLocalScale = previous;
+        if (animation != null) animation.enabled = !isDown;
+        onPointerUp();
+      }
+    }
+  }
+}
---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/SinusoidScale.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Utils {
+  public class SinusoidScale : MonoBehaviour {
+    public Vector3 from = Vector3.one;
+    public Vector3 to = Vector3.one;
+    public float speed = 1;
+    float timeShift;
+    bool timeshiftSet;
+
+    internal void Start() {
+      if (!timeshiftSet) {
+        setTimeShift(Random.value);
+      }
+    }
+
+    public void OnEnable() {
+      Update();
+    }
+
+    internal void Update() {
+      transform.localScale = Vector3.Lerp(from, to, (Mathf.Sin(Time.time * speed + timeShift) + 1) * .5f);
+    }
+
+    public void setTimeShift(float t) {
+      timeshiftSet = true;
+      timeShift = t * Mathf.PI * 2;
+      Update();
+    }
+  }
+}
