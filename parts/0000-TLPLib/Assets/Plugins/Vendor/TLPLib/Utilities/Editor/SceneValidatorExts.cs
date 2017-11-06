53023872b57641e0f27b53ebdf2706297eac5b92
Ignotas <ignotas.m@gmail.com>
2017-11-06 10:14:24 +0200

    added convenience method

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/Editor/SceneValidatorExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/Editor/SceneValidatorExts.cs
@@ -24,6 +24,9 @@ namespace com.tinylabproductions.TLPLib.Utilities.Editor {
   }
 
   public static class SceneValidatorExts {
+    public static SceneWithValidator withValidator(this ScenePath path, SceneValidator validator) =>
+      SceneWithValidator.a(path, validator);
+
     public static SceneValidator join(
       this SceneValidator a, SceneValidator b
     ) => scene => a(scene).AddRange(b(scene));
