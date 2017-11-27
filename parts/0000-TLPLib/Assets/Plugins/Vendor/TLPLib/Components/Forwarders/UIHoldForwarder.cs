60143e9d74f3947f7593c077e2a8dab5f5428776
Ignotas <ignotas.m@gmail.com>
2017-11-27 10:51:01 +0200

    Hold forwarder

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/UIHoldForwarder.cs
@@ -0,0 +1,25 @@
+﻿using com.tinylabproductions.TLPLib.Components.Interfaces;
+using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace com.tinylabproductions.TLPLib.Components {
+  public class UIHoldForwarder : MonoBehaviour, IMB_Update, IPointerDownHandler, IPointerUpHandler {
+    readonly IRxRef<bool> _isHeldDown = RxRef.a(false);
+    public IRxVal<bool> isHeldDown => _isHeldDown;
+
+    readonly Subject<Unit> _onHold = new Subject<Unit>();
+    public IObservable<Unit> onHold => _onHold;
+
+    public void Update() {
+      if (isHeldDown.value) _onHold.push(F.unit);
+    }
+
+    public void OnPointerDown(PointerEventData eventData) => 
+      _isHeldDown.value = true;
+
+    public void OnPointerUp(PointerEventData eventData) => 
+      _isHeldDown.value = false;
+  }
+}
