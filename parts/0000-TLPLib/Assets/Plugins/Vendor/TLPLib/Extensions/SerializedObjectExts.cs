0de9ec44ecde0bebc84efa4d3ffb1b2b3c841d6e
Astro75 <evaldas757@gmail.com>
2017-11-27 11:49:57 +0200

    SerializedObjectExts

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/SerializedObjectExts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class SerializedObjectExts {
+    public static IEnumerable<SerializedProperty> iterate(
+      this SerializedObject so, bool enterChildren
+    ) {
+      var sp = so.GetIterator();
+      while (sp.Next(enterChildren)) yield return sp;
+    }
+
+    public static IEnumerable<SerializedProperty> iterateVisible(
+      this SerializedObject so, bool enterChildren
+    ) {
+      var sp = so.GetIterator();
+      while (sp.NextVisible(enterChildren)) yield return sp;
+    }
+  }
+}
