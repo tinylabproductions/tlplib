2ad0023c5151c7d2e817f2c48f2a8c74656c294c
Ignotas <ignotas.m@gmail.com>
2017-12-08 16:04:49 +0200

    fix

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Dispose/DisposeTracker.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Dispose/DisposeTracker.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection;
 
 namespace com.tinylabproductions.TLPLib.dispose {
   public interface IDisposeTracker<in A> : IDisposable {
