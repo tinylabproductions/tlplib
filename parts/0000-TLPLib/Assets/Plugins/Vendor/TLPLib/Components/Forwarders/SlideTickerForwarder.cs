94f49dd047909bbb8fff3f910ead69dc8c475c28
Donatas <laurinavicius.donatas@gmail.com>
2017-12-15 13:08:58 +0200

    SlideTickerForwarder to tlplib

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/SlideTickerForwarder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Code.UI {
+  public class SlideTickerForwarder : MonoBehaviour, IBeginDragHandler, IDragHandler {
+    public float tickDistance;
+
+    readonly Subject<Tpl<int, int>> _onSlideTick = new Subject<Tpl<int, int>>();
+    public IObservable<Tpl<int, int>> onSlideTick => _onSlideTick;
+    Vector2 dragBeginPos;
+
+    public void OnBeginDrag(PointerEventData eventData) {
+      if (!mayDrag(eventData)) return;
+      dragBeginPos = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+      if (!mayDrag(eventData)) return;
+      var delta = eventData.position - dragBeginPos;
+      var tickX = calcAxisTicks(delta.x, tickDistance);
+      var tickY = calcAxisTicks(delta.y, tickDistance);
+      if (tickX != 0 || tickY != 0) {
+        dragBeginPos += new Vector2(tickX * tickDistance, tickY * tickDistance);
+        _onSlideTick.push(F.t(tickX, tickY));
+      }
+    }
+
+    static int calcAxisTicks(float delta, float tickDistance) {
+      var ticks = Mathf.FloorToInt(Mathf.Abs(delta) / tickDistance);
+      return delta >= 0 ? ticks : -ticks;
+    }
+
+    static bool mayDrag(PointerEventData eventData) =>
+      eventData.button == PointerEventData.InputButton.Left;
+  }
+}
