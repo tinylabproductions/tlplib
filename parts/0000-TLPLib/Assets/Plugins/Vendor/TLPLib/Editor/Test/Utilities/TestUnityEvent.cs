2e3f7497bbb170cf22ad3c4f82a09b16716dd150
Arturas Slajus <arturas@tinylabproductions.com>
2017-12-27 11:51:48 +0200

    Fix compiler warnings.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Test/Utilities/TestUnityEvent.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Test/Utilities/TestUnityEvent.cs
@@ -3,7 +3,11 @@ using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Events;
 
-internal class TestUnityEvent : ScriptableObject {
+class TestUnityEvent : ScriptableObject {
+  #region Unity Serialized Fields
+
+#pragma warning disable 649, 169
+  // ReSharper disable NotNullMemberIsNotInitialized, FieldCanBeMadeReadOnly.Local, ConvertToConstant.Local
   [Serializable] class NestedEvent {
     [SerializeField, UsedImplicitly] UnityEvent unityEvent;
   }
@@ -11,4 +15,8 @@ internal class TestUnityEvent : ScriptableObject {
   [SerializeField, UsedImplicitly] UnityEvent unityEvent;
   [SerializeField, UsedImplicitly] NestedEvent nestedEvent;
   [SerializeField, UsedImplicitly] NestedEvent[] nestedEventArray;
+  // ReSharper restore NotNullMemberIsNotInitialized, FieldCanBeMadeReadOnly.Local, ConvertToConstant.Local
+#pragma warning restore 649, 169
+
+  #endregion
 }
