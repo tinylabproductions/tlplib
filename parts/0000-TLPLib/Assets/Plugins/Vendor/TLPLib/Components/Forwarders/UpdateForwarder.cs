9e9f44516914fd2d5c8d81aea0c0a95580726417
Arnas Janulionis <arnas.janulionis@gmail.com>
2018-01-05 18:27:41 +0200

    added update forwarder

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/UpdateForwarder.cs
@@ -0,0 +1,9 @@
+﻿using com.tinylabproductions.TLPLib.Components.Forwarders;
+using com.tinylabproductions.TLPLib.Components.Interfaces;
+using com.tinylabproductions.TLPLib.Functional;
+
+namespace com.tinylabproductions.TLPLib.Components {
+  public class UpdateForwarder : EventForwarder<Unit>, IMB_Update {
+    public void Update() => _onEvent.push(F.unit);
+  }
+}
