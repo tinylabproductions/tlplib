ffb28a641638228075232e3edcd3e0209fe3d6ae
Karolis <karolisjc@gmail.com>
2018-01-05 16:55:40 +0200

    customizable vertical layout

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/OnRectTransformDimensionsChangeForwarder.cs
@@ -0,0 +1,15 @@
+﻿using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace com.tinylabproductions.TLPLib.Components.Forwarders {
+  public class OnRectTransformDimensionsChangeForwarder : UIBehaviour {
+    readonly Subject<Unit> _rectDimensionsChanged = new Subject<Unit>();
+
+    public IObservable<Unit> rectDimensionsChanged => _rectDimensionsChanged;
+    public RectTransform rectTransform => (RectTransform) transform;
+     
+    protected override void OnRectTransformDimensionsChange() => _rectDimensionsChanged.push(F.unit);
+  }
+}
---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/ui/CustomizableVerticalLayout.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using com.tinylabproductions.TLPLib.Components.Forwarders;
+using com.tinylabproductions.TLPLib.Data;
+using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Reactive;
+using com.tinylabproductions.TLPLib.Utilities;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.tinylabproductions.TLPLib.Components.ui {
+  public class CustomizableVerticalLayout : MonoBehaviour{
+    #region Unity Serialized Fields
+
+#pragma warning disable 649
+// ReSharper disable NotNullMemberIsNotInitialized, FieldCanBeMadeReadOnly.Local
+    [SerializeField, NotNull] ScrollRect _scrollRect;
+    [SerializeField, NotNull] RectTransform _container, _maskRect;
+// ReSharper restore NotNullMemberIsNotInitialized, FieldCanBeMadeReadOnly.Local
+#pragma warning restore 649
+
+    #endregion
+
+    public OnRectTransformDimensionsChangeForwarder container => _container.gameObject.EnsureComponent<OnRectTransformDimensionsChangeForwarder>();
+    public OnRectTransformDimensionsChangeForwarder mask => _maskRect.gameObject.EnsureComponent<OnRectTransformDimensionsChangeForwarder>();
+
+    public interface ILayoutItem : IDisposable {
+      RectTransform rectTransform { get; }
+    }
+
+    public interface IData {
+      float height { get; }
+      Percentage width { get; }
+      ILayoutItem createItem(Transform parent);
+    }
+
+    public class Init {
+      const float EPS = 1e-9f;
+
+      readonly CustomizableVerticalLayout backing;
+      readonly ImmutableArray<IData> listData;
+      readonly IRxVal<Rect> maskSize;
+      readonly Dictionary<IData, ILayoutItem> items = new Dictionary<IData, ILayoutItem>();
+      readonly OnRectTransformDimensionsChangeForwarder container;
+
+      public Init(
+        CustomizableVerticalLayout backing, 
+        ImmutableArray<IData> listData
+      ) {
+        this.backing = backing;
+        this.listData = listData;
+
+        container = backing.container;
+
+        // We need oncePerFrame() because Unity doesn't allow doing opperations like gameObject.SetActive() 
+        // from OnRectTransformDimensionsChange()
+        var mask = backing.mask;
+        maskSize = 
+          mask.rectDimensionsChanged.oncePerFrame().map(_ => mask.rectTransform.rect).toRxVal(mask.rectTransform.rect);
+
+        maskSize.subscribe(_ => {
+          clearLayout();
+          updateLayout();
+        });
+        
+        backing._scrollRect.onValueChanged.AddListener(_ => updateLayout());
+      }
+
+      void clearLayout() {
+        foreach (var kv in items) {
+          kv.Value.Dispose();
+        }
+        items.Clear();
+      }
+
+      void updateLayout() {
+        var visibleRect = backing.mask.rectTransform.rect.convertCoordinateSystem(
+          backing.mask.rectTransform, backing.container.rectTransform
+        );
+
+        var height = 0f;
+        var currentWidthPerc = 0f;
+        foreach (var data in listData) {
+          var itemWidthPerc = data.width.value;
+          var itemLeftPerc = 0f;
+          if (currentWidthPerc + itemWidthPerc > 1f + EPS) {
+            currentWidthPerc = itemWidthPerc;
+          }
+          else {
+            itemLeftPerc = currentWidthPerc;
+            currentWidthPerc += itemWidthPerc;
+          }
+
+          if (Mathf.Approximately(itemLeftPerc, 0f)) height += data.height;
+
+          var x = itemLeftPerc * maskSize.value.width;
+          var cellRect = Rect.MinMaxRect(x, -height, x + maskSize.value.width * itemWidthPerc, -height + data.height) ;
+
+          var placementVisible = visibleRect.Overlaps(cellRect, true);
+
+          if (placementVisible && !items.ContainsKey(data)) {
+            var instance = data.createItem(backing._container);
+            var rectTrans = instance.rectTransform;
+            rectTrans.anchorMin = Vector2.up;
+            rectTrans.anchorMax = Vector2.up;
+            rectTrans.localPosition = Vector3.zero;
+            rectTrans.anchoredPosition = cellRect.center;
+            items.Add(data, instance);
+          }
+          else if (!placementVisible && items.ContainsKey(data)) {
+            var item = items[data];
+            items.Remove(data);
+            item.Dispose();
+          }
+        }
+
+        if (!Mathf.Approximately(container.rectTransform.rect.height, height)) {
+          container.rectTransform.sizeDelta = new Vector2(container.rectTransform.rect.width, height);
+        }
+      }
+    }
+  }
+}
