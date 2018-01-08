a75c1e9d1dec883a1ba72b7528e2f9c3d6d4fc43
Arnas Janulionis <arnas.janulionis@gmail.com>
2018-01-08 11:52:16 +0200

    UpdateForwarder documentation

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/UpdateForwarder.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/UpdateForwarder.cs
@@ -3,6 +3,11 @@ using com.tinylabproductions.TLPLib.Components.Interfaces;
 using com.tinylabproductions.TLPLib.Functional;
 
 namespace com.tinylabproductions.TLPLib.Components {
+  /**
+   * UpdateForwarder addresses an issue where ASync.EveryFrame creates a new
+   * coroutine and drops it if GameObject is disabled.
+   * UpdateForwarder does not drop subscriptions if GameObject is disabled.
+   */
   public class UpdateForwarder : EventForwarder<Unit>, IMB_Update {
     public void Update() => _onEvent.push(F.unit);
   }
