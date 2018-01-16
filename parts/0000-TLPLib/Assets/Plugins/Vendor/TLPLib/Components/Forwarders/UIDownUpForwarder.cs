c77d36e5cb3131633a428babb1ac22561a1c974f
gyts <gytis.stanciukas@gmail.com>
2018-01-16 11:00:43 +0200

    up onDisable & down onEnable if was down when disabling

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/UIDownUpForwarder.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/UIDownUpForwarder.cs
@@ -1,27 +1,46 @@
-﻿using com.tinylabproductions.TLPLib.Functional;
+﻿using com.tinylabproductions.TLPLib.Components.Interfaces;
+using com.tinylabproductions.TLPLib.Functional;
 using com.tinylabproductions.TLPLib.Reactive;
 using UnityEngine.EventSystems;
 
 namespace com.tinylabproductions.TLPLib.Components {
-  public class UIDownUpForwarder : UIBehaviour, IPointerDownHandler, IPointerUpHandler {
+  public class UIDownUpForwarder : UIBehaviour, IPointerDownHandler, IPointerUpHandler, IMB_OnEnable, IMB_OnDisable {
     readonly Subject<Unit> _onDown = new Subject<Unit>();
     readonly Subject<Unit> _onUp = new Subject<Unit>();
     public IObservable<Unit> onDown => _onDown;
     public IObservable<Unit> onUp => _onUp;
     public bool isDown { get; private set; }
 
+    bool disabledWithOnDown;
+
+    void down() {
+      _onDown.push(new Unit());
+      isDown = true;
+    }
+
+    void up() {
+      _onUp.push(new Unit());
+      isDown = false;
+    }
+
     public void OnPointerDown(PointerEventData eventData) {
-      if (eventData.button == PointerEventData.InputButton.Left && IsActive()) {
-        _onDown.push(new Unit());
-        isDown = true;
-      }
+      if (eventData.button == PointerEventData.InputButton.Left && IsActive())
+        down();
     }
 
     public void OnPointerUp(PointerEventData eventData) {
-      if (eventData.button == PointerEventData.InputButton.Left && IsActive()) {
-        _onUp.push(new Unit());
-        isDown = false;
-      }
+      if (eventData.button == PointerEventData.InputButton.Left && IsActive())
+        up();
+    }
+
+    public new void OnEnable() {
+      if (disabledWithOnDown)
+        down();
+    }
+
+    public new void OnDisable() {
+      disabledWithOnDown = isDown;
+      up();
     }
   }
 }
