8b592ed7116171ecb05830dcac5f345852562e6f
Karolis <karolisjc@gmail.com>
2018-01-17 14:27:30 +0200

    UnityEventExts

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/UnityEventExts.cs
@@ -0,0 +1,12 @@
+﻿using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class UnityEventExts {
+    public static ISubscription subscribe(this UnityEvent<Vector2> rectEvent, UnityAction<Vector2> act) {
+      rectEvent.AddListener(act);
+      return new Subscription(() => rectEvent.RemoveListener(act));
+    }
+  }
+}
