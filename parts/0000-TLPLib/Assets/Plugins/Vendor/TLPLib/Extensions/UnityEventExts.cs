8f309db88687d2c7f011e7c3d21c8705b736a65b
Karolis <karolisjc@gmail.com>
2018-01-17 14:34:12 +0200

    container width instead of mask width

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/UnityEventExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/UnityEventExts.cs
@@ -1,10 +1,9 @@
 ﻿using com.tinylabproductions.TLPLib.Reactive;
-using UnityEngine;
 using UnityEngine.Events;
 
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class UnityEventExts {
-    public static ISubscription subscribe(this UnityEvent<Vector2> rectEvent, UnityAction<Vector2> act) {
+    public static ISubscription subscribe<A>(this UnityEvent<A> rectEvent, UnityAction<A> act) {
       rectEvent.AddListener(act);
       return new Subscription(() => rectEvent.RemoveListener(act));
     }
