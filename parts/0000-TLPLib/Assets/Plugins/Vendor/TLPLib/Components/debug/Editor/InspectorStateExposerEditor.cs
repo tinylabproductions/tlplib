74004fdf6390c8836d54e48a2deaff16bf48cbb2
Arturas Slajus <arturas@tinylabproductions.com>
2018-01-23 11:39:26 +0200

    InspectorStateExposer

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/debug/Editor/InspectorStateExposerEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Components.debug {
+  [CustomEditor(typeof(InspectorStateExposer))]
+  public class InspectorStateExposerEditor : UnityEditor.Editor {
+    public override void OnInspectorGUI() {
+      var so = (InspectorStateExposer) serializedObject.targetObject;
+      var first = true;
+      foreach (var group in so.groupedData) {
+        if (!first) GUILayoutUtility.GetRect(24f, 24f);
+        EditorGUILayout.LabelField($"For {group.Key} ({group.Key.GetHashCode()}):");
+        EditorGUILayout.Space();
+        foreach (var data in group) {
+          data.value.voidMatch(
+            str => EditorGUILayout.LabelField(data.name, str.value),
+            flt => EditorGUILayout.FloatField(data.name, flt.value),
+            obj => EditorGUILayout.ObjectField(data.name, obj.value, typeof(Object), allowSceneObjects: true)
+          );
+        }
+
+        first = false;
+      }
+      
+      // Update inspector every frame.
+      Repaint();
+    }
+  }
+}
