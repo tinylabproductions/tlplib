71644f5a40aadb5ef13027c103a3f5a58cd46402
Arturas Slajus <arturas@tinylabproductions.com>
2018-01-24 11:11:49 +0200

    NeverDisposeDisposableTracker

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Dispose/DisposeTracker.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Dispose/DisposeTracker.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using com.tinylabproductions.TLPLib.Components.debug;
 using com.tinylabproductions.TLPLib.Data.typeclasses;
-using com.tinylabproductions.TLPLib.Reactive;
+using com.tinylabproductions.TLPLib.Extensions;
 using GenerationAttributes;
-using JetBrains.Annotations;
 
 namespace com.tinylabproductions.TLPLib.dispose {
   [Record]
@@ -70,5 +70,35 @@ namespace com.tinylabproductions.TLPLib.dispose {
     public int trackedCount => 0;
     public IEnumerable<TrackedDisposable> trackedDisposables => Enumerable.Empty<TrackedDisposable>();
     public void Dispose() {}
+  }
+
+  /// <summary>
+  /// Tracker that allows you to register a subscription to be kept forever.
+  /// 
+  /// This should only be used for things that should never go out 
+  /// </summary>
+  public class NeverDisposeDisposableTracker : IDisposableTracker {
+    public static readonly IDisposableTracker instance = new NeverDisposeDisposableTracker();
+
+    readonly List<TrackedDisposable> list = new List<TrackedDisposable>();
+    public int trackedCount => list.Count;
+    public IEnumerable<TrackedDisposable> trackedDisposables => list;
+    
+    NeverDisposeDisposableTracker() {
+#if UNITY_EDITOR
+      var go = new UnityEngine.GameObject(nameof(NeverDisposeDisposableTracker));
+      go.exposeToInspector(this, nameof(trackedCount), _ => _.trackedCount);
+      go.exposeToInspector(this, nameof(list), _ => _.list.Select(d => d.asString()).mkString("\n"));
+#endif
+    }
+
+    public void Dispose() {}
+
+    public void track(
+      IDisposable a,
+      [CallerMemberName] string callerMemberName = "", 
+      [CallerFilePath] string callerFilePath = "", 
+      [CallerLineNumber] int callerLineNumber = 0
+    ) => list.Add(new TrackedDisposable(a, callerMemberName, callerFilePath, callerLineNumber));
   }
 }
