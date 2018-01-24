5e83b74e2dee7a2e1291163045421f224ea8e69f
Arturas Slajus <arturas@tinylabproductions.com>
2018-01-24 15:40:36 +0200

    Make TextureToGameObjectImporter more user friendly.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/import/TextureToGameObjectImporterEditor.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/import/TextureToGameObjectImporterEditor.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using AdvancedInspector;
 using com.tinylabproductions.TLPLib.Editor.Utils;
 using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Functional;
 using com.tinylabproductions.TLPLib.Logger;
 using UnityEditor;
 using UnityEngine;
@@ -17,15 +18,46 @@ namespace com.tinylabproductions.TLPLib.import {
       base.OnInspectorGUI();
       EditorGUILayout.Space();
       if (GUILayout.Button("Generate")) {
+        if (!obj.texture) {
+          EditorUtils.userInfo(
+            "Texture not set!", "Please set the texture before generating.", Log.Level.ERROR
+          );
+          return;
+        }
+        
         var width = obj.texture.width;
         var height = obj.texture.height;
         var unknownColorsFound = new HashSet<Color32>();
         const byte maxAlpha = byte.MaxValue;
         var ignoredColors = obj.ignoredColors.Select(_ => _.with32Alpha(maxAlpha)).toHashSet();
+        var dictV = obj.pallete
+          .GroupBy(_ => _.color.with32Alpha(maxAlpha))
+          .Select(group => {
+            var gameObjects = group.Select(_ => _.gameObject).ToArray();
+            return (
+              gameObjects.Length == 1
+              ? Either<string, KeyValuePair<Color32, GameObject>>.Right(F.kv(
+                group.Key, gameObjects[0]
+              ))
+              : Either<string, KeyValuePair<Color32, GameObject>>.Left(
+                $"More than 1 game object found for #{group.Key.toHex()}: " +
+                $"{gameObjects.Select(_ => _.nameOrNull()).mkStringEnum()}"
+              )
+            ).asValidation();
+          })
+          .sequenceValidations();
+        if (dictV.isLeft) {
+          EditorUtils.userInfo(
+            "Invalid pallete!",
+            dictV.__unsafeGetLeft.mkString("\n"),
+            Log.Level.ERROR
+          );
+          return;
+        }
+        var dict = dictV.__unsafeGetRight.toDict();
         
         using (var progress = new EditorProgress("Generating objects")) {
           var pixels = progress.execute("Getting pixels", obj.texture.GetPixels32);
-          var dict = obj.pallete.toDict(_ => _.color.with32Alpha(maxAlpha), _ => _.gameObject);
           var parent = new GameObject(obj.holderGameObjectName).transform;
 
           progress.execute("Reading pixels", () => {
