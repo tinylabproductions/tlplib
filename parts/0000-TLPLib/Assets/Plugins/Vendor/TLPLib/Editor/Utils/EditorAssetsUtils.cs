3d06d958deaec88f3a42d6eeb0cfb1ec0fc1c460
Astro75 <evaldas757@gmail.com>
2018-01-29 16:46:22 +0200

    reserializeAllAssets: display confirmation dialog

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/EditorAssetsUtils.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/EditorAssetsUtils.cs
@@ -5,6 +5,8 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
   class EditorAssetsUtils {
     [UsedImplicitly, MenuItem("TLP/Tools/Reserialize all assets")]
     static void reserializeAllAssets() {
+      if (!EditorUtility.DisplayDialog("Slow operation", "Do you really want to reserialize all assets?", "Yes", "No"))
+        return;
       using (var editorProgress = new EditorProgress("Reserializing All Assets")) { 
         var assetsPaths = editorProgress.execute("Loading all assets", AssetDatabase.GetAllAssetPaths);
         
