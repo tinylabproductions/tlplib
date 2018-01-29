e664750937b09307aa862159b0ee74ac75e235e9
Astro75 <evaldas757@gmail.com>
2018-01-29 17:01:36 +0200

    Added TLP/Tools/Reserialize all scenes

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/EditorAssetsUtils.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/EditorAssetsUtils.cs
@@ -1,11 +1,12 @@
-﻿using JetBrains.Annotations;
+﻿using com.tinylabproductions.TLPLib.Utilities;
+using JetBrains.Annotations;
 using UnityEditor;
 
 namespace com.tinylabproductions.TLPLib.Editor.Utils {
   class EditorAssetsUtils {
     [UsedImplicitly, MenuItem("TLP/Tools/Reserialize all assets")]
     static void reserializeAllAssets() {
-      if (!EditorUtility.DisplayDialog("Slow operation", "Do you really want to reserialize all assets?", "Yes", "No"))
+      if (!EditorUtility.DisplayDialog("Slow operation", "Do you really want to reserialize all ASSETS?", "Yes", "No"))
         return;
       using (var editorProgress = new EditorProgress("Reserializing All Assets")) { 
         var assetsPaths = editorProgress.execute("Loading all assets", AssetDatabase.GetAllAssetPaths);
@@ -21,6 +22,14 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
         
         editorProgress.execute("Saving reserialized assets", AssetDatabase.SaveAssets);
       }
+    }
+    
+    [UsedImplicitly, MenuItem("TLP/Tools/Reserialize all scenes")]
+    static void reserializeAllScenes() {
+      if (!EditorUtility.DisplayDialog("Slow operation", "Do you really want to reserialize all SCENES?", "Yes", "No"))
+        return;
+
+      SceneUtils.modifyAllScenesInProject(scene => true);
     }
   }
 }
