5762b8485818428d3bd178bb50bfab2fb9a885a9
Astro75 <evaldas757@gmail.com>
2018-01-30 11:32:35 +0200

    Fix runtime errors

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/InvisibleUIClickEventsReceiver.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/InvisibleUIClickEventsReceiver.cs
@@ -8,12 +8,17 @@ namespace com.tinylabproductions.TLPLib.Components {
   /// Allows to receive UI click events even if we do not have a visible part for this UI object.
   /// </summary>
   public class InvisibleUIClickEventsReceiver : Graphic {
+    InvisibleUIClickEventsReceiver() {
+      useLegacyMeshGeneration = false;
+    }
+
     // http://forum.unity3d.com/threads/recttransform-and-events.285740/
     // Do not generate mesh (do not call base).
     [Obsolete] protected override void OnFillVBO(List<UIVertex> vbo) {}
 
 #if !UNITY_5_1 && !UNITY_5_0
     [Obsolete] protected override void OnPopulateMesh(Mesh m) => m.Clear();
+    protected override void OnPopulateMesh(VertexHelper vh) { }
 #endif
   }
 }
