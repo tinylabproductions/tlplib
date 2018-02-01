a07757c910b2b5a629e580a86884f68b90cc69e2
gyts <gytis.stanciukas@gmail.com>
2018-02-01 15:50:49 +0200

    Fixed > 1 pointers bug & pushing position instead of unit

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/UIHoldForwarder.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/UIHoldForwarder.cs
@@ -1,4 +1,7 @@
-﻿using com.tinylabproductions.TLPLib.Components.Interfaces;
+﻿using System.Collections.Generic;
+using System.Linq;
+using com.tinylabproductions.TLPLib.Components.Interfaces;
+using com.tinylabproductions.TLPLib.Extensions;
 using com.tinylabproductions.TLPLib.Functional;
 using com.tinylabproductions.TLPLib.Reactive;
 using UnityEngine;
@@ -6,20 +9,29 @@ using UnityEngine.EventSystems;
 
 namespace com.tinylabproductions.TLPLib.Components {
   public class UIHoldForwarder : MonoBehaviour, IMB_Update, IPointerDownHandler, IPointerUpHandler {
-    readonly IRxRef<bool> _isHeldDown = RxRef.a(false);
-    public IRxVal<bool> isHeldDown => _isHeldDown;
+    readonly IRxRef<Option<Vector2>> _isHeldDown = RxRef.a(F.none<Vector2>());
+    public IRxVal<Option<Vector2>> isHeldDown => _isHeldDown;
 
-    readonly Subject<Unit> _onHoldEveryFrame = new Subject<Unit>();
-    public IObservable<Unit> onHoldEveryFrame => _onHoldEveryFrame;
+    readonly Subject<Vector2> _onHoldEveryFrame = new Subject<Vector2>();
+    public IObservable<Vector2> onHoldEveryFrame => _onHoldEveryFrame;
 
+    readonly List<PointerEventData> pointerData = new List<PointerEventData>();
+    
     public void Update() {
-      if (isHeldDown.value) _onHoldEveryFrame.push(F.unit);
+      if (isHeldDown.value)
+        _onHoldEveryFrame.push(pointerData.Last().position);
     }
 
-    public void OnPointerDown(PointerEventData eventData) => 
-      _isHeldDown.value = true;
+    public void OnPointerDown(PointerEventData eventData) {
+      pointerData.Add(eventData);
+      _isHeldDown.value = eventData.position.some();
+    }
 
-    public void OnPointerUp(PointerEventData eventData) => 
-      _isHeldDown.value = false;
+    public void OnPointerUp(PointerEventData eventData) {
+      pointerData.Remove(eventData);
+      if (pointerData.isEmpty()) {
+        _isHeldDown.value = _isHeldDown.value.none;
+      }
+    }
   }
 }
