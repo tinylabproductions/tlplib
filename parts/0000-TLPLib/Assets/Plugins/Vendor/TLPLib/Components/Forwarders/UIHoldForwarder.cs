6c74eae90bc50e417e86510d1906e847ab6dad7e
Arturas Slajus <arturas@tinylabproductions.com>
2018-02-06 14:23:08 +0200

    Small refactoring.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/UIHoldForwarder.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/UIHoldForwarder.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using com.tinylabproductions.TLPLib.Components.Interfaces;
 using com.tinylabproductions.TLPLib.Extensions;
 using com.tinylabproductions.TLPLib.Functional;
@@ -15,11 +14,28 @@ namespace com.tinylabproductions.TLPLib.Components {
     readonly Subject<Vector2> _onHoldEveryFrame = new Subject<Vector2>();
     public IObservable<Vector2> onHoldEveryFrame => _onHoldEveryFrame;
 
+    /// <summary>
+    /// We need a list here, because:
+    /// * If we made several touches and then released some of them, we need to
+    ///   set isHeldDown to the last touch position we still have. Therefore we
+    ///   cannot use a counter. 
+    /// * we can release touches in different order than we pressed. Therefore
+    ///   we cannot use a stack.
+    /// 
+    /// The amount of elements should be small (amount of simultaneous touches
+    /// recognized by a device, which is usually less than 10), therefore the
+    /// performance gains from switching to other data structure would be
+    /// negligable here.
+    /// </summary>
     readonly List<PointerEventData> pointerData = new List<PointerEventData>();
     
     public void Update() {
-      if (isHeldDown.value)
-        _onHoldEveryFrame.push(pointerData.Last().position);
+      if (pointerData.isEmpty()) return;
+      
+      // HOT CODE: We explicitly use indexing instead of LINQ .Last() to make
+      // sure this performs well and does not do unnecessary checks.
+      var lastPointer = pointerData[pointerData.Count - 1];
+      _onHoldEveryFrame.push(lastPointer.position);
     }
 
     public void OnPointerDown(PointerEventData eventData) {
