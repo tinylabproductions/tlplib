f25f30d299c144bb9b037a031a48a467339f1937
Arturas Slajus <arturas@tinylabproductions.com>
2018-03-14 18:30:48 +0200

    IPlatformPackageManager#packageNames

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Platform/AndroidPlatformPackageManager.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Platform/AndroidPlatformPackageManager.cs
@@ -2,14 +2,13 @@
 using System.Collections.Immutable;
 using System.Linq;
 using com.tinylabproductions.TLPLib.Android;
-using com.tinylabproductions.TLPLib.Android.Bindings.android.content;
 using com.tinylabproductions.TLPLib.Android.Bindings.android.content.pm;
 using com.tinylabproductions.TLPLib.Data;
 using com.tinylabproductions.TLPLib.Functional;
 
 namespace com.tinylabproductions.TLPLib.Platform {
   class AndroidPlatformPackageManager : IPlatformPackageManager {
-    readonly ImmutableSortedSet<string> packageNames;
+    public ImmutableSortedSet<string> packageNames { get; }
 
     public AndroidPlatformPackageManager() {
       //Cache all of the package names for better performance
--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Platform/PlatformPackageManager.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Platform/PlatformPackageManager.cs
@@ -1,11 +1,14 @@
-﻿using com.tinylabproductions.TLPLib.Data;
+﻿using System.Collections.Immutable;
+using com.tinylabproductions.TLPLib.Data;
 using com.tinylabproductions.TLPLib.Functional;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Platform {
   public interface IPlatformPackageManager {
-    bool hasAppInstalled(string bundleIdentifier);
-    Option<ErrorMsg> openApp(string bundleIdentifier);
+    [PublicAPI] ImmutableSortedSet<string> packageNames { get; }
+    [PublicAPI] bool hasAppInstalled(string bundleIdentifier);
+    [PublicAPI] Option<ErrorMsg> openApp(string bundleIdentifier);
   }
 
   public static class PlatformPackageManager {
@@ -20,6 +23,7 @@ namespace com.tinylabproductions.TLPLib.Platform {
   }
 
   class NoOpPlatformPackageManager : IPlatformPackageManager {
+    public ImmutableSortedSet<string> packageNames => ImmutableSortedSet<string>.Empty;
     public bool hasAppInstalled(string bundleIdentifier) => false;
     public Option<ErrorMsg> openApp(string bundleIdentifier) => Option<ErrorMsg>.None;
   }
