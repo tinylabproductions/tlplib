1ba65cdebce907ddd73ae5993dab7aeea6ee1b5c
Arturas Slajus <arturas@tinylabproductions.com>
2018-04-04 16:43:22 +0300

    ImmutableList<A>#Add(Option<A>)

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/ImmutableListExts.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Immutable;
+using com.tinylabproductions.TLPLib.Functional;
+using JetBrains.Annotations;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class ImmutableListExts {
+    [PublicAPI] public static ImmutableList<A> Add<A>(
+      this ImmutableList<A> list, Option<A> maybeA
+    ) => maybeA.isSome ? list.Add(maybeA.__unsafeGetValue) : list;
+  }
+}
