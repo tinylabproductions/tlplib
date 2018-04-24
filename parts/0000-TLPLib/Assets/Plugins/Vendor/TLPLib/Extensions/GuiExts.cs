97fa95c5bb1b2e3f60f5aac6cee7fb017fb6314e
Donatas <laurinavicius.donatas@gmail.com>
2018-04-24 09:50:18 +0300

    Added Gui conversion to uLong

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/GuiExts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Plugins.Vendor.TLPLib.Extensions {
+  public static class GuiExts {
+    public static ulong asULong(this Guid g) {
+      var bytes = g.ToByteArray();
+      return unchecked (BitConverter.ToUInt64(bytes, 0) + BitConverter.ToUInt64(bytes, 8));
+    }
+  }
+}
