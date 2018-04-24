aa6bc1865573f0ecf1eb7eadc11398da5f360af9
Donatas <laurinavicius.donatas@gmail.com>
2018-04-24 10:04:53 +0300

    Using byteArrayExts

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/GuidExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/GuidExts.cs
@@ -4,9 +4,6 @@ using JetBrains.Annotations;
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class GuidExts {
     [PublicAPI]
-    public static ulong asULong(this Guid g) {
-      var bytes = g.ToByteArray();
-      return unchecked (BitConverter.ToUInt64(bytes, 0) + BitConverter.ToUInt64(bytes, 8));
-    }
+    public static ulong asULong(this Guid g) => g.ToByteArray().guidAsULong();
   }
 }
