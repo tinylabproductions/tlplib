e52c5fd4ebccafbd0487312eb98e18de18a69e34
Arturas Slajus <arturas@tinylabproductions.com>
2018-04-27 12:04:59 +0300

    Fix the comments.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/EditorInspectorUtils.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/Utils/EditorInspectorUtils.cs
@@ -4,6 +4,7 @@ using com.tinylabproductions.TLPLib.reflection;
 using JetBrains.Annotations;
 using UnityEditor;
 using UnityEngine;
+using Debug = System.Diagnostics.Debug;
 using Object = UnityEngine.Object;
 
 namespace com.tinylabproductions.TLPLib.Editor.Utils {
@@ -17,6 +18,7 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
       var inspectorType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.InspectorWindow");
       // Create an InspectorWindow instance
       var inspectorInstance = ScriptableObject.CreateInstance(inspectorType) as EditorWindow;
+      Debug.Assert(inspectorInstance != null, nameof(inspectorInstance) + " != null");
       // We display it - currently, it will inspect whatever gameObject is currently selected
       // So we need to find a way to let it inspect/aim at our target GO that we passed
       // For that we do a simple trick:
@@ -32,6 +34,7 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
       // Get a ref to the public "locked" property, which will lock the state of the inspector to the current inspected target
       var isLocked = inspectorType.GetProperty("isLocked", BindingFlags.Instance | BindingFlags.Public);
       // Invoke `isLocked` setter method passing 'true' to lock the inspector
+      Debug.Assert(isLocked != null, nameof(isLocked) + " != null");
       isLocked.GetSetMethod().Invoke(inspectorInstance, new object[] { true });
       // Finally revert back to the previous selection so that other inspectors continue to inspect whatever they were inspecting...
       Selection.activeGameObject = prevSelection;
@@ -51,13 +54,7 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
       // Get a reference to the `InspectorWindow` type object
       var windowType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.ProjectBrowser");
       var windowInstance = ScriptableObject.CreateInstance(windowType) as EditorWindow;
-      // We display it - currently, it will inspect whatever gameObject is currently selected
-      // So we need to find a way to let it inspect/aim at our target GO that we passed
-      // For that we do a simple trick:
-      // 1- Cache the current selected gameObject
-      // 2- Set the current selection to our target GO (so now all inspectors are targeting it)
-      // 3- Lock our created inspector to that target
-      // 4- Fallback to our previous selection
+      Debug.Assert(windowInstance != null, nameof(windowInstance) + " != null");
       windowInstance.Show();
       PrivateMethod.obtain(windowType, "Init")(windowInstance);
 
@@ -73,7 +70,6 @@ namespace com.tinylabproductions.TLPLib.Editor.Utils {
         )},
         true
       );
-      // Get a ref to the "locked" property, which will lock the state of the inspector to the current inspected target
       var isLockedFieldAccessor = PrivateField.accessor<bool>(windowType, "m_IsLocked"); 
       var isLockedField = isLockedFieldAccessor(windowInstance);
       isLockedField.value = true;
