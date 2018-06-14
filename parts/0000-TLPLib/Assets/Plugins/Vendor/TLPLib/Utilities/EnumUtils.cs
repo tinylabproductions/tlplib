d62d447bb07037389dd454058e6cc3c744ea105c
Arturas Slajus <arturas@tinylabproductions.com>
2018-06-14 15:50:19 +0300

    EnumUtils#checkOpt<A>

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/EnumUtils.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/EnumUtils.cs
@@ -4,18 +4,25 @@ using System.Collections.Immutable;
 using System.Linq;
 using com.tinylabproductions.TLPLib.Extensions;
 using com.tinylabproductions.TLPLib.Functional;
+using JetBrains.Annotations;
 
 namespace com.tinylabproductions.TLPLib.Utilities {
   public static class EnumUtils {
     static readonly Dictionary<Type, object> cache = new Dictionary<Type, object>();
 
-    public static ImmutableList<T> GetValues<T>() {
+    [PublicAPI] public static ImmutableHashSet<T> GetValues<T>() {
       var type = typeof(T);
-      var untyped = cache.getOrUpdate(type, () => Enum.GetValues(type).Cast<T>().ToImmutableList());
-      return (ImmutableList<T>) untyped;
+      var untyped = cache.getOrUpdate(type, () => Enum.GetValues(type).Cast<T>().ToImmutableHashSet());
+      return (ImmutableHashSet<T>) untyped;
     }
 
-    public static IEnumerable<Option<T>> GetValuesWithOption<T>() =>
+    [PublicAPI] public static Option<A> checkOpt<A>(A a) =>
+      GetValues<A>().Contains(a) ? F.some(a) : F.none_;
+
+    [PublicAPI] public static bool check<A>(A a) =>
+      GetValues<A>().Contains(a);
+
+    [PublicAPI] public static IEnumerable<Option<T>> GetValuesWithOption<T>() =>
       Option<T>.None.Yield().Concat(GetValues<T>().Select(F.some));
   }
 }
