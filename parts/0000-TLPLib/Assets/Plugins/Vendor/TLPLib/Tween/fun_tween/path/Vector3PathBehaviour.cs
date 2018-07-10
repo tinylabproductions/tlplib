21d11e7a4da637a4a070fa3897754c5e926bc72e
LukasB <lukas.bloznelis@gmail.com>
2018-07-10 11:23:00 +0300

    Extended path resolution range to 1000

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Tween/fun_tween/path/Vector3PathBehaviour.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Tween/fun_tween/path/Vector3PathBehaviour.cs
@@ -7,7 +7,7 @@ namespace com.tinylabproductions.TLPLib.Tween.fun_tween.path {
   public partial class Vector3PathBehaviour : MonoBehaviour {
     
     [SerializeField] bool _relative, _closed;
-    [SerializeField, Range(50, 500)] int pathResolution = 250; //TODO: check range
+    [SerializeField, Range(50, 1000)] int pathResolution = 250;
     [SerializeField] Vector3Path.InterpolationMethod _method;
     [SerializeField] List<Vector3> _nodes = new List<Vector3>();
 
@@ -19,6 +19,5 @@ namespace com.tinylabproductions.TLPLib.Tween.fun_tween.path {
       _path ?? (
         _path = new Vector3Path(_method, _closed, _nodes.ToImmutableArray(), _relative ? F.some(transform) : F.none_, pathResolution)
       );
-
   }
 }
