3d6c4a05ccdb296fb61fa3ac763945bdc39926bf
gyts <gytis.stanciukas@gmail.com>
2018-07-16 11:42:07 +0300

    Point2D as Serializable struct

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Data/Point2D.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Data/Point2D.cs
@@ -5,35 +5,25 @@ using JetBrains.Annotations;
 using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Data {
-  [Serializable, Record] public partial class Point2DSerializable {
-    #region Unity Serialized Fields
-#pragma warning disable 649
-// ReSharper disable FieldCanBeMadeReadOnly.Local
-    [SerializeField] int _x, _y;
-// ReSharper restore FieldCanBeMadeReadOnly.Local
-#pragma warning restore 649
-    #endregion
-
-    public Point2D toPoint2D() => new Point2D(_x, _y);
-  }
-
-  [Record(GenerateToString = false)]
+  [Record(GenerateToString = false), Serializable]
   public partial struct Point2D {
-    public readonly int x, y;
+    // ReSharper disable FieldCanBeMadeReadOnly.Local
+    [SerializeField, PublicAccessor] int _x, _y;
+    // ReSharper restore FieldCanBeMadeReadOnly.Local
 
-    [PublicAPI] public Point2D withX(int x) => new Point2D(x, y);
-    [PublicAPI] public Point2D withY(int y) => new Point2D(x, y);
+    [PublicAPI] public Point2D withX(int x) => new Point2D(x, _y);
+    [PublicAPI] public Point2D withY(int y) => new Point2D(_x, y);
 
-    [PublicAPI] public Point2D up => new Point2D(x, y+1);
-    [PublicAPI] public Point2D down => new Point2D(x, y-1);
-    [PublicAPI] public Point2D left => new Point2D(x-1, y);
-    [PublicAPI] public Point2D right => new Point2D(x+1, y);
+    [PublicAPI] public Point2D up => new Point2D(_x, _y+1);
+    [PublicAPI] public Point2D down => new Point2D(_x, _y-1);
+    [PublicAPI] public Point2D left => new Point2D(_x-1, _y);
+    [PublicAPI] public Point2D right => new Point2D(_x+1, _y);
 
-    public static implicit operator Vector2(Point2D p) => new Vector2(p.x, p.y);
-    public static implicit operator Vector3(Point2D p) => new Vector3(p.x, p.y);
-    public static Point2D operator +(Point2D a, Point2D b) => new Point2D(a.x + b.x, a.y + b.y);
-    public static Point2D operator -(Point2D a, Point2D b) => new Point2D(a.x - b.x, a.y - b.y);
-    public static Point2D operator -(Point2D a) => new Point2D(-a.x, -a.y);
+    public static implicit operator Vector2(Point2D p) => new Vector2(p._x, p._y);
+    public static implicit operator Vector3(Point2D p) => new Vector3(p._x, p._y);
+    public static Point2D operator +(Point2D a, Point2D b) => new Point2D(a._x + b._x, a._y + b._y);
+    public static Point2D operator -(Point2D a, Point2D b) => new Point2D(a._x - b._x, a._y - b._y);
+    public static Point2D operator -(Point2D a) => new Point2D(-a._x, -a._y);
 
     [PublicAPI]
     public IEnumerable<Point2D> around(uint radius) {
@@ -43,12 +33,10 @@ namespace com.tinylabproductions.TLPLib.Data {
       }
     }
 
-    public override string ToString() => $"({x},{y})";
+    public override string ToString() => $"({_x},{_y})";
 
     [PublicAPI]
     public static readonly ISerializedRW<Point2D> rw =
-      SerializedRW.integer.and(SerializedRW.integer, (x, y) => new Point2D(x, y), _ => _.x, _ => _.y);
-
-    public Point2DSerializable toPoint2DSerializable() => new Point2DSerializable(x, y);
+      SerializedRW.integer.and(SerializedRW.integer, (x, y) => new Point2D(x, y), _ => _._x, _ => _._y);
   }
 }
