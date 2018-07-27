16f32a89b4aa9109fd39c4aaa98634058ea3d896
Arturas Slajus <arturas@tinylabproductions.com>
2018-07-27 12:31:42 +0300

    Wrote a custom inspector for unity option.

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/unity_serialization/DrawerUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Editor.unity_serialization {
+  public static class DrawerUtils {
+    [PublicAPI]
+    public static void twoFields(Rect position, GUIContent label, out Rect firstField, out Rect secondField) {
+      position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+      var half = position.width / 2;
+      firstField = new Rect(position.x, position.y, half, position.height);
+      secondField = new Rect(position.x + half, position.y, half, position.height);
+    }
+
+    [PublicAPI]
+    public static void twoFields(Rect position, out Rect firstField, out Rect secondField) {
+      firstField = new Rect(position.x, position.y, EditorGUIUtility.labelWidth, position.height);
+      secondField = new Rect(
+        position.x + EditorGUIUtility.labelWidth, position.y,
+        position.width - EditorGUIUtility.labelWidth, position.height
+      );
+    }
+  }
+
+  [PublicAPI]
+  public struct EditorIndent : IDisposable {
+    readonly int initialLevel;
+
+    public EditorIndent(int wantedLevel) {
+      initialLevel = EditorGUI.indentLevel;
+      EditorGUI.indentLevel = wantedLevel;
+    }
+    
+    public void Dispose() { EditorGUI.indentLevel = initialLevel; }
+  }
+}
