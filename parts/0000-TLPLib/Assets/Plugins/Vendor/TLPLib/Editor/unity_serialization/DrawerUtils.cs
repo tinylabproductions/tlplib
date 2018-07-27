e1668f3d66aae1150784d2b9aafa46791140dbd1
Arturas Slajus <arturas@tinylabproductions.com>
2018-07-27 14:55:05 +0300

    Duration type drawer.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/unity_serialization/DrawerUtils.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Editor/unity_serialization/DrawerUtils.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEditor;
 using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Editor.unity_serialization {
   public static class DrawerUtils {
+    static readonly Dictionary<Color, GUIStyle> overlayStyleCache = new Dictionary<Color, GUIStyle>();
+
+    public static GUIStyle overlayStyle() => overlayStyle(Color.blue);
+    
+    public static GUIStyle overlayStyle(Color color) {
+      if (overlayStyleCache.TryGetValue(color, out var cached)) {
+        return cached;
+      }
+      else {
+        var overlay = new GUIStyle(EditorStyles.miniLabel) {
+          alignment = TextAnchor.MiddleRight,
+          contentOffset = new Vector2(-2, 0)
+        };
+
+        var c = EditorGUIUtility.isProSkin ? new Color(1 - color.r, 1 - color.g, 1 - color.b) : color;
+        c.a = 0.75f;
+
+        overlay.normal.textColor = c;
+        overlayStyleCache.Add(color, overlay);
+        return overlay;
+      }
+    }
+    
     [PublicAPI]
     public static void twoFields(Rect position, GUIContent label, out Rect firstField, out Rect secondField) {
       position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
