7e96c934a98c2ab309607576483bd00899250964
Arturas Slajus <arturas@tinylabproductions.com>
2018-08-13 16:13:53 +0300

    Updated Percentage to be serializable and drawable from unity

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Data/Percentage.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Data/Percentage.cs
@@ -1,34 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using GenerationAttributes;
+using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Data {
-  public struct Percentage : IEquatable<Percentage> {
-    // [0, 1]
-    public readonly float value;
-
-    public Percentage(float value) {
-      this.value = value;
-    }
-
-    public bool Equals(Percentage other) {
-      return value.Equals(other.value);
-    }
-
-    public override bool Equals(object obj) {
-      if (ReferenceEquals(null, obj)) return false;
-      return obj is Percentage && Equals((Percentage) obj);
-    }
-
-    public override int GetHashCode() {
-      return value.GetHashCode();
-    }
-
-    public static bool operator ==(Percentage left, Percentage right) {
-      return left.Equals(right);
-    }
-
-    public static bool operator !=(Percentage left, Percentage right) {
-      return !left.Equals(right);
-    }
+  [Serializable, Record]
+  public partial struct Percentage {
+    [SerializeField, Range(0, 1), PublicAccessor] float _value;
   }
 }
