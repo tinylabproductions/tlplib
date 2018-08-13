8c89b90140cedff58f83d5d08febf4ffbea4ea75
JurgisRainys <jurgiuir@gmail.com>
2018-08-13 14:44:43 +0300

    namespace fix

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/TriggerEnterForwarder.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/Forwarders/TriggerEnterForwarder.cs
@@ -1,8 +1,7 @@
-﻿using com.tinylabproductions.TLPLib.Components.Forwarders;
-using com.tinylabproductions.TLPLib.Components.Interfaces;
+﻿using com.tinylabproductions.TLPLib.Components.Interfaces;
 using UnityEngine;
 
-namespace Plugins.Vendor.TLPLib.Components.Forwarders {
+namespace com.tinylabproductions.TLPLib.Components.Forwarders {
   public class TriggerEnterForwarder : EventForwarder<Collider>, IMB_OnTriggerEnter {
     public void OnTriggerEnter(Collider other) => _onEvent.push(other);
   }
