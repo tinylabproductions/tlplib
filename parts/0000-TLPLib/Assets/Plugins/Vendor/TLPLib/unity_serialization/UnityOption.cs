5290db44f0212d409e36ed9f04361a0749b6b386
Arturas Slajus <x11@arturaz.net>
2018-08-19 12:20:09 +0300

    Reorganization.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/unity_serialization/UnityOption.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/unity_serialization/UnityOption.cs
@@ -140,14 +140,4 @@ namespace com.tinylabproductions.TLPLib.unity_serialization {
   [Serializable, PublicAPI] public class UnityOptionParticleSystem : UnityOption<ParticleSystem> {}
   [Serializable, PublicAPI] public class UnityOptionTrailRenderer : UnityOption<TrailRenderer> {}
   [Serializable, PublicAPI] public class UnityOptionLayerMask : UnityOption<LayerMask> {}
-  [Serializable] public struct Tag {
-    #region Unity Serialized Fields
-#pragma warning disable 649
-    // ReSharper disable NotNullMemberIsNotInitialized, FieldCanBeMadeReadOnly.Local, ConvertToConstant.Local
-    [SerializeField, TextField(TextFieldType.Tag)] public string value;
-    // ReSharper restore NotNullMemberIsNotInitialized, FieldCanBeMadeReadOnly.Local, ConvertToConstant.Local
-#pragma warning restore 649
-    #endregion
-  }
-  [Serializable, PublicAPI] public class UnityOptionTag : UnityOption<Tag> { }
 }
