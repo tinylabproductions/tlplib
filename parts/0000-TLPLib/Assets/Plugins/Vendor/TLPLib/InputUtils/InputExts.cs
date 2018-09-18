8a2545d50c64fccce14c750fc5c1a611a9cca14e
Astro75 <evaldas757@gmail.com>
2018-09-18 19:41:26 +0300

    Reduced GC allocations

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/InputUtils/InputExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/InputUtils/InputExts.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using com.tinylabproductions.TLPLib.Extensions;
 using JetBrains.Annotations;
 using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.InputUtils {
   public static class InputExts {
-    [PublicAPI] public static bool getKey(this IList<KeyCode> kcs) => kcs.anyGCFree(Input.GetKey); 
-    [PublicAPI] public static bool getKeyDown(this IList<KeyCode> kcs) => kcs.anyGCFree(Input.GetKeyDown); 
-    [PublicAPI] public static bool getKeyUp(this IList<KeyCode> kcs) => kcs.anyGCFree(Input.GetKeyUp); 
+    // ReSharper disable ConvertClosureToMethodGroup
+    [PublicAPI] public static bool getKey(this IList<KeyCode> kcs) => kcs.anyGCFree(_ => Input.GetKey(_));
+    [PublicAPI] public static bool getKeyDown(this IList<KeyCode> kcs) => kcs.anyGCFree(_ => Input.GetKeyDown(_));
+    [PublicAPI] public static bool getKeyUp(this IList<KeyCode> kcs) => kcs.anyGCFree(_ => Input.GetKeyUp(_));
+    // ReSharper restore ConvertClosureToMethodGroup
   }
 }
