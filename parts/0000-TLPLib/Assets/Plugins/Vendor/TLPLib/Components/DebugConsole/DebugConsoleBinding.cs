04a03350c5a3e6ae34e8cbea7326cbbdb07215f2
JurgisRainys <jurgiuir@gmail.com>
2018-09-18 11:07:14 +0300

    minimize

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/DebugConsole/DebugConsoleBinding.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/DebugConsole/DebugConsoleBinding.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using com.tinylabproductions.TLPLib.Components.ui;
 using com.tinylabproductions.TLPLib.Data;
+using GenerationAttributes;
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace com.tinylabproductions.TLPLib.Components.DebugConsole {
-  public class DebugConsoleBinding : MonoBehaviour {
+  public partial class DebugConsoleBinding : MonoBehaviour {
     [NotNull] public DebugConsoleListBinding commandGroups, commands;
     [NotNull] public Text commandGroupLabel;
     [NotNull] public ButtonBinding buttonPrefab;
     [NotNull] public Button closeButton;
+    [SerializeField, NotNull, PublicAccessor] UIClickForwarder _minimizeButton;
+    [SerializeField, NotNull, PublicAccessor] GameObject _minimizableObjectsContainer;
     [NotNull] public DynamicVerticalLayout dynamicLayout;
     [NotNull] public VerticalLayoutLogEntryPrefab logEntry;
     
