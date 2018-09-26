5458cec0056eda878bc96bcee943dac7e4092b10
Arturas Slajus <arturas@tinylabproductions.com>
2018-09-26 14:27:02 +0300

    Fix compiler warnings.

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/unity_serialization/SpriteAndRenderer.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/unity_serialization/SpriteAndRenderer.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.unity_serialization {
   [Serializable] public partial struct SpriteAndRenderer {
+    #region Unity Serialized Fields
+#pragma warning disable 649
+    // ReSharper disable UnassignedField.Global, FieldCanBeMadeReadOnly.Global
     [SerializeField, NotNull, PublicAccessor] Sprite _sprite;
     [SerializeField, NotNull, PublicAccessor] SpriteRenderer _renderer;
+    
+    // ReSharper restore UnassignedField.Global, FieldCanBeMadeReadOnly.Global
+#pragma warning restore 649
+    #endregion
   }
   [Serializable] public class SpriteAndRendererOption : UnityOption<SpriteAndRenderer> {}
   [Serializable] public class SpritesAndRenderersOption : UnityOption<SpriteAndRenderer[]> {}
