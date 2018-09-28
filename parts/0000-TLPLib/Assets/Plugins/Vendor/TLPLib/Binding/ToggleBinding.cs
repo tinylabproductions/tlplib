466452c7dde80e4b319217937c925e8400eed5d5
Donatas Laurinavicius <laurinavicius.donatas@gmail.com>
2018-09-28 15:11:44 +0300

    ToggleBinding from FKR

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Binding/ToggleBinding.cs
@@ -0,0 +1,22 @@
+﻿using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Reactive;
+using UnityEngine;
+
+namespace Plugins.Vendor.TLPLib.Binding {
+  public class ToggleBinding : MonoBehaviour {
+    [Header("Optional")]
+    public GameObject whenEnabled, whenDisabled;
+
+    public virtual void setEnabled(bool enabled) {
+      if (whenEnabled) {
+        whenEnabled.SetActive(enabled);
+      }
+      if (whenDisabled) {
+        whenDisabled.SetActive(!enabled);
+      }
+    }
+
+    public IObservable<Unit> uiClick => gameObject.uiClick();
+  }
+}
