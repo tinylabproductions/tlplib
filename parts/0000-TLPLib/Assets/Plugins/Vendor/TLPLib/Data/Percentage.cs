7531acee4da1f892684d89428f1e51bc21fb8c95
gyts <gytis.stanciukas@gmail.com>
2018-10-02 15:20:11 +0300

    Percentage removed ToString() override

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Data/Percentage.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Data/Percentage.cs
@@ -7,13 +7,11 @@ using UnityEngine;
 using Object = UnityEngine.Object;
 
 namespace com.tinylabproductions.TLPLib.Data {
-  [Record(GenerateToString = false), Serializable]
+  [Record, Serializable]
   public partial struct Percentage : OnObjectValidate {
     // [0, 1]
     // ReSharper disable once FieldCanBeMadeReadOnly.Local
     [SerializeField, PublicAccessor, Help(HelpType.Info, "[0.0, 1.0]")] float _value;
-
-    public override string ToString() => $"{_value * 100}%";
 
     public IEnumerable<ErrorMsg> onObjectValidate(Object containingComponent) {
       if (_value < 0f || _value > 1f)
