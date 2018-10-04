b2899a80ca157ea636efc1b72f57aa1d840ec1d1
Arturas Slajus <arturas@tinylabproductions.com>
2018-10-04 10:24:18 +0300

    [PublicAPI]

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/ImmutableArrayExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/ImmutableArrayExts.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using com.tinylabproductions.TLPLib.Data;
 using com.tinylabproductions.TLPLib.Functional;
+using JetBrains.Annotations;
 
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class ImmutableArrayExts {
-    public static ImmutableArray<To> map<From, To>(
+    [PublicAPI] public static ImmutableArray<To> map<From, To>(
       this ImmutableArray<From> source, Fn<From, To> mapper
     ) {
       var target = ImmutableArray.CreateBuilder<To>(source.Length);
@@ -14,32 +15,48 @@ namespace com.tinylabproductions.TLPLib.Extensions {
       return target.MoveToImmutable();
     }
 
-    public static Option<T> get<T>(this ImmutableArray<T> list, int index) =>
+    [PublicAPI] public static Option<T> get<T>(this ImmutableArray<T> list, int index) =>
       index >= 0 && index < list.Length ? F.some(list[index]) : F.none<T>();
 
-    public static bool isEmpty<A>(this ImmutableArray<A> list) => list.Length == 0;
-    public static bool nonEmpty<A>(this ImmutableArray<A> list) => list.Length != 0;
+    [PublicAPI] public static bool isEmpty<A>(this ImmutableArray<A> list) => list.Length == 0;
+    [PublicAPI] public static bool nonEmpty<A>(this ImmutableArray<A> list) => list.Length != 0;
 
-    public static Range indexRange<A>(this ImmutableArray<A> coll) =>
+    [PublicAPI] public static Range indexRange<A>(this ImmutableArray<A> coll) =>
       new Range(0, coll.Length - 1);
+
+    [PublicAPI]
+    public static ImmutableArray<A> ReplaceAt<A>(this ImmutableArray<A> arr, int idx, A value) {
+      if (idx < 0 || idx >= arr.Length) throw new ArgumentOutOfRangeException(
+        nameof(idx), idx, $"index {idx} is < 0 || >= array length {arr.Length}"
+      );
+      var b = ImmutableArray.CreateBuilder<A>(arr.Length);
+      for (var i = 0; i < idx; i++) {
+        b.Add(arr[i]);
+      }
+      b.Add(value);
+      for (var i = idx + 1; i < arr.Length; i++) {
+        b.Add(arr[i]);
+      }
+      return b.MoveToImmutable();
+    }
   }
 
   public static class ImmutableArrayBuilderExts {
-    public static ImmutableArray<A>.Builder addAnd<A>(
+    [PublicAPI] public static ImmutableArray<A>.Builder addAnd<A>(
       this ImmutableArray<A>.Builder b, A a
     ) {
       b.Add(a);
       return b;
     }
 
-    public static ImmutableArray<A>.Builder addOptAnd<A>(
+    [PublicAPI] public static ImmutableArray<A>.Builder addOptAnd<A>(
       this ImmutableArray<A>.Builder b, Option<A> aOpt
     ) {
       if (aOpt.isSome) b.Add(aOpt.__unsafeGetValue);
       return b;
     }
 
-    public static ImmutableArray<A>.Builder addRangeAnd<A>(
+    [PublicAPI] public static ImmutableArray<A>.Builder addRangeAnd<A>(
       this ImmutableArray<A>.Builder b, IEnumerable<A> aEnumerable
     ) {
       b.AddRange(aEnumerable);
@@ -52,7 +69,7 @@ namespace com.tinylabproductions.TLPLib.Extensions {
     /// <typeparam name="A"></typeparam>
     /// <param name="b"></param>
     /// <returns></returns>
-    public static ImmutableArray<A> MoveToImmutableSafe<A>(
+    [PublicAPI] public static ImmutableArray<A> MoveToImmutableSafe<A>(
       this ImmutableArray<A>.Builder b
     ) => b.Capacity == b.Count ? b.MoveToImmutable() : b.ToImmutable();
   }
