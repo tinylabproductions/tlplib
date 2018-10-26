11c1bf8ae0a5632ae0b0ea565150f4e138162a0f
Karolis <karolisjc@gmail.com>
2018-10-26 10:10:34 +0300

    warnings

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/DebugConsole/DebugConsoleBinding.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Components/DebugConsole/DebugConsoleBinding.cs
@@ -8,6 +8,10 @@ using UnityEngine.UI;
 
 namespace com.tinylabproductions.TLPLib.Components.DebugConsole {
   public partial class DebugConsoleBinding : MonoBehaviour {
+    #region Unity Serialized Fields
+
+#pragma warning disable 649
+// ReSharper disable NotNullMemberIsNotInitialized, FieldCanBeMadeReadOnly.Local
     [NotNull] public DebugConsoleListBinding commandGroups, commands;
     [NotNull] public Text commandGroupLabel;
     [NotNull] public ButtonBinding buttonPrefab;
@@ -16,6 +20,10 @@ namespace com.tinylabproductions.TLPLib.Components.DebugConsole {
     [SerializeField, NotNull, PublicAccessor] GameObject _minimizableObjectsContainer;
     [NotNull] public DynamicVerticalLayout dynamicLayout;
     [NotNull] public VerticalLayoutLogEntryPrefab logEntry;
+// ReSharper restore NotNullMemberIsNotInitialized, FieldCanBeMadeReadOnly.Local
+#pragma warning restore 649
+
+    #endregion
     
     public float lineWidth => dynamicLayout.maskRect.rect.width;
   }
