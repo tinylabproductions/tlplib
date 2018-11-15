f437d10132de79cad5d9c1de342da9285af2702b
Astro75 <evaldas757@gmail.com>
2018-11-15 14:13:38 +0200

    Updated texture types

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/TextureSize.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/TextureSize.cs
@@ -114,6 +114,11 @@ namespace Code.Utils {
         case TextureFormat.BC5: return false;
         case TextureFormat.BC6H: return false;
         case TextureFormat.BC7: return true;
+        case TextureFormat.RGB9e5Float: return false;
+        case TextureFormat.RG16: return false;
+        case TextureFormat.R8: return false;
+        case TextureFormat.ETC_RGB4Crunched: return false;
+        case TextureFormat.ETC2_RGBA8Crunched: return true;
         default:
           throw new ArgumentOutOfRangeException(nameof(tf), tf, null);
       }
@@ -175,6 +180,11 @@ namespace Code.Utils {
         case TextureFormat.BC5: return 8;
         case TextureFormat.BC6H: return 8;
         case TextureFormat.BC7: return 8;
+        case TextureFormat.RGB9e5Float: return 9 * 3 + 5;
+        case TextureFormat.RG16: return 16;
+        case TextureFormat.R8: return 8;
+        case TextureFormat.ETC_RGB4Crunched: return 4;
+        case TextureFormat.ETC2_RGBA8Crunched: return 8;
         default:
           throw new ArgumentOutOfRangeException(nameof(tf), tf, null);
       }
