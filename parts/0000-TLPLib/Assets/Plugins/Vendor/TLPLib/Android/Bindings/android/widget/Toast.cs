f26f135d8389efc0811e8f052c6b3d503380c6f6
gyts <gytis.stanciukas@gmail.com>
2018-11-21 17:04:20 +0200

    toast

---
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Android/Bindings/android/widget/Toast.cs
@@ -0,0 +1,18 @@
+﻿using com.tinylabproductions.TLPLib.Android.Bindings.android.app;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Android.Bindings.android.widget {
+  public class Toast : Binding {
+    public enum Duration { LENGTH_SHORT = 0, LENGTH_LONG = 1 }
+
+    Toast(AndroidJavaObject java) : base(java) { }
+
+    public static Toast create(string message, Duration duration, Activity activity = null) =>
+      new Toast(
+        new AndroidJavaClass("android.widget.Toast")
+        .csjo("makeText", (activity ?? AndroidActivity.current).java, message, duration)
+      );
+
+    public void show() => java.Call("show");
+  }
+}
