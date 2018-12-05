a02024355a7ee61d8a30c4d648e10d1a9782e744
Arturas Slajus <arturas@tinylabproductions.com>
2018-12-05 10:49:34 +0200

    MathUtils.PointDistanceToLine, MathUtils.PointDistanceToLineSegment

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/MathUtils.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Utilities/MathUtils.cs
@@ -1,5 +1,7 @@
-﻿using com.tinylabproductions.TLPLib.Data;
+﻿using System;
+using com.tinylabproductions.TLPLib.Data;
 using com.tinylabproductions.TLPLib.Functional;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Utilities {
@@ -140,6 +142,51 @@ namespace com.tinylabproductions.TLPLib.Utilities {
 
         return false;
       }
+    }      
+    
+    /// <summary>Distance from a point to a line.</summary>
+    [PublicAPI] public static float PointDistanceToLine(Vector3 point, Vector3 a, Vector3 b) => 
+      Mathf.Abs((b.x - a.x) * (a.y - point.y) - (a.x - point.x) * (b.y - a.y)) 
+      / Mathf.Sqrt(Mathf.Pow(b.x - a.x, 2) + Mathf.Pow(b.y - a.y, 2));
+    
+    // Calculate the distance between
+    // point pt and the segment p1 --> p2.
+    // http://csharphelper.com/blog/2016/09/find-the-shortest-distance-between-a-point-and-a-line-segment-in-c/
+    [PublicAPI] public static float PointDistanceToLineSegment(
+      Vector2 pt, Vector2 p1, Vector2 p2, out Vector2 closest
+    ) {
+      var dx = p2.x - p1.x;
+      var dy = p2.y - p1.y;
+      if (dx == 0 && dy == 0) {
+        // It's a point not a line segment.
+        closest = p1;
+        dx = pt.x - p1.x;
+        dy = pt.y - p1.y;
+        return Mathf.Sqrt(dx * dx + dy * dy);
+      }
+
+      // Calculate the t that minimizes the distance.
+      var t = ((pt.x - p1.x) * dx + (pt.y - p1.y) * dy) / (dx * dx + dy * dy);
+
+      // See if this represents one of the segment's
+      // end points or a point in the middle.
+      if (t < 0) {
+        closest = new Vector2(p1.x, p1.y);
+        dx = pt.x - p1.x;
+        dy = pt.y - p1.y;
+      }
+      else if (t > 1) {
+        closest = new Vector2(p2.x, p2.y);
+        dx = pt.x - p2.x;
+        dy = pt.y - p2.y;
+      }
+      else {
+        closest = new Vector2(p1.x + t * dx, p1.y + t * dy);
+        dx = pt.x - closest.x;
+        dy = pt.y - closest.y;
+      }
+
+      return Mathf.Sqrt(dx * dx + dy * dy);
     }
 
     /// <summary>Re-maps a number from one range to another</summary>
