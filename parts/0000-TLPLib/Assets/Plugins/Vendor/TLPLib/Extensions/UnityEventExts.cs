e03a06932b752ac7481bd8d08102ca07a1307bba
Arturas Slajus <arturas@tinylabproductions.com>
2019-01-15 17:39:27 +0200

    UnityEvent<A>#toRxVal

--- parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/UnityEventExts.cs
+++ parts/0000-TLPLib/Assets/Plugins/Vendor/TLPLib/Extensions/UnityEventExts.cs
@@ -1,11 +1,19 @@
-﻿using com.tinylabproductions.TLPLib.Reactive;
+﻿using com.tinylabproductions.TLPLib.dispose;
+using com.tinylabproductions.TLPLib.Reactive;
+using JetBrains.Annotations;
 using UnityEngine.Events;
 
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class UnityEventExts {
-    public static ISubscription subscribe<A>(this UnityEvent<A> rectEvent, UnityAction<A> act) {
-      rectEvent.AddListener(act);
-      return new Subscription(() => rectEvent.RemoveListener(act));
+    [PublicAPI] public static ISubscription subscribe<A>(this UnityEvent<A> evt, UnityAction<A> act) {
+      evt.AddListener(act);
+      return new Subscription(() => evt.RemoveListener(act));
+    }
+
+    [PublicAPI] public static IRxVal<A> toRxVal<A>(this UnityEvent<A> evt, IDisposableTracker tracker, A initial) {
+      var rx = RxRef.a(initial);
+      tracker.track(evt.subscribe(a => rx.value = a));
+      return rx;
     }
   }
 }
