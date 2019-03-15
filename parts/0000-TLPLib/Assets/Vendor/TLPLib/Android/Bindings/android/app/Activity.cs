3e70158631b322b67bef3547cc757ef9e3f859ab
Arturas Slajus <arturas@tinylabproductions.com>
2019-03-15 12:18:15 +0200

    Android getIntent/getAction

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Android/Bindings/android/app/Activity.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Android/Bindings/android/app/Activity.cs
@@ -7,17 +7,18 @@ using JetBrains.Annotations;
 using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Android.Bindings.android.app {
-  [JavaBinding("android.app.Activity")]
+  [JavaBinding("android.app.Activity"), PublicAPI]
   public class Activity : Context {
     public Activity(AndroidJavaObject java) : base(java) {}
 
-    [PublicAPI]
     public Application application => 
       new Application(java.cjo("getApplication"));
     
-    [PublicAPI]
     public void runOnUIThread(Action action) =>
       java.Call("runOnUiThread", new Runnable(action));
+    
+    public Intent getIntent() =>
+      new Intent(java.cjo("getIntent"));
   }
 }
 #endif
--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Android/Bindings/android/content/Intent.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Android/Bindings/android/content/Intent.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿#if UNITY_ANDROID
+using GenerationAttributes;
+using JetBrains.Annotations;
+using UnityEngine;
 
-#if UNITY_ANDROID
 namespace com.tinylabproductions.TLPLib.Android.Bindings.android.content {
+  [JavaBinding("android.content.Intent"), PublicAPI]
   public class Intent : Binding {
     public Intent(AndroidJavaObject java) : base(java) { }
+
+    public string getAction() => java.Call<string>("getAction");
   }
 }
 #endif
