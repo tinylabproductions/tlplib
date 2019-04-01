b2d80457bbc87d38eab17d03483c4ab9b6a7852c
Arturas Slajus <arturas@tinylabproductions.com>
2019-04-01 10:26:42 +0300

    Make KeyCodeWithModifiers serializable.

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Data/KeyCodeWithModifiers.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Data/KeyCodeWithModifiers.cs
@@ -1,19 +1,22 @@
+using System;
 using GenerationAttributes;
 using JetBrains.Annotations;
 using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Data {
-  [Record, PublicAPI] public readonly partial struct KeyCodeWithModifiers {
-    public readonly KeyCode keyCode;
-    public readonly bool shift, alt, ctrl;
+  [Record, PublicAPI, Serializable] public partial struct KeyCodeWithModifiers {
+#pragma warning disable 649
+    [PublicAccessor, SerializeField] KeyCode _keyCode;
+    [PublicAccessor, SerializeField] bool _shift, _alt, _ctrl;
+#pragma warning restore 649
 
     bool modifiersValid =>
-      (!shift || Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-      && (!alt || Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt) || Input.GetKey(KeyCode.AltGr))
-      && (!ctrl || Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl));
+      (!_shift || Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+      && (!_alt || Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt) || Input.GetKey(KeyCode.AltGr))
+      && (!_ctrl || Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl));
     
-    public bool getKey => Input.GetKey(keyCode) && modifiersValid;
-    public bool getKeyDown => Input.GetKeyDown(keyCode) && modifiersValid;
-    public bool getKeyUp => Input.GetKeyUp(keyCode) && modifiersValid;
+    public bool getKey => Input.GetKey(_keyCode) && modifiersValid;
+    public bool getKeyDown => Input.GetKeyDown(_keyCode) && modifiersValid;
+    public bool getKeyUp => Input.GetKeyUp(_keyCode) && modifiersValid;
   }
 }
