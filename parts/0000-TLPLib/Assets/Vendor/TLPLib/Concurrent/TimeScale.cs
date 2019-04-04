b90e8530d31e39c95cf90874efc1f05b45415a43
Astro75 <evaldas757@gmail.com>
2019-04-04 11:42:41 +0300

    TimeScale.delta

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Concurrent/TimeScale.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Concurrent/TimeScale.cs
@@ -22,6 +22,18 @@ namespace com.tinylabproductions.TLPLib.Concurrent {
       }
     }
 
+    public static float delta(this TimeScale ts) {
+      switch (ts) {
+        // we don't have delta time for realtime scale
+        case TimeScale.Realtime:     return Time.unscaledDeltaTime;
+        case TimeScale.Unity:        return Time.deltaTime;
+        case TimeScale.FixedTime:    return Time.fixedDeltaTime;
+        case TimeScale.UnscaledTime: return Time.unscaledDeltaTime;
+        default:
+          throw new ArgumentOutOfRangeException(nameof(ts), ts, null);
+      }
+    }
+
     public static TimeContext asContext(this TimeScale ts) {
       switch (ts) {
         case TimeScale.Realtime: return TimeContext.realTime;
