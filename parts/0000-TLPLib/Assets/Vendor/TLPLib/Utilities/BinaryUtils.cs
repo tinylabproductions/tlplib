a30247eaf10c13256871732dc6cf4678dcbad4a1
Arturas Slajus <arturas@tinylabproductions.com>
2019-04-10 12:58:55 +0300

    WriteLongBigEndian/ReadLongBigEndian

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Utilities/BinaryUtils.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Utilities/BinaryUtils.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
+using com.tinylabproductions.TLPLib.Tween.fun_tween.path;
 using JetBrains.Annotations;
 
 namespace com.tinylabproductions.TLPLib.Utilities {
@@ -22,6 +24,8 @@ namespace com.tinylabproductions.TLPLib.Utilities {
       }
     }
     
+    #region int
+    
     public static unsafe void WriteIntBigEndian(this byte[] writeBuffer, int i, int offset) {
       var block = stackalloc byte[4];
       var asIntPtr = (int*) block;
@@ -43,7 +47,39 @@ namespace com.tinylabproductions.TLPLib.Utilities {
     public static int ReadIntBigEndian(this byte[] data, int offset) => 
       data[offset] << 24 | data[offset + 1] << 16 | data[offset + 2] << 8 | data[offset + 3];
     
+    #endregion
     
+    #region long
+
+    const int SIZE_LONG = 8;
+    
+    public static unsafe void WriteLongBigEndian(this byte[] writeBuffer, long i, int offset) {
+      var block = stackalloc byte[SIZE_LONG];
+      var asIntPtr = (long*) block;
+      *asIntPtr = i;
+      if (BitConverter.IsLittleEndian) {
+        for (var idx = 0; idx < SIZE_LONG; idx++) {
+          writeBuffer[offset + idx] = block[SIZE_LONG - 1 - idx];
+        }
+      }
+      else {
+        for (var idx = 0; idx < SIZE_LONG; idx++) {
+          writeBuffer[offset + idx] = block[idx];
+        }
+      }
+    }
+
+    public static long ReadLongBigEndian(this byte[] data, int offset) {
+      var l = 0L;
+      for (var idx = 0; idx < SIZE_LONG; idx++) {
+        l = l | (data[offset + idx] << 8 * (SIZE_LONG - 1 - idx));
+      }
+      return l;
+    }
+    
+    #endregion
+    
+    #region ushort
     
     public static unsafe void WriteUShortBigEndian(this byte[] writeBuffer, ushort s, int offset) {
       var block = stackalloc byte[2];
@@ -61,5 +97,7 @@ namespace com.tinylabproductions.TLPLib.Utilities {
     
     public static ushort ReadUShortBigEndian(this byte[] data, int offset) => 
       (ushort) (data[offset] << 8 | data[offset + 1]);
+    
+    #endregion
   }
 }
