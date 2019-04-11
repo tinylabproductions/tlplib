7df518c0718ee93d318ebbf50de491a8a5028eef
Arturas Slajus <arturas@tinylabproductions.com>
2019-04-11 16:09:54 +0300

    Unity bind stuff

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Binding/UnityBind.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using com.tinylabproductions.TLPLib.Concurrent;
+using com.tinylabproductions.TLPLib.dispose;
+using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Pools;
+using com.tinylabproductions.TLPLib.Reactive;
+using GenerationAttributes;
+using JetBrains.Annotations;
+using UnityEngine;
+using Coroutine = com.tinylabproductions.TLPLib.Concurrent.Coroutine;
+
+namespace com.tinylabproductions.TLPLib.binding {
+  [PublicAPI] public static partial class UnityBind {
+    public static ISubscription bind<A>(
+      this IRxObservable<A> observable, IDisposableTracker tracker, Func<A, Coroutine> f
+    ) {
+      var lastCoroutine = F.none<Coroutine>();
+      void stopOpt() { foreach (var c in lastCoroutine) { c.stop(); } };
+      var sub = observable.subscribe(
+        NoOpDisposableTracker.instance,
+        a => {
+          stopOpt();
+          lastCoroutine = f(a).some();
+        }
+      ).andThen(stopOpt);
+      tracker.track(sub);
+      return sub;
+    }
+    
+    public static void bindEnumerable<Template, Data>(
+      string gameObjectPoolName,
+      Template template, IRxObservable<IEnumerable<Data>> rx,
+      IDisposableTracker tracker, Func<Template, Data, ISubscription> setup
+    ) where Template : Component {
+      template.gameObject.SetActive(false);
+      var pool = GameObjectPool.a(GameObjectPool.Init.noReparenting(
+        gameObjectPoolName,
+        create: () => template.clone(parent: template.transform.parent)
+      ));
+      
+      var current = new List<BindEnumerableEntry<Template>>();
+
+      rx.subscribe(tracker,list => {
+        cleanupCurrent();
+        
+        foreach (var element in list) {
+          var instance = pool.borrow();
+          var sub = setup(instance, element);
+          current.Add(new BindEnumerableEntry<Template>(instance, sub));
+        }
+      });
+      tracker.track(new Subscription(cleanupCurrent));
+      
+      void cleanupCurrent() {
+        foreach (var element in current) {
+          pool.release(element.instance);
+          element.subscription.unsubscribe();
+        }
+        current.Clear();
+      }
+    }
+
+    [Record] public readonly partial struct BindEnumerableEntry<Template> {
+      public readonly Template instance;
+      public readonly ISubscription subscription;
+    }
+  }
+}
