dd7f3574445b99b94e351c665a1cb4b6a4d1088a
Arturas Slajus <arturas@tinylabproductions.com>
2019-05-23 13:11:33 +0300

    PlayableDirector#play should log if director  is not enabled

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/PlayableDirectorExts.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/PlayableDirectorExts.cs
@@ -1,15 +1,27 @@
 using System.Collections;
+using com.tinylabproductions.TLPLib.Logger;
 using UnityEngine;
 using UnityEngine.Playables;
 
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class PlayableDirectorExts {
-    
     public static IEnumerator play(this PlayableDirector director, PlayableAsset asset) {
-      director.Play(asset, DirectorWrapMode.Hold);
-      director.Evaluate();
-      while (!Mathf.Approximately((float) director.time, (float) asset.duration))
-        yield return null;
+      // Directors do not play if the game object is not active.
+      if (!director.isActiveAndEnabled) {
+        Log.d.error(
+          $"Wanted to play {asset} on director, which is not active and enabled! " +
+          "This does not work, ensure the director is active and enabled.",
+          director
+        );
+      }
+      else {
+        director.gameObject.SetActive(true);
+        director.enabled = true;
+        director.Play(asset, DirectorWrapMode.Hold);
+        director.Evaluate();
+        while (!Mathf.Approximately((float) director.time, (float) asset.duration))
+          yield return null;
+      }
     }
     
     public static IEnumerator play(this PlayableDirector director) {
@@ -21,6 +33,5 @@ namespace com.tinylabproductions.TLPLib.Extensions {
       director.Evaluate();
       director.Stop();
     }
-    
   }
 }
