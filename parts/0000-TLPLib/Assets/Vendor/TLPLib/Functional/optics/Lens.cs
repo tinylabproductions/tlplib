d8dcd38be99ee9b6d45a82a24517c25ac4540d7f
Arturas Slajus <arturas@tinylabproductions.com>
2019-06-21 14:24:39 +0300

    Prism

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/optics/Lens.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/optics/Lens.cs
@@ -1,12 +1,14 @@
-using System;
 using JetBrains.Annotations;
 
 namespace com.tinylabproductions.TLPLib.Functional.optics {
   [PublicAPI] public sealed class Lens<From, To> {
-    public readonly Func<From, To> get;
-    public readonly Func<From, To, From> set;
+    public delegate To Get(From from);
+    public delegate From Set(From on, To value);
+    
+    public readonly Get get;
+    public readonly Set set;
 
-    public Lens(Func<From, To> get, Func<From, To, From> set) {
+    public Lens(Get get, Set set) {
       this.get = get;
       this.set = set;
     }
@@ -15,16 +17,25 @@ namespace com.tinylabproductions.TLPLib.Functional.optics {
       from => lens.get(get(from)),
       (from, toA) => set(from, lens.set(get(from), toA))
     );
+    
+    public Prism<From, ToA> combine<ToA>(Prism<To, ToA> prism) => new Prism<From, ToA>(
+      from => prism.get(get(from)),
+      (from, toA) => {
+        var toEither = prism.set(get(@from), toA);
+        if (toEither.rightValueOut(out var to2)) return set(from, to2);
+        else return toEither.__unsafeGetLeft;
+      }
+    );
   }
 
   [PublicAPI] public static class Lens {
     public static Builder<From> from<From>() => Builder<From>.instance;
 
-    public class Builder<From> {
+    [PublicAPI] public class Builder<From> {
       public static readonly Builder<From> instance = new Builder<From>();
       Builder() {}
 
-      public Lens<From, To> to<To>(Func<From, To> get, Func<From, To, From> set) => 
+      public Lens<From, To> to<To>(Lens<From, To>.Get get, Lens<From, To>.Set set) => 
         new Lens<From, To>(get, set);
     }
   }
---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Functional/optics/Prism.cs
@@ -0,0 +1,56 @@
+using JetBrains.Annotations;
+
+namespace com.tinylabproductions.TLPLib.Functional.optics {
+  [PublicAPI] public sealed class Prism<From, To> {
+    public delegate Either<string, To> Get(From from);
+    public delegate Either<string, From> Set(From on, To value);
+    
+    public readonly Get get;
+    public readonly Set set;
+
+    public Prism(Get get, Set set) {
+      this.get = get;
+      this.set = set;
+    }
+    
+    public Prism<From, ToA> combine<ToA>(Prism<To, ToA> prism) => new Prism<From, ToA>(
+      from => {
+        var toEither = get(from);
+        return toEither.rightValueOut(out var to) ? prism.get(to) : toEither.__unsafeGetLeft;
+      },
+      (on, toAValue) => {
+        var toEither1 = get(on);
+        if (toEither1.rightValueOut(out var to1)) {
+          var toEither2 = prism.set(to1, toAValue);
+          return toEither2.rightValueOut(out var to2) ? set(on, to2) : toEither2.__unsafeGetLeft;
+        }
+        else {
+          return toEither1.__unsafeGetLeft;
+        }
+      }
+    );
+    
+    public Prism<From, ToA> combine<ToA>(Lens<To, ToA> lens) => new Prism<From, ToA>(
+      from => {
+        var toEither = get(from);
+        return toEither.rightValueOut(out var to) ? (Either<string, ToA>) lens.get(to) : toEither.__unsafeGetLeft;
+      },
+      (on, toAValue) => {
+        var toEither1 = get(on);
+        return toEither1.rightValueOut(out var to1) ? set(@on, lens.set(to1, toAValue)) : toEither1.__unsafeGetLeft;
+      }
+    );
+  }
+
+  [PublicAPI] public static class Prism {
+    public static Builder<From> from<From>() => Builder<From>.instance;
+
+    [PublicAPI] public class Builder<From> {
+      public static readonly Builder<From> instance = new Builder<From>();
+      Builder() { }
+
+      public Prism<From, To> to<To>(Prism<From, To>.Get get, Prism<From, To>.Set set) =>
+        new Prism<From, To>(get, set);
+    }
+  }
+}
