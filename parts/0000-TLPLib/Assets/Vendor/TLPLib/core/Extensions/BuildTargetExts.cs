5d11018ef11d9fa94c938ee39421788a9a1d03cc
Arturas Slajus <x11@arturaz.net>
2020-02-20 17:58:23 +0200

    Lessen number of warnings.

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/core/Extensions/BuildTargetExts.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/core/Extensions/BuildTargetExts.cs
@@ -12,13 +12,17 @@ namespace com.tinylabproductions.TLPLib.Editor.Extensions {
         case BuildTarget.StandaloneWindows64:
           return RuntimePlatform.WindowsPlayer;
         case BuildTarget.StandaloneLinux64:
+#if !UNITY_2019_2_OR_NEWER
         case BuildTarget.StandaloneLinuxUniversal:
         case BuildTarget.StandaloneLinux:
+#endif
           return RuntimePlatform.LinuxPlayer;
         case BuildTarget.iOS: return RuntimePlatform.IPhonePlayer;
         case BuildTarget.Android: return RuntimePlatform.Android;
         case BuildTarget.WebGL: return RuntimePlatform.WebGLPlayer;
+#if !UNITY_2018_3_OR_NEWER
         case BuildTarget.PSP2: return RuntimePlatform.PSP2;
+#endif
         case BuildTarget.PS4: return RuntimePlatform.PS4;
         case BuildTarget.XboxOne: return RuntimePlatform.XboxOne;
         case BuildTarget.tvOS: return RuntimePlatform.tvOS;
@@ -33,7 +37,9 @@ namespace com.tinylabproductions.TLPLib.Editor.Extensions {
         case BuildTarget.WebPlayerStreamed:
 #endif
 #if UNITY_5_5_OR_NEWER
+#if !UNITY_2018_1_OR_NEWER
         case BuildTarget.N3DS:
+#endif
 #else
         case BuildTarget.PS3: return RuntimePlatform.PS3;
         case BuildTarget.XBOX360: return RuntimePlatform.XBOX360;
@@ -71,13 +77,17 @@ namespace com.tinylabproductions.TLPLib.Editor.Extensions {
         case BuildTarget.StandaloneWindows: return BuildTargetGroup.Standalone;
         case BuildTarget.iOS: return BuildTargetGroup.iOS;
         case BuildTarget.Android: return BuildTargetGroup.Android;
+#if !UNITY_2019_2_OR_NEWER
         case BuildTarget.StandaloneLinux: return BuildTargetGroup.Standalone;
+        case BuildTarget.StandaloneLinuxUniversal: return BuildTargetGroup.Standalone;
+#endif
         case BuildTarget.StandaloneWindows64: return BuildTargetGroup.Standalone;
         case BuildTarget.WebGL: return BuildTargetGroup.WebGL;
         case BuildTarget.WSAPlayer: return BuildTargetGroup.WSA;
         case BuildTarget.StandaloneLinux64: return BuildTargetGroup.Standalone;
-        case BuildTarget.StandaloneLinuxUniversal: return BuildTargetGroup.Standalone;
+#if !UNITY_2018_3_OR_NEWER
         case BuildTarget.PSP2: return BuildTargetGroup.PSP2;
+#endif
         case BuildTarget.PS4: return BuildTargetGroup.PS4;
         case BuildTarget.XboxOne: return BuildTargetGroup.XboxOne;
         case BuildTarget.tvOS: return BuildTargetGroup.tvOS;
@@ -92,7 +102,9 @@ namespace com.tinylabproductions.TLPLib.Editor.Extensions {
         case BuildTarget.BlackBerry: return BuildTargetGroup.BlackBerry;
 #endif
 #if UNITY_5_5_OR_NEWER
+#if !UNITY_2018_1_OR_NEWER
         case BuildTarget.N3DS: return BuildTargetGroup.N3DS;
+#endif
 #else
         case BuildTarget.PS3: return BuildTargetGroup.PS3;
         case BuildTarget.XBOX360: return BuildTargetGroup.XBOX360;
