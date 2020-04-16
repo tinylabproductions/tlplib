60be27de2cb1e8a1498561b9fb48d363303f1f05
Arturas Slajus <x11@arturaz.net>
2020-04-16 15:30:13 +0300

    macros for ILog

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/core/Logger/Log.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/core/Logger/Log.cs
@@ -233,7 +233,7 @@ namespace com.tinylabproductions.TLPLib.Logger {
     IRxObservable<LogEvent> messageLogged { get; }
   }
 
-  public static class ILogExts {
+  [PublicAPI] public static class ILogExts {
     public static bool isVerbose(this ILog log) => log.willLog(Log.Level.VERBOSE);
     public static bool isDebug(this ILog log) => log.willLog(Log.Level.DEBUG);
     public static bool isInfo(this ILog log) => log.willLog(Log.Level.INFO);
@@ -242,21 +242,49 @@ namespace com.tinylabproductions.TLPLib.Logger {
     public static void log(this ILog log, Log.Level l, string message) =>
       log.log(l, LogEntry.simple(message));
 
-    public static void verbose(this ILog log, string msg, Object context = null) =>
+    [StatementMethodMacro(
+@"{
+  var macro__log__ = ${log};
+  if (macro__log__.isVerbose()) macro__log__.verbose(msg: ${msg}, context: ${context});
+}")]
+    public static void mVerbose(this ILog log, string msg, object context = null) => throw new MacroException();
+    public static void verbose(this ILog log, string msg, object context = null) =>
       log.log(Log.Level.VERBOSE, LogEntry.simple(msg, context: context));
-    public static void debug(this ILog log, string msg, Object context = null) =>
+    
+    [StatementMethodMacro(
+@"{
+  var macro__log__ = ${log};
+  if (macro__log__.isDebug()) macro__log__.debug(msg: ${msg}, context: ${context});
+}")]
+    public static void mDebug(this ILog log, string msg, object context = null) => throw new MacroException();
+    public static void debug(this ILog log, string msg, object context = null) =>
       log.log(Log.Level.DEBUG, LogEntry.simple(msg, context: context));
-    public static void info(this ILog log, string msg, Object context = null) =>
+    
+    [StatementMethodMacro(
+@"{
+  var macro__log__ = ${log};
+  if (macro__log__.isInfo()) macro__log__.info(msg: ${msg}, context: ${context});
+}")]
+    public static void mInfo(this ILog log, string msg, object context = null) => throw new MacroException();
+    public static void info(this ILog log, string msg, object context = null) =>
       log.log(Log.Level.INFO, LogEntry.simple(msg, context: context));
-    public static void warn(this ILog log, string msg, Object context = null) =>
+    
+    [StatementMethodMacro(
+@"{
+  var macro__log__ = ${log};
+  if (macro__log__.isWarn()) macro__log__.warn(msg: ${msg}, context: ${context});
+}")]
+    public static void mWarn(this ILog log, string msg, object context = null) => throw new MacroException();
+    public static void warn(this ILog log, string msg, object context = null) =>
       log.warn(LogEntry.simple(msg, context: context));
     public static void warn(this ILog log, LogEntry entry) =>
       log.log(Log.Level.WARN, entry);
-    public static void error(this ILog log, string msg, Object context = null) =>
+    
+    public static void error(this ILog log, string msg, object context = null) =>
       log.error(LogEntry.simple(msg, context: context));
     public static void error(this ILog log, LogEntry entry) =>
       log.log(Log.Level.ERROR, entry);
-    public static void error(this ILog log, Exception ex, Object context = null) =>
+    public static void error(this ILog log, Exception ex, object context = null) =>
       log.error(ex.Message, ex, context);
     public static void error(this ILog log, string msg, Exception ex, object context = null) =>
       log.error(LogEntry.fromException(msg, ex, context: context));
