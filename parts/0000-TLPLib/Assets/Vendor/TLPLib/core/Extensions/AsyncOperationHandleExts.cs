1465be5a1d36570467234512753b740826ec8a82
Arturas Slajus <x11@arturaz.net>
2020-05-13 14:19:09 +0300

    AsyncOperationHandle<A>#toFuture: check if handle is already completed.

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/core/Extensions/AsyncOperationHandleExts.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/core/Extensions/AsyncOperationHandleExts.cs
@@ -7,14 +7,19 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 namespace com.tinylabproductions.TLPLib.Extensions {
   [PublicAPI] public static class AsyncOperationHandleExts {
     public static Future<AsyncOperationHandle<A>> toFuture<A>(this AsyncOperationHandle<A> handle) {
-      var f = Future.async<AsyncOperationHandle<A>>(out var promise);
-      handle.Completed += h => promise.complete(h);
-      return f;
+      if (handle.IsDone) {
+        return Future.successful(handle);
+      }
+      else {
+        var f = Future.async<AsyncOperationHandle<A>>(out var promise);
+        handle.Completed += h => promise.complete(h);
+        return f;
+      }
     }
 
     public static Either<Exception, A> toEither<A>(this AsyncOperationHandle<A> handle) =>
       handle.Status switch {
-        AsyncOperationStatus.None => new Exception("Handle is in status None!"),
+        AsyncOperationStatus.None => new Exception("Handle is not completed!"),
         AsyncOperationStatus.Succeeded => handle.Result,
         AsyncOperationStatus.Failed => handle.OperationException,
         _ => throw new ArgumentOutOfRangeException()
