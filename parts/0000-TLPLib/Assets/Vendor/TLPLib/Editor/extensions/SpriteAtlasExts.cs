838591a006f3d808cf1db65376dbfca1f7e3caf0
Astro75 <evaldas757@gmail.com>
2020-06-02 11:17:16 +0300

    Bugfix: fix finding sprites in subfolders at SpriteAtlasExts.getPackedSprites

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/extensions/SpriteAtlasExts.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/extensions/SpriteAtlasExts.cs
@@ -20,7 +20,12 @@ namespace com.tinylabproductions.TLPLib.Editor.extensions {
       // - Folder will be traversed. Each "Texture2D" child will be packed. Sub folder will be traversed.
       var packables = atlas.GetPackables();
       var sprites = ImmutableHashSet.CreateBuilder<Sprite>();
-      var folders = new List<string>();
+      var texturePaths = new List<string>();
+      AssetDatabase.Refresh();
+      var allImagePaths = AssetDatabase.GetAllAssetPaths().Select(_ => _.ToLowerInvariant()).Where(
+        // this is not a complete list, but it is what we use for our project
+        p => p.EndsWithFast(".psd") || p.EndsWithFast(".png")
+      ).ToArray();
       foreach (var packable in packables) {
         switch (packable) {
           case Sprite sprite:
@@ -30,28 +35,31 @@ namespace com.tinylabproductions.TLPLib.Editor.extensions {
             addSpritesFromTexture(texture);
             break;
           case DefaultAsset folder:
-            var path = AssetDatabase.GetAssetPath(folder);
-            folders.Add(path);
+            var path = AssetDatabase.GetAssetPath(folder).ToLowerInvariant();
+            texturePaths.AddRange(allImagePaths.Where(p => p.StartsWithFast(path)));
             break;
           default:
             throw new Exception($"Unknown packable of type {packable.GetType().FullName}: {packable}");
         }
       }
 
-      var textures = 
-        AssetDatabase.FindAssets("t:" + nameof(Texture2D), folders.ToArray())
-        .Select(AssetDatabase.GUIDToAssetPath)
-        .Select(AssetDatabase.LoadAssetAtPath<Texture2D>)
-        .toHashSet();
-
-      foreach (var texture in textures) {
-        addSpritesFromTexture(texture);
+      // AssetDatabase.FindAssets often fails to find all assets
+      // var textures = 
+      //   AssetDatabase.FindAssets("t:" + nameof(Texture2D), folders.ToArray())
+      //   .Select(AssetDatabase.GUIDToAssetPath)
+      //   .Select(AssetDatabase.LoadAssetAtPath<Texture2D>)
+      //   .toHashSet();
+      
+      foreach (var texturePath in texturePaths.Distinct()) {
+        addSpritesFromTexturePath(texturePath);
       }
 
       return sprites.ToImmutable();
 
-      void addSpritesFromTexture(Texture2D texture) {
-        var textureSprites = AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(texture)).OfType<Sprite>();
+      void addSpritesFromTexture(Texture2D texture) => addSpritesFromTexturePath(AssetDatabase.GetAssetPath(texture));
+      
+      void addSpritesFromTexturePath(string path) {
+        var textureSprites = AssetDatabase.LoadAllAssetsAtPath(path).OfType<Sprite>();
         foreach (var sprite in textureSprites) {
           sprites.Add(sprite);
         }
