37969eb1d3e11aefb449c1ace8f4caccdb869573
Arturas Slajus <arturas.slajus@gmail.com>
2020-10-22 11:19:11 +0300

    pzd.lib

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/core/Extensions/DoubleExts.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace com.tinylabproductions.TLPLib.Extensions {
-  public static class DoubleExts {
-    public static long roundToLong(this double d) => (long) Math.Round(d);
-  }
-}
