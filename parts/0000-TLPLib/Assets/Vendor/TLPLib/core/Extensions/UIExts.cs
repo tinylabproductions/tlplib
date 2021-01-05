d39b32a4ae11e73c651129a032756b8fdcfb9e96
Arturas Slajus <arturas.slajus@gmail.com>
2021-01-05 13:14:28 +0200

    GameObject.uiDownUp()

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/core/Extensions/UIExts.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/core/Extensions/UIExts.cs
@@ -1,5 +1,8 @@
-﻿using com.tinylabproductions.TLPLib.Components;
+﻿using System;
+using com.tinylabproductions.TLPLib.Components;
 using JetBrains.Annotations;
+using pzd.lib.concurrent;
+using pzd.lib.dispose;
 using pzd.lib.functional;
 using pzd.lib.reactive;
 using UnityEngine;
@@ -11,5 +14,22 @@ namespace com.tinylabproductions.TLPLib.Extensions {
     public static IRxObservable<Unit> uiClick(this GameObject go) => go.EnsureComponent<UIClickForwarder>().onClick;
     public static IRxObservable<PointerEventData> uiDown(this GameObject go) => go.EnsureComponent<UIDownUpForwarder>().onDown;
     public static IRxObservable<PointerEventData> uiUp(this GameObject go) => go.EnsureComponent<UIDownUpForwarder>().onUp;
+
+    /// <summary>
+    /// Returns the <see cref="TimeSpan"/> (which is calculated from <see cref="ITimeContext"/>) between the down
+    /// event and up event.
+    /// </summary>
+    public static IRxObservable<TimeSpan> uiDownUp(this GameObject go, ITimeContext timeContext) {
+      var mapper = new Func<PointerEventData, TimeSpan>(_ => timeContext.passedSinceStartup);
+
+      var downAt = go.uiDown().map(mapper);
+      var upAt = go.uiUp().map(mapper);
+      return downAt
+        .zip(upAt, (downAt, upAt) =>
+          // We need to filter this to prevent an event firing in the case of UP -> DOWN event sequence. 
+          upAt >= downAt ? Some.a(upAt - downAt) : None._
+        )
+        .collect(_ => _);
+    }
   }
 }
