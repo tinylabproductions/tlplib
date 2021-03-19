f005ac4ea2869653060a63185e32983924c8f550
Artūras Šlajus <a.slajus@game-insight.com>
2021-03-19 16:16:14 +0200

    More docs for TLPLibIOSBinding.cliArguments

--- parts/0000-TLPLib/Assets/Plugins/iOS/tlplib/TLPLibIOSBinding.cs
+++ parts/0000-TLPLib/Assets/Plugins/iOS/tlplib/TLPLibIOSBinding.cs
@@ -9,6 +9,9 @@ namespace com.tinylabproductions.TLPLib.iOS {
     /// Fetches arguments for the process from
     /// https://developer.apple.com/documentation/foundation/nsprocessinfo/1415596-arguments
     ///
+    /// The first argument will be a path to the process executable, for example:
+    /// /var/containers/Bundle/Application/GUID/your_bundle_id.app/your_bundle_id 
+    ///
     /// Beware that you can only pass these arguments from a debug build of the application.
     ///
     /// So if you have a production .ipa file, the process of launching it with command line interface (CLI)
@@ -31,17 +34,43 @@ namespace com.tinylabproductions.TLPLib.iOS {
     ///    Provisioning profiles that you already imported to XCode are stored in
     ///    "~/Library/MobileDevice/Provisioning Profiles".
     ///
-    /// 2. Use the [ipa-deploy](https://github.com/floatinghotpot/ipa-deploy) tool to launch the modified .ipa.
+    /// 2. Launch the modified .ipa.
     ///
-    ///    You can pass it the arguments of [ios-deploy](https://github.com/ios-control/ios-deploy) and it will pass
-    ///    those along for you.
+    ///    Method a)
+    ///      Use the [ios-deploy](https://github.com/ios-control/ios-deploy) tool.
     ///
-    ///    Example:
-    ///    /// <code><![CDATA[
-    ///      ipa-deploy your.ipa --debug --args "your command line arguments go here"
-    ///    ]]></code>
+    ///      From our experience this gives you an attached debugger and fast transmission of application logs from
+    ///      the device to the terminal. 
     ///
-    ///    BEWARE: the arguments are separated by space by the ios-deploy.
+    ///      Example:
+    ///      /// <code><![CDATA[
+    ///        # unzip the package
+    ///        unzip your.ipa
+    ///        # deploy & launch the bundle
+    ///        #
+    ///        # The argument parser supports quotes, so --foo="bar baz" will be passed as "--foo=bar baz" to C#.
+    ///        #
+    ///        # It uses https://docs.python.org/3/library/shlex.html#shlex.split to parse the arguments and errors
+    ///        # out if you use single quotes (') because internally it sticks your arguments into a Python script in
+    ///        # the following way:
+    ///        #
+    ///        #    args-arr = args_arr + shlex.split('your args string goes here')
+    ///        ios-deploy --bundle Payload/your_bundle_id.app --debug --args "your command line arguments go here"
+    ///      ]]></code>
+    ///
+    ///    Method b)
+    ///      Use the [ideviceinstaller](https://github.com/libimobiledevice/ideviceinstaller) shell tools.
+    ///
+    ///      For some reason the transmission of application logs from the device to the terminal is a lot slower than
+    ///      using method a. 
+    ///
+    ///      Example:
+    ///      /// <code><![CDATA[
+    ///        # install the package
+    ///        ideviceinstaller -i your.ipa
+    ///        # launch the bundle
+    ///        idevicedebug run your_bundle_id your command line arguments go here
+    ///      ]]></code>
     /// </summary>
     [LazyProperty] public static ImmutableArrayC<string> cliArguments {
       get {
