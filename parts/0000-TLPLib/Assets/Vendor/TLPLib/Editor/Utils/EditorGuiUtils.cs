b07f402143aee4d948d5e4905c4242239b49ea68
Evaldas Ciakas <evaldas757@gmail.com>
2021-05-28 19:11:01 +0300

    EditorGuiUtils.toggle

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Editor/Utils/EditorGuiUtils.cs
@@ -0,0 +1,20 @@
+﻿using pzd.lib.functional;
+using UnityEditor;
+
+namespace com.tinylabproductions.TLPLib.Editor.Utils {
+  public static class EditorGuiUtils {
+    /// <returns>True if value was changed</returns>
+    public static bool toggle(string label, ref bool value) {
+      EditorGUI.BeginChangeCheck();
+      value = EditorGUILayout.Toggle(label, value);
+      return EditorGUI.EndChangeCheck();
+    }
+    
+    /// <returns>Some if value was changed</returns>
+    public static Option<bool> toggleReturn(string label, bool value) {
+      EditorGUI.BeginChangeCheck();
+      var newValue = EditorGUILayout.Toggle(label, value);
+      return EditorGUI.EndChangeCheck() ? Some.a(newValue) : None._;
+    }
+  }
+}
