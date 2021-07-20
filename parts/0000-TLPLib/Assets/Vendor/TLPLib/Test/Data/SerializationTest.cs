f3e148f961f751fc827b5a7553f17adf57182dd1
Artūras Šlajus <a.slajus@game-insight.com>
2021-07-20 17:13:18 +0300

    Fix tests

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Test/Data/SerializationTest.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Test/Data/SerializationTest.cs
@@ -3,22 +3,16 @@ using com.tinylabproductions.TLPLib.Functional;
 using NUnit.Framework;
 using pzd.lib.serialization;
 using pzd.lib.test.serialization;
-using pzd.lib.test_framework;
 
 namespace com.tinylabproductions.TLPLib.Data {
   public class SerializationTestTplRW : SerializationTestBase {
     static readonly ISerializedRW<Tpl<int, string>> rw =
-      SerializedRWU.tpl(SerializedRW.integer, SerializedRW.str);
+      SerializedRW.integer.tpl(SerializedRW.str);
 
-    [Test]
-    public void TestTpl() {
+    [Test] public void TestTpl() {
       var t = F.t(1, "foo");
       var serialized = rw.serializeToArray(t);
       checkWithNoise(rw, serialized, t);
     }
-
-    [Test]
-    public void TestFailure() =>
-      rw.deserialize(noise.toArray(), 0).shouldBeLeft();
   }
 }
